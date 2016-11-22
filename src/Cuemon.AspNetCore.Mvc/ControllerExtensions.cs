f3e7f7f847ed0f28949331edd73ede96d550c894
gimlichael <mmdevelopment@hotmail.com>
2016-11-22 22:27:21 +0100

    -

--- src/Cuemon.AspNetCore.Mvc/ControllerExtensions.cs
+++ src/Cuemon.AspNetCore.Mvc/ControllerExtensions.cs
@@ -16,13 +16,13 @@ namespace Cuemon.AspNetCore.Mvc
     public static class ControllerExtensions
     {
         /// <summary>
-        /// Creates an <see cref="IActionResult" /> that produces an OK (200) response from <see cref="OkObjectResult" /> or a NotModified (304) response from <see cref="NoContentResult" />.
+        /// Creates an <see cref="IActionResult" /> that produces an OK (200) response from <see cref="OkObjectResult" /> or a NotModified (304) response from <see cref="EmptyResult" />.
         /// </summary>
         /// <typeparam name="T">The type of the value for the <see cref="IActionResult"/>.</typeparam>
         /// <param name="controller">The controller to extend.</param>
         /// <param name="value">The content to format into the entity body.</param>
         /// <param name="parser">The function delegate that will retrieve a <see cref="CacheValidator" /> and thereby determines the <see cref="IActionResult" /> of this method.</param>
-        /// <returns>An <see cref="IActionResult" /> object that is either created from <see cref="NoContentResult" /> or <see cref="OkObjectResult" />.</returns>
+        /// <returns>An <see cref="IActionResult" /> object that is either created from <see cref="EmptyResult" /> or <see cref="OkObjectResult" />.</returns>
         public static IActionResult OkOrNotModified<T>(this Controller controller, T value, Func<T, CacheValidator> parser = null)
         {
             var validator = parser?.Invoke(value) ?? CacheValidator.ReferencePoint;
