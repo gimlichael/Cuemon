5f3b2671c4e79de57556aad01bed221e45b3561d
gimlichael <mmdevelopment@hotmail.com>
2016-12-12 01:07:27 +0100

    Updated version to 4.1.2016.3500.

--- src/Cuemon.Serialization/Properties/AssemblyInfo.cs
+++ src/Cuemon.Serialization/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("442b5eb8-7212-4cb4-aab2-9587b647486e")]
-[assembly: AssemblyFileVersion("4.1.2016.3450")]
+[assembly: AssemblyFileVersion("4.1.2016.3500")]
 [assembly: AssemblyVersion("4.0.2016.0")]
