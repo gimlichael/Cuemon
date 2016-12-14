6284f0ab04e550b64cf663380ffb41a32c14747b
gimlichael <mmdevelopment@hotmail.com>
2016-12-14 23:04:17 +0100

    -

--- src/Cuemon.AspNetCore.Mvc/Infrastructure.cs
+++ src/Cuemon.AspNetCore.Mvc/Infrastructure.cs
@@ -16,18 +16,31 @@ namespace Cuemon.AspNetCore.Mvc
             var descriptor = context.ActionDescriptor as ControllerActionDescriptor;
             if (descriptor != null)
             {
-                var objects = new List<object>();
-                foreach (var pi in context.ActionDescriptor.Parameters)
+                var objects = context.ParseRuntimeParameters(descriptor);
+                context.Result = TimeMeasure.WithFunc(descriptor.MethodInfo.Invoke, context.Controller, objects, o =>
                 {
-                    objects.Add(ObjectConverter.ChangeType(context.RouteData.Values[pi.Name], pi.ParameterType));
-                }
-                context.Result = TimeMeasure.WithFunc(descriptor.MethodInfo.Invoke, context.Controller, objects.ToArray(), o =>
-                {
-                    o.RuntimeParameters = options.RuntimeParameters ?? objects.ToArray();
-                    o.MethodDescriptor = options.MethodDescriptor ?? (() => new MethodDescriptor(descriptor.MethodInfo));
+                    o.RuntimeParameters = options.RuntimeParameters ?? objects;
+                    o.MethodDescriptor = options.MethodDescriptor ?? (() => context.ParseMethodDescriptor(descriptor));
                     o.TimeMeasureCompletedThreshold = options.TimeMeasureCompletedThreshold;
                 }).Result as IActionResult;
             }
+        }
+
+        internal static object[] ParseRuntimeParameters(this FilterContext context, ControllerActionDescriptor descriptor)
+        {
+            if (descriptor == null) { return null; }
+            var objects = new List<object>();
+            foreach (var pi in descriptor.Parameters)
+            {
+                objects.Add(ObjectConverter.ChangeType(context.RouteData.Values[pi.Name], pi.ParameterType));
+            }
+            return objects.ToArray();
+        }
+
+        internal static MethodDescriptor ParseMethodDescriptor(this FilterContext context, ControllerActionDescriptor descriptor)
+        {
+            if (descriptor == null) { return null; }
+            return new MethodDescriptor(descriptor.MethodInfo);
         }
     }
 }
