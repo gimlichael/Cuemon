dc490db737afb3ac721f98ad1e3a3376c92e67aa
gimlichael <mmdevelopment@hotmail.com>
2016-12-15 01:49:51 +0100

    Minor tweak to support 400 Badrequest (in case user changes route parameters or similiar).

--- src/Cuemon.AspNetCore.Mvc/Infrastructure.cs
+++ src/Cuemon.AspNetCore.Mvc/Infrastructure.cs
@@ -1,4 +1,5 @@
-﻿using Cuemon.Diagnostics;
+﻿using System.Collections.Generic;
+using Cuemon.Diagnostics;
 using Cuemon.Reflection;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Controllers;
@@ -14,14 +15,27 @@ namespace Cuemon.AspNetCore.Mvc
             var descriptor = context.ActionDescriptor as ControllerActionDescriptor;
             if (descriptor != null)
             {
-                var objects = context.ActionArguments.Values.ToArray();
-                context.Result = TimeMeasure.WithFunc(descriptor.MethodInfo.Invoke, context.Controller, objects, o =>
+                var expectedObjects = context.ParseRuntimeParameters(descriptor);
+                var verifiedObjects = context.ActionArguments.Values.ToArray();
+                if (verifiedObjects.Length == expectedObjects.Length) { expectedObjects = verifiedObjects; }
+                context.Result = TimeMeasure.WithFunc(descriptor.MethodInfo.Invoke, context.Controller, expectedObjects, o =>
                 {
-                    o.RuntimeParameters = options.RuntimeParameters ?? objects;
+                    o.RuntimeParameters = options.RuntimeParameters ?? expectedObjects;
                     o.MethodDescriptor = options.MethodDescriptor ?? (() => context.ParseMethodDescriptor(descriptor));
                     o.TimeMeasureCompletedThreshold = options.TimeMeasureCompletedThreshold;
                 }).Result as IActionResult;
             }
+        }
+
+        internal static object[] ParseRuntimeParameters(this FilterContext context, ControllerActionDescriptor descriptor)
+        {
+            if (descriptor == null) { return null; }
+            var objects = new List<object>();
+            foreach (var pi in descriptor.Parameters)
+            {
+                objects.Add(ObjectConverter.ChangeType(context.RouteData.Values[pi.Name], pi.ParameterType));
+            }
+            return objects.ToArray();
         }
 
         internal static MethodDescriptor ParseMethodDescriptor(this FilterContext context, ControllerActionDescriptor descriptor)
