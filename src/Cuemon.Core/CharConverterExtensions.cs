c2b7fe2a781e582a13c41c78efd7f5c40d263fa9
gimlichael <mmdevelopment@hotmail.com>
2017-01-25 00:20:19 +0100

    Simplified.

--- src/Cuemon.Core/CharConverterExtensions.cs
+++ src/Cuemon.Core/CharConverterExtensions.cs
@@ -10,34 +10,14 @@ namespace Cuemon
     public static class CharConverterExtensions
     {
         /// <summary>
-        /// Converts the given <see cref="Stream"/> to a char array starting from position 0 (when supported), using UTF-16 for the encoding preserving any preamble sequences.
-        /// </summary>
-        /// <param name="value">The <see cref="Stream"/> value to be converted.</param>
-        /// <returns>A sequence of characters equivalent to the <see cref="Stream"/> value.</returns>
-        public static char[] ToCharArray(this Stream value)
-        {
-            return CharConverter.FromStream(value);
-        }
-
-        /// <summary>
         /// Converts the given <see cref="Stream"/> to a char array starting from position 0 (when supported).
         /// </summary>
         /// <param name="value">The <see cref="Stream"/> value to be converted.</param>
         /// <param name="setup">The <see cref="EncodingOptions"/> which need to be configured.</param>
         /// <returns>A sequence of characters equivalent to the <see cref="Stream"/> value.</returns>
-        public static char[] ToCharArray(this Stream value, Action<EncodingOptions> setup)
+        public static char[] ToCharArray(this Stream value, Action<EncodingOptions> setup = null)
         {
             return CharConverter.FromStream(value, setup);
-        }
-
-        /// <summary>
-        /// Converts the given <see cref="String"/> to an equivalent sequence of characters using UTF-16 for the encoding and preserving any preamble sequences.
-        /// </summary>
-        /// <param name="value">The <see cref="String"/> value to be converted.</param>
-        /// <returns>A sequence of characters equivalent to the <see cref="String"/> value.</returns>
-        public static char[] ToCharArray(this string value)
-        {
-            return CharConverter.FromString(value);
         }
 
         /// <summary>
@@ -46,7 +26,7 @@ namespace Cuemon
         /// <param name="value">The <see cref="String"/> value to be converted.</param>
         /// <param name="setup">The <see cref="EncodingOptions"/> which need to be configured.</param>
         /// <returns>A sequence of characters equivalent to the <see cref="String"/> value.</returns>
-        public static char[] ToCharArray(this string value, Action<EncodingOptions> setup)
+        public static char[] ToCharArray(this string value, Action<EncodingOptions> setup = null)
         {
             return CharConverter.FromString(value, setup);
         }
