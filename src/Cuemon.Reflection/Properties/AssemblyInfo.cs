99bf502f07546c058ce5ea6864450a90f048d40f
gimlichael <mmdevelopment@hotmail.com>
2017-02-06 01:04:13 +0100

    InternalsVisibleTo

--- src/Cuemon.Reflection/Properties/AssemblyInfo.cs
+++ src/Cuemon.Reflection/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
 ﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Cuemon.Reflection")]
@@ -13,3 +14,4 @@ using System.Runtime.InteropServices;
 [assembly: Guid("400a8b90-69d3-473d-b025-cb6b977a78ff")]
 [assembly: AssemblyFileVersion("4.1.2017.280")]
 [assembly: AssemblyVersion("4.1.2017.0")]
+[assembly: InternalsVisibleTo("Cuemon.Integrity")]
