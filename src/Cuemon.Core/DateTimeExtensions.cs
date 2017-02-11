bd2c05cd4c0ba1d6b8998263df35098eff0153d8
gimlichael <mmdevelopment@hotmail.com>
2017-02-11 23:46:05 +0100

    New DateTime extensions

--- src/Cuemon.Core/DateTimeExtensions.cs
+++ src/Cuemon.Core/DateTimeExtensions.cs
@@ -42,5 +42,78 @@ namespace Cuemon
             if (value.Kind != kind) { value = DateTime.SpecifyKind(value, kind); }
             return value;
         }
+
+        /// <summary>
+        /// Determines whether the specified <paramref name="dt"/> is within range of <paramref name="min"/> and <paramref name="max"/>.
+        /// </summary>
+        /// <param name="dt">The <see cref="DateTime"/> to compare.</param>
+        /// <param name="min">The minimum value of <paramref name="dt"/>.</param>
+        /// <param name="max">The maximum value of <paramref name="dt"/>.</param>
+        /// <returns><c>true</c> if <paramref name="dt"/> is within the specified range of <paramref name="min"/> and <paramref name="max"/>; otherwise <c>false</c>.</returns>
+        public static bool IsWithinRange(this DateTime dt, DateTime min, DateTime max)
+        {
+            return Condition.IsWithinRange(dt, min, max);
+        }
+
+        /// <summary>
+        /// Determines whether the specified <paramref name="dt"/> is within <paramref name="range"/>.
+        /// </summary>
+        /// <param name="dt">The <see cref="DateTime"/> to compare.</param>
+        /// <param name="range">The <see cref="TimeRange"/> of <paramref name="dt"/>.</param>
+        /// <returns><c>true</c> if <paramref name="dt"/> is within the specified <paramref name="range"/>; otherwise <c>false</c>.</returns>
+        public static bool IsWithinRange(this DateTime dt, TimeRange range)
+        {
+            return dt.IsWithinRange(range.Start, range.End);
+        }
+
+        /// <summary>
+        /// Determines whether the specified <paramref name="dt"/> is within <see cref="DayParts.Night"/>.
+        /// </summary>
+        /// <param name="dt">The <see cref="DateTime"/> to compare.</param>
+        /// <returns><c>true</c> if <paramref name="dt"/> is within <see cref="DayParts.Night"/>; otherwise <c>false</c>.</returns>
+        public static bool IsTimeOfDayNight(this DateTime dt)
+        {
+            return dt.ToUtcKind().IsWithinRange(DayParts.Night.Range.Start, DayParts.Night.Range.End);
+        }
+
+        /// <summary>
+        /// Determines whether the specified <paramref name="dt"/> is within <see cref="DayParts.Morning"/>.
+        /// </summary>
+        /// <param name="dt">The <see cref="DateTime"/> to compare.</param>
+        /// <returns><c>true</c> if <paramref name="dt"/> is within <see cref="DayParts.Morning"/>; otherwise <c>false</c>.</returns>
+        public static bool IsTimeOfDayMorning(this DateTime dt)
+        {
+            return dt.ToUtcKind().IsWithinRange(DayParts.Morning.Range.Start, DayParts.Morning.Range.End);
+        }
+
+        /// <summary>
+        /// Determines whether the specified <paramref name="dt"/> is within <see cref="DayParts.Forenoon"/>.
+        /// </summary>
+        /// <param name="dt">The <see cref="DateTime"/> to compare.</param>
+        /// <returns><c>true</c> if <paramref name="dt"/> is within <see cref="DayParts.Forenoon"/>; otherwise <c>false</c>.</returns>
+        public static bool IsTimeOfDayForenoon(this DateTime dt)
+        {
+            return dt.ToUtcKind().IsWithinRange(DayParts.Forenoon.Range.Start, DayParts.Forenoon.Range.End);
+        }
+
+        /// <summary>
+        /// Determines whether the specified <paramref name="dt"/> is within <see cref="DayParts.Afternoon"/>.
+        /// </summary>
+        /// <param name="dt">The <see cref="DateTime"/> to compare.</param>
+        /// <returns><c>true</c> if <paramref name="dt"/> is within <see cref="DayParts.Afternoon"/>; otherwise <c>false</c>.</returns>
+        public static bool IsTimeOfDayAfternoon(this DateTime dt)
+        {
+            return dt.ToUtcKind().IsWithinRange(DayParts.Afternoon.Range.Start, DayParts.Afternoon.Range.End);
+        }
+
+        /// <summary>
+        /// Determines whether the specified <paramref name="dt"/> is within <see cref="DayParts.Evening"/>.
+        /// </summary>
+        /// <param name="dt">The <see cref="DateTime"/> to compare.</param>
+        /// <returns><c>true</c> if <paramref name="dt"/> is within <see cref="DayParts.Evening"/>; otherwise <c>false</c>.</returns>
+        public static bool IsTimeOfDayEvening(this DateTime dt)
+        {
+            return dt.ToUtcKind().IsWithinRange(DayParts.Evening.Range.Start, DayParts.Evening.Range.End);
+        }
     }
 }
