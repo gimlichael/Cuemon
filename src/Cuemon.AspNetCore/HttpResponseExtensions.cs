ad54371a94b1e4d9afb5175dc075597748af4773
gimlichael <mmdevelopment@hotmail.com>
2017-02-11 23:51:43 +0100

    New extension for HttpResponse.

---
+++ src/Cuemon.AspNetCore/HttpResponseExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Cuemon.AspNetCore
+{
+    /// <summary>
+    /// Extension methods for the <see cref="HttpResponse"/> class.
+    /// </summary>
+    public static class HttpResponseExtensions
+    {
+        /// <summary>
+        /// Determines whether the HTTP response was successful.
+        /// </summary>
+        /// <param name="response">An instance of the <see cref="HttpResponse"/> object.</param>
+        /// <returns><c>true</c> if <see cref="HttpResponse.StatusCode"/> was in the <b>Successful</b> range (200-299); otherwise, <c>false</c>.</returns>
+        public static bool IsSuccessStatusCode(this HttpResponse response)
+        {
+            return (response.StatusCode >= 200 && response.StatusCode <= 299);
+        }
+    }
+}
