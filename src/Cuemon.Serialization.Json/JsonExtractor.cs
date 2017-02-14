4e54bd67bb49b1d46371776d48df01ee6c66cf90
gimlichael <mmdevelopment@hotmail.com>
2017-02-14 01:01:00 +0100

    Added a generic (but not complete) way to parse a Json object.

---
+++ src/Cuemon.Serialization.Json/JsonExtractor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Cuemon.Serialization.Json
+{
+
+    internal class JsonExtractor<T>
+    {
+        internal JsonExtractor(string propertyNames, Action<T, object[]> parser)
+        {
+            Validator.ThrowIfNullOrWhitespace(propertyNames, nameof(propertyNames));
+            Validator.ThrowIfNull(parser, nameof(parser));
+            PropertyNames = propertyNames.Split(',');
+            Parser = parser;
+        }
+
+        internal JsonExtractor(string[] propertyNames, Action<T, object[]> parser)
+        {
+            Validator.ThrowIfNull(propertyNames, nameof(propertyNames));
+            Validator.ThrowIfNull(parser, nameof(parser));
+            PropertyNames = propertyNames;
+            Parser = parser;
+        }
+
+        internal string[] PropertyNames { get; }
+
+        internal Action<T, object[]> Parser { get; }
+    }
+}
