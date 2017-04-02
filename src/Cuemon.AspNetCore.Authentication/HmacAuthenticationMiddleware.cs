02af3a56b4804d3a614ce11d3210cf1035e530d7
gimlichael <mmdevelopment@hotmail.com>
2017-04-02 16:54:30 +0200

    Added HmacAuthentication middleware and related classes.

---
+++ src/Cuemon.AspNetCore.Authentication/HmacAuthenticationMiddleware.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Cuemon.Security.Cryptography;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Net.Http.Headers;
+
+namespace Cuemon.AspNetCore.Authentication
+{
+    /// <summary>
+    /// Provides a HTTP HMAC Authentication middleware implementation for ASP.NET Core.
+    /// </summary>
+    public class HmacAuthenticationMiddleware : Middleware<HmacAuthenticationOptions>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="HmacAuthenticationMiddleware"/> class.
+        /// </summary>
+        /// <param name="next">The delegate of the request pipeline to invoke.</param>
+        /// <param name="setup">The middleware <see cref="HmacAuthenticationOptions"/> which need to be configured.</param>
+        public HmacAuthenticationMiddleware(RequestDelegate next, Action<HmacAuthenticationOptions> setup)
+            : base(next, setup)
+        {
+        }
+
+        /// <summary>
+        /// Executes the <see cref="HmacAuthenticationMiddleware" />.
+        /// </summary>
+        /// <param name="context">The context of the current request.</param>
+        /// <returns>A task that represents the execution of this middleware.</returns>
+        public override async Task Invoke(HttpContext context)
+        {
+            if (!AuthenticationUtility.TryAuthenticate(context, Options.RequireSecureConnection, AuthorizationHeaderParser, TryAuthenticate))
+            {
+                context.Response.StatusCode = AuthenticationUtility.HttpNotAuthorizedStatusCode;
+                context.Response.Headers.Add(HeaderNames.WWWAuthenticate, "{0}".FormatWith(Options.AuthenticationScheme));
+                await context.WriteHttpNotAuthorizedBody(Options.HttpNotAuthorizedBody).ConfigureAwait(false);
+                return;
+            }
+            await Next.Invoke(context).ConfigureAwait(false);
+        }
+
+        private bool TryAuthenticate(HttpContext context, Template<string, string> credentials, out ClaimsPrincipal result)
+        {
+            if (Options.Authenticator == null) { throw new InvalidOperationException("The {0} cannot be null.".FormatWith(nameof(Options.Authenticator))); }
+            var requestBodyMd5 = context.Request.Headers[HeaderNames.ContentMD5].FirstOrDefault()?.ComputeHash(HashAlgorithmType.MD5, Encoding.UTF8).ToHexadecimal();
+            if (!requestBodyMd5.IsNullOrWhiteSpace() && !context.Request.Body.ComputeHash(HashAlgorithmType.MD5).ToHexadecimal().Equals(requestBodyMd5, StringComparison.Ordinal))
+            {
+                result = null;
+                return false;
+            }
+            var publicKey = credentials.Arg1;
+            var signature = credentials.Arg2;
+            var stringToSign = Options.MessageDescriptor(context);
+            byte[] privateKey = new byte[0];
+            result = Options?.Authenticator(publicKey, out privateKey);
+            var computedSignature = Options?.HmacSigner(new HmacAuthenticationParameters(Options.Algorithm, privateKey, stringToSign))?.ToBase64();
+            return signature.Equals(computedSignature, StringComparison.Ordinal) && Condition.IsNotNull(result);
+        }
+
+        private Template<string, string> AuthorizationHeaderParser(HttpContext context, string authorizationHeader)
+        {
+            if (AuthenticationUtility.IsAuthenticationSchemeValid(authorizationHeader, Options.AuthenticationScheme))
+            {
+                var credentials = authorizationHeader.Remove(0, Options.AuthenticationScheme.Length + 1).Split(':');
+                var publicKey = credentials?[0];
+                var signature = credentials?[1];
+                if (!publicKey.IsNullOrWhiteSpace() && !signature.IsNullOrWhiteSpace()) { return TupleUtility.CreateTwo(publicKey, signature); }
+            }
+            return null;
+        }
+    }
+
+    /// <summary>
+    /// This is a factory implementation of the <see cref="HmacAuthenticationMiddleware"/> class.
+    /// </summary>
+    public static class HmacAuthenticationBuilderExtension
+    {
+        /// <summary>
+        /// Adds a HTTP Basic Authentication scheme to the <see cref="IApplicationBuilder"/> request execution pipeline.
+        /// </summary>
+        /// <param name="builder">The type that provides the mechanisms to configure an application’s request pipeline.</param>
+        /// <param name="setup">The HTTP <see cref="HmacAuthenticationOptions"/> middleware which need to be configured.</param>
+        /// <returns>A reference to this instance after the operation has completed.</returns>
+        public static IApplicationBuilder UseHmacAuthentication(this IApplicationBuilder builder, Action<HmacAuthenticationOptions> setup)
+        {
+            return builder.UseMiddleware<HmacAuthenticationMiddleware>(setup);
+        }
+    }
+}
---
+++ src/Cuemon.AspNetCore.Authentication/HmacAuthenticationOptions.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using Cuemon.Security.Cryptography;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.Net.Http.Headers;
+
+namespace Cuemon.AspNetCore.Authentication
+{
+    /// <summary>
+    /// Configuration options for <see cref="HmacAuthenticationMiddleware"/>. This class cannot be inherited.
+    /// </summary>
+    /// <seealso cref="AuthenticationOptions" />
+    public sealed class HmacAuthenticationOptions : AuthenticationOptions
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="HmacAuthenticationOptions"/> class.
+        /// </summary>
+        public HmacAuthenticationOptions()
+        {
+            AuthenticationScheme = "HMAC";
+            Algorithm = HmacAlgorithmType.SHA1;
+            MessageDescriptor = context => "{0}:{1}:{2}:{3}:{4}:{5}".FormatWith(
+                context.Request.Method,
+                context.Request.GetDisplayUrl(),
+                context.Request.Headers[HeaderNames.ContentMD5].FirstOrDefault(),
+                context.Request.Headers[HeaderNames.ContentType].FirstOrDefault(),
+                context.Request.Headers[HeaderNames.Date].FirstOrDefault(),
+                context.Request.Headers[HeaderNames.UserAgent].FirstOrDefault());
+            HmacSigner = parameters => HmacUtility.ComputeKeyedHash(parameters.Message, parameters.PrivateKey, parameters.Algorithm, Encoding.UTF8).Value;
+        }
+
+        /// <summary>
+        /// Gets the name of the authentication scheme. Default is "HMAC".
+        /// </summary>
+        /// <value>The name of the authentication scheme.</value>
+        public string AuthenticationScheme { get; set; }
+
+        /// <summary>
+        /// Gets or sets the algorithm of the HMAC Authentication. Default is <see cref="HmacAlgorithmType.SHA1"/>.
+        /// </summary>
+        /// <value>The algorithm of the HMAC Authentication.</value>
+        public HmacAlgorithmType Algorithm { get; set; }
+
+        /// <summary>
+        /// Gets or sets the function delegate that provides information about the message to be signed.
+        /// </summary>
+        /// <value>The function delegate that provides information about the message to be signed.</value>
+        public Func<HttpContext, string> MessageDescriptor { get; set; }
+
+        /// <summary>
+        /// Gets or sets the function delegate that will perform the authentication from the specified <c>publicKey</c>.
+        /// </summary>
+        /// <value>The function delegate that will perform the authentication.</value>
+        public HmacAuthenticator Authenticator { get; set; }
+
+        /// <summary>
+        /// Gets or sets the function delegate that will sign a message retrieved by <see cref="MessageDescriptor"/>.
+        /// </summary>
+        /// <value>The function delegate that will sign a message.</value>
+        public Func<HmacAuthenticationParameters, byte[]> HmacSigner { get; set; }
+    }
+}
