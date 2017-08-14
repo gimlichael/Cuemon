af2fa1fcf18aaf156d14e7d6f0578610ca84c627
gimlichael <mmdevelopment@hotmail.com>
2017-08-14 11:39:04 +0200

    New extension method; GetValueOrDefault

--- src/Cuemon.Core/Collections/Generic/DictionaryExtensions.cs
+++ src/Cuemon.Core/Collections/Generic/DictionaryExtensions.cs
@@ -9,6 +9,37 @@ namespace Cuemon.Collections.Generic
     public static class DictionaryExtensions
     {
         /// <summary>
+        /// Gets the value associated with the specified <paramref name="key"/> or <c>default(<typeparamref name="TValue"/>)</c> when the key does not exists in the <paramref name="dictionary"/>.
+        /// </summary>
+        /// <typeparam name="TKey">The type of the keys in the <paramref name="dictionary"/>.</typeparam>
+        /// <typeparam name="TValue">The type of the values in the <paramref name="dictionary"/>.</typeparam>
+        /// <param name="dictionary">The dictionary to extend.</param>
+        /// <param name="key">The key of the value to get.</param>
+        /// <returns>Either the value associated with the specified <paramref name="key"/> or a default value through <paramref name="defaultProvider"/> when the key does not exists.</returns>
+        public static TValue GetValueOrDefault<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key)
+        {
+            return GetValueOrDefault(dictionary, key, () => default(TValue));
+        }
+
+        /// <summary>
+        /// Gets the value associated with the specified <paramref name="key"/> or a default value through <paramref name="defaultProvider"/> when the key does not exists in the <paramref name="dictionary"/>.
+        /// </summary>
+        /// <typeparam name="TKey">The type of the keys in the <paramref name="dictionary"/>.</typeparam>
+        /// <typeparam name="TValue">The type of the values in the <paramref name="dictionary"/>.</typeparam>
+        /// <param name="dictionary">The dictionary to extend.</param>
+        /// <param name="key">The key of the value to get.</param>
+        /// <param name="defaultProvider">The function delegate that will provide a default value when the <paramref name="key"/> does not exists in the <paramref name="dictionary"/>.</param>
+        /// <returns>Either the value associated with the specified <paramref name="key"/> or a default value through <paramref name="defaultProvider"/> when the key does not exists.</returns>
+        public static TValue GetValueOrDefault<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key, Func<TValue> defaultProvider)
+        {
+            Validator.ThrowIfNull(dictionary, nameof(dictionary));
+            Validator.ThrowIfNull(key, nameof(key));
+            Validator.ThrowIfNull(defaultProvider, nameof(defaultProvider));
+            TValue value;
+            return dictionary.TryGetValue(key, out value) ? value : defaultProvider();
+        }
+
+        /// <summary>
         /// Gets the <paramref name="value"/> associated with the specified <paramref name="key"/>.
         /// </summary>
         /// <typeparam name="TKey">The type of the keys in the <paramref name="dictionary"/>.</typeparam>
