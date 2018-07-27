8a30271e0574984d61c9b817b3e085ff8244d149
gimlichael <mmdevelopment@hotmail.com>
2018-07-27 06:46:20 +0200

    4.2.2018.840 --> 4.2.2018.2050

--- src/Cuemon.Net/Properties/AssemblyInfo.cs
+++ src/Cuemon.Net/Properties/AssemblyInfo.cs
@@ -2,14 +2,14 @@
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Cuemon.Net")]
-[assembly: AssemblyDescription("The Cuemon.Net assembly is a member of the Cuemon .NET Core family and provides enhancements to the System.Net namespace and among those a versatile HttpManager that transparently promotes the HttpClient.")]
+[assembly: AssemblyDescription("The Cuemon.Net assembly is a member of the Cuemon .NET Standard family and provides enhancements to the System.Net namespace and among those a versatile HttpManager that transparently promotes the HttpClient.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Weubphoria")]
-[assembly: AssemblyProduct("Cuemon .NET Core")]
+[assembly: AssemblyProduct("Cuemon .NET Standard")]
 [assembly: AssemblyCopyright("Copyright © Weubphoria 2009-2018. All rights reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("67976fe9-081d-46e7-8272-3afe85ecef09")]
-[assembly: AssemblyFileVersion("4.2.2018.840")]
+[assembly: AssemblyFileVersion("4.2.2018.2050")]
 [assembly: AssemblyVersion("4.2.2017.0")]
