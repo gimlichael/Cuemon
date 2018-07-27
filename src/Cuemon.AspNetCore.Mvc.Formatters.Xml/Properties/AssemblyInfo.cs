8e4819ffc2112e0c44d6c15d8b35cad5bf532490
gimlichael <mmdevelopment@hotmail.com>
2018-07-27 06:28:13 +0200

    Updated to .NET Standard 2.0 due to breaking changes in Microsoft.AspNetCore.Http.Abstractions by Microsoft.

--- src/Cuemon.AspNetCore.Mvc.Formatters.Xml/Properties/AssemblyInfo.cs
+++ src/Cuemon.AspNetCore.Mvc.Formatters.Xml/Properties/AssemblyInfo.cs
@@ -2,14 +2,14 @@
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Cuemon.AspNetCore.Mvc.Formatters.Xml")]
-[assembly: AssemblyDescription("The Cuemon.AspNetCore.Mvc.Formatters.Xml assembly is a stand-alone member of the Cuemon .NET Core family and serves as a supplement to the Microsoft ASP.NET Core MVC.")]
+[assembly: AssemblyDescription("The Cuemon.AspNetCore.Mvc.Formatters.Xml assembly is a stand-alone member of the Cuemon .NET Standard family and serves as a supplement to the Microsoft ASP.NET Core MVC.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Weubphoria")]
-[assembly: AssemblyProduct("Cuemon .NET Core")]
+[assembly: AssemblyProduct("Cuemon .NET Standard")]
 [assembly: AssemblyCopyright("Copyright © Weubphoria 2009-2018. All rights reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("001e3753-2e46-4c7c-9612-3623a9c99bd5")]
-[assembly: AssemblyFileVersion("4.2.2018.840")]
+[assembly: AssemblyFileVersion("4.2.2018.2050")]
 [assembly: AssemblyVersion("4.2.2017.0")]
