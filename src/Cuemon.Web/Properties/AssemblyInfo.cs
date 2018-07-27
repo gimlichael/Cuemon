dd4a6b2ead236f2ee83266b517dae220692eafae
gimlichael <mmdevelopment@hotmail.com>
2018-07-27 19:06:21 +0200

    4.2.2018.840 --> 4.2.2018.2050

--- src/Cuemon.Web/Properties/AssemblyInfo.cs
+++ src/Cuemon.Web/Properties/AssemblyInfo.cs
@@ -2,14 +2,14 @@
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Cuemon.Web")]
-[assembly: AssemblyDescription("The Cuemon.Web assembly is a member of the Cuemon .NET Core family and provides enhancements to the now deprecated System.Web namespace.")]
+[assembly: AssemblyDescription("The Cuemon.Web assembly is a member of the Cuemon .NET Standard family and provides enhancements to the now deprecated System.Web namespace.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Weubphoria")]
-[assembly: AssemblyProduct("Cuemon .NET Core")]
+[assembly: AssemblyProduct("Cuemon .NET Standard")]
 [assembly: AssemblyCopyright("Copyright © Weubphoria 2009-2018. All rights reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("fe4cd252-48aa-40b7-9389-a448cd522edd")]
-[assembly: AssemblyFileVersion("4.2.2018.840")]
+[assembly: AssemblyFileVersion("4.2.2018.2050")]
 [assembly: AssemblyVersion("4.2.2017.0")]
