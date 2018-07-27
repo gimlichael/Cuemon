26f5637d76659a07a9b684d830bd9ba3e04e0af3
gimlichael <mmdevelopment@hotmail.com>
2018-07-27 06:58:21 +0200

    4.2.2018.840 --> 4.2.2018.2050

--- src/Cuemon.Runtime/Properties/AssemblyInfo.cs
+++ src/Cuemon.Runtime/Properties/AssemblyInfo.cs
@@ -2,14 +2,14 @@
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Cuemon.Runtime")]
-[assembly: AssemblyDescription("The Cuemon.Runtime assembly is a member of the Cuemon .NET Core family and provides enhancements to the System.Runtime namespace.")]
+[assembly: AssemblyDescription("The Cuemon.Runtime assembly is a member of the Cuemon .NET Standard family and provides enhancements to the System.Runtime namespace.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Weubphoria")]
-[assembly: AssemblyProduct("Cuemon .NET Core")]
+[assembly: AssemblyProduct("Cuemon .NET Standard")]
 [assembly: AssemblyCopyright("Copyright © Weubphoria 2009-2018. All rights reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("ddf69535-62f4-450d-9e51-8c84604bc141")]
-[assembly: AssemblyFileVersion("4.2.2018.840")]
+[assembly: AssemblyFileVersion("4.2.2018.2050")]
 [assembly: AssemblyVersion("4.2.2017.0")]
