3bebd2f707b8911b8e03fa64fb1b35bbbe66b694
gimlichael <mmdevelopment@hotmail.com>
2018-07-27 06:59:27 +0200

    4.2.2018.840 --> 4.2.2018.2050

--- src/Cuemon.Runtime.Caching/Properties/AssemblyInfo.cs
+++ src/Cuemon.Runtime.Caching/Properties/AssemblyInfo.cs
@@ -2,14 +2,14 @@
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Cuemon.Runtime.Caching")]
-[assembly: AssemblyDescription("The Cuemon.Runtime.Caching assembly is a member of the Cuemon .NET Core family and provides a versatile way to gain access to caching in an application.")]
+[assembly: AssemblyDescription("The Cuemon.Runtime.Caching assembly is a member of the Cuemon .NET Standard family and provides a versatile way to gain access to caching in an application.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Weubphoria")]
-[assembly: AssemblyProduct("Cuemon .NET Core")]
+[assembly: AssemblyProduct("Cuemon .NET Standard")]
 [assembly: AssemblyCopyright("Copyright © Weubphoria 2009-2018. All rights reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("31a36935-dcc9-43d5-9478-580b8ecc672d")]
-[assembly: AssemblyFileVersion("4.2.2018.840")]
+[assembly: AssemblyFileVersion("4.2.2018.2050")]
 [assembly: AssemblyVersion("4.2.2017.0")]
