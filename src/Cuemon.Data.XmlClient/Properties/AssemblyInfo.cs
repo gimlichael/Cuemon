1a9facfc9145b0f122323c9e1edaf50d88696877
gimlichael <mmdevelopment@hotmail.com>
2018-07-27 06:36:38 +0200

    4.2.2018.840 --> 4.2.2018.2050

--- src/Cuemon.Data.XmlClient/Properties/AssemblyInfo.cs
+++ src/Cuemon.Data.XmlClient/Properties/AssemblyInfo.cs
@@ -2,14 +2,14 @@
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Cuemon.Data.XmlClient")]
-[assembly: AssemblyDescription("The Cuemon.Data.XmlClient assembly is a member of the Cuemon .NET Core family and provides enhancements to the Cuemon.Data namespace.")]
+[assembly: AssemblyDescription("The Cuemon.Data.XmlClient assembly is a member of the Cuemon .NET Standard family and provides enhancements to the Cuemon.Data namespace.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Weubphoria")]
-[assembly: AssemblyProduct("Cuemon .NET Core")]
+[assembly: AssemblyProduct("Cuemon .NET Standard")]
 [assembly: AssemblyCopyright("Copyright © Weubphoria 2009-2018. All rights reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("34931d1f-06bd-4156-a6e2-38512b491b38")]
-[assembly: AssemblyFileVersion("4.2.2018.840")]
+[assembly: AssemblyFileVersion("4.2.2018.2050")]
 [assembly: AssemblyVersion("4.2.2017.0")]
