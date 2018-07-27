e41cea87571ed9deb3209ed86e2667dd1e138c8d
gimlichael <mmdevelopment@hotmail.com>
2018-07-27 06:57:21 +0200

    4.2.2018.840 --> 4.2.2018.2050

--- src/Cuemon.Reflection/Properties/AssemblyInfo.cs
+++ src/Cuemon.Reflection/Properties/AssemblyInfo.cs
@@ -1,16 +1,15 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Cuemon.Reflection")]
-[assembly: AssemblyDescription("The Cuemon.Reflection assembly is a member of the Cuemon .NET Core family and provides enhancements to the System.Reflection namespace.")]
+[assembly: AssemblyDescription("The Cuemon.Reflection assembly is a member of the Cuemon .NET Standard family and provides enhancements to the System.Reflection namespace.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Weubphoria")]
-[assembly: AssemblyProduct("Cuemon .NET Core")]
+[assembly: AssemblyProduct("Cuemon .NET Standard")]
 [assembly: AssemblyCopyright("Copyright © Weubphoria 2009-2018. All rights reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("400a8b90-69d3-473d-b025-cb6b977a78ff")]
-[assembly: AssemblyFileVersion("4.2.2018.840")]
+[assembly: AssemblyFileVersion("4.2.2018.2050")]
 [assembly: AssemblyVersion("4.2.2017.0")]
