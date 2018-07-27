d0d9b1321254dbfac5da46b959d2f2efc9808cea
gimlichael <mmdevelopment@hotmail.com>
2018-07-27 06:56:31 +0200

    4.2.2018.840 --> 4.2.2018.2050

--- src/Cuemon.Net.Mail/Properties/AssemblyInfo.cs
+++ src/Cuemon.Net.Mail/Properties/AssemblyInfo.cs
@@ -2,14 +2,14 @@
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Cuemon.Net.Mail")]
-[assembly: AssemblyDescription("The Cuemon.Net.Mail assembly is a stand-alone member of the Cuemon .NET Core family and provides ways to ship e-mails in batches.")]
+[assembly: AssemblyDescription("The Cuemon.Net.Mail assembly is a stand-alone member of the Cuemon .NET Standard family and provides ways to ship e-mails in batches.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Weubphoria")]
-[assembly: AssemblyProduct("Cuemon .NET Core")]
+[assembly: AssemblyProduct("Cuemon .NET Standard")]
 [assembly: AssemblyCopyright("Copyright © Weubphoria 2009-2018. All rights reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("ccd7313f-9918-492f-88f4-df186b66383f")]
-[assembly: AssemblyFileVersion("4.2.2018.840")]
+[assembly: AssemblyFileVersion("4.2.2018.2050")]
 [assembly: AssemblyVersion("4.2.2017.0")]
