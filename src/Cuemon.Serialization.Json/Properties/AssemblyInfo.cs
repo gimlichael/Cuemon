b2b83d27a34d0d0d39a5ddf1cdb3a0da7555ac16
gimlichael <mmdevelopment@hotmail.com>
2018-07-27 19:01:55 +0200

    4.2.2018.840 --> 4.2.2018.2050

--- src/Cuemon.Serialization.Json/Properties/AssemblyInfo.cs
+++ src/Cuemon.Serialization.Json/Properties/AssemblyInfo.cs
@@ -2,14 +2,14 @@
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Cuemon.Serialization.Json")]
-[assembly: AssemblyDescription("The Cuemon.Serialization.Json assembly is a stand-alone member of the Cuemon .NET Core family and provides JSON serialization using the highly acclaimed Newtonsoft.Json package.")]
+[assembly: AssemblyDescription("The Cuemon.Serialization.Json assembly is a stand-alone member of the Cuemon .NET Standard family and provides JSON serialization using the highly acclaimed Newtonsoft.Json package.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Weubphoria")]
-[assembly: AssemblyProduct("Cuemon .NET Core")]
+[assembly: AssemblyProduct("Cuemon .NET Standard")]
 [assembly: AssemblyCopyright("Copyright © Weubphoria 2009-2018. All rights reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("e3d2572e-8d77-47fe-9cf3-dba77c55f95a")]
-[assembly: AssemblyFileVersion("4.2.2018.840")]
+[assembly: AssemblyFileVersion("4.2.2018.2050")]
 [assembly: AssemblyVersion("4.2.2017.0")]
