1ddf94b95c4b6151ccf3cdec3a2146c4b7fd9660
gimlichael <mmdevelopment@hotmail.com>
2018-07-27 06:38:14 +0200

    4.2.2018.840 --> 4.2.2018.2050

--- src/Cuemon.Integrity/Properties/AssemblyInfo.cs
+++ src/Cuemon.Integrity/Properties/AssemblyInfo.cs
@@ -2,14 +2,14 @@
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Cuemon.Integrity")]
-[assembly: AssemblyDescription("The Cuemon.Integrity assembly is a member of the Cuemon .NET Core family and provides a way to represent cacheable data-centric content that can be validated by cache-aware applications.")]
+[assembly: AssemblyDescription("The Cuemon.Integrity assembly is a member of the Cuemon .NET Standard family and provides a way to represent cacheable data-centric content that can be validated by cache-aware applications.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Weubphoria")]
-[assembly: AssemblyProduct("Cuemon .NET Core")]
+[assembly: AssemblyProduct("Cuemon .NET Standard")]
 [assembly: AssemblyCopyright("Copyright © Weubphoria 2009-2018. All rights reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("9f90ae49-0d4a-45f0-b70b-422b59cb4c77")]
-[assembly: AssemblyFileVersion("4.2.2018.840")]
+[assembly: AssemblyFileVersion("4.2.2018.2050")]
 [assembly: AssemblyVersion("4.2.2017.0")]
