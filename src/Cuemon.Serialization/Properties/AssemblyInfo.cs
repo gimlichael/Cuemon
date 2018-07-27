cc9bf9b5c2ffce435aea09c0234f037f2ffba7e2
gimlichael <mmdevelopment@hotmail.com>
2018-07-27 19:00:21 +0200

    4.2.2018.840 --> 4.2.2018.2050

--- src/Cuemon.Serialization/Properties/AssemblyInfo.cs
+++ src/Cuemon.Serialization/Properties/AssemblyInfo.cs
@@ -2,14 +2,14 @@
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Cuemon.Serialization")]
-[assembly: AssemblyDescription("The Cuemon.Serialization assembly is a member of the Cuemon .NET Core family and provides generic base classes for serialization.")]
+[assembly: AssemblyDescription("The Cuemon.Serialization assembly is a member of the Cuemon .NET Standard family and provides generic base classes for serialization.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Weubphoria")]
-[assembly: AssemblyProduct("Cuemon .NET Core")]
+[assembly: AssemblyProduct("Cuemon .NET Standard")]
 [assembly: AssemblyCopyright("Copyright © Weubphoria 2009-2018. All rights reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("442b5eb8-7212-4cb4-aab2-9587b647486e")]
-[assembly: AssemblyFileVersion("4.2.2018.840")]
+[assembly: AssemblyFileVersion("4.2.2018.2050")]
 [assembly: AssemblyVersion("4.2.2017.0")]
