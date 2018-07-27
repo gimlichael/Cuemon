9daf6043f301f962f1b262cf6ed7c7d92fad89bb
gimlichael <mmdevelopment@hotmail.com>
2018-07-27 06:34:13 +0200

    4.2.2018.840 --> 4.2.2018.2050

--- src/Cuemon.Collections.Specialized/Properties/AssemblyInfo.cs
+++ src/Cuemon.Collections.Specialized/Properties/AssemblyInfo.cs
@@ -2,14 +2,14 @@
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Cuemon.Collections.Specialized")]
-[assembly: AssemblyDescription("The Cuemon.Collections.Specialized assembly is a member of the Cuemon .NET Core family and provides enhancements to the System.Collections.Specialized assembly.")]
+[assembly: AssemblyDescription("The Cuemon.Collections.Specialized assembly is a member of the Cuemon .NET Standard family and provides enhancements to the System.Collections.Specialized assembly.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Weubphoria")]
-[assembly: AssemblyProduct("Cuemon .NET Core")]
+[assembly: AssemblyProduct("Cuemon .NET Standard")]
 [assembly: AssemblyCopyright("Copyright © Weubphoria 2009-2018. All rights reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("db7945f1-4c3b-4aa2-bb99-6d67e651618b")]
-[assembly: AssemblyFileVersion("4.2.2018.840")]
+[assembly: AssemblyFileVersion("4.2.2018.2050")]
 [assembly: AssemblyVersion("4.2.2017.0")]
