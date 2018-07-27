34c18a12bbf3e66163a835befbc4f40f5e6b654a
gimlichael <mmdevelopment@hotmail.com>
2018-07-27 18:58:46 +0200

    4.2.2018.840 --> 4.2.2018.2050

--- src/Cuemon.Security/Properties/AssemblyInfo.cs
+++ src/Cuemon.Security/Properties/AssemblyInfo.cs
@@ -2,14 +2,14 @@
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Cuemon.Security")]
-[assembly: AssemblyDescription("The Cuemon.Security assembly is a member of the Cuemon .NET Core family and provides enhancements to the System.Security namespace.")]
+[assembly: AssemblyDescription("The Cuemon.Security assembly is a member of the Cuemon .NET Standard family and provides enhancements to the System.Security namespace.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Weubphoria")]
-[assembly: AssemblyProduct("Cuemon .NET Core")]
+[assembly: AssemblyProduct("Cuemon .NET Standard")]
 [assembly: AssemblyCopyright("Copyright © Weubphoria 2009-2018. All rights reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("4028a482-40f9-489b-a9b9-93364072f678")]
-[assembly: AssemblyFileVersion("4.2.2018.840")]
+[assembly: AssemblyFileVersion("4.2.2018.2050")]
 [assembly: AssemblyVersion("4.2.2017.0")]
