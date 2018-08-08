0dc5efca69533a0ea63e3237c0082c7b93c2e3d0
gimlichael <mmdevelopment@hotmail.com>
2018-08-08 22:30:34 +0200

    Added AsyncOptions.

---
+++ src/Cuemon.Core/Threading/AsyncOptions.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading;
+
+namespace Cuemon.Threading
+{
+    /// <summary>
+    /// Specifies options that is related to asynchronous operations.
+    /// </summary>
+    public class AsyncOptions
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AsyncOptions"/> class.
+        /// </summary>
+        public AsyncOptions()
+        {
+            CancellationToken = default(CancellationToken);
+        }
+
+        /// <summary>
+        /// Gets or sets the cancellation token of an asynchronous operations.
+        /// </summary>
+        /// <value>The cancellation token of an asynchronous operations.</value>
+        public CancellationToken CancellationToken { get; set; }
+    }
+}
