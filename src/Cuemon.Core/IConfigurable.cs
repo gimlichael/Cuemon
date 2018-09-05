abb0dd01303c32c3384425f95ae6f1c23fdc7bea
gimlichael <mmdevelopment@hotmail.com>
2018-09-05 20:36:08 +0200

    Provides a generic way to support the options pattern on a class level.

---
+++ src/Cuemon.Core/IConfigurable.cs
@@ -0,0 +1,15 @@
+﻿namespace Cuemon
+{
+    /// <summary>
+    /// Provides a generic way to support the options pattern on a class level.
+    /// </summary>
+    /// <typeparam name="TOptions">The type of the configured options.</typeparam>
+    public interface IConfigurable<out TOptions> where TOptions : class, new()
+    {
+        /// <summary>
+        /// Gets the configured options of this instance.
+        /// </summary>
+        /// <value>The configured options of this instance.</value>
+        TOptions Options { get; }
+    }
+}
