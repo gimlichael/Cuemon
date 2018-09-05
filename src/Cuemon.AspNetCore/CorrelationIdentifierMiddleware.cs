addaf856a388bb25b7db9a7c333ffab2b0e8db05
gimlichael <mmdevelopment@hotmail.com>
2018-09-06 00:43:28 +0200

    Added a way to provides a Correlation ID middleware implementation for ASP.NET Core.

---
+++ src/Cuemon.AspNetCore/CorrelationIdentifierMiddleware.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Threading.Tasks;
+using Cuemon.AspNetCore.Builder;
+using Cuemon.Collections.Generic;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
+
+namespace Cuemon.AspNetCore
+{
+    /// <summary>
+    /// Provides a Correlation ID middleware implementation for ASP.NET Core.
+    /// </summary>
+    public class CorrelationIdentifierMiddleware : ConfigurableMiddleware<CorrelationIdentifierOptions>
+    {
+        /// <summary>
+        /// The key from where the Correlation ID is stored throughout the request scope.
+        /// </summary>
+        public const string HttpContextItemsKey = "Cuemon.AspNetCore.CorrelationMiddleware";
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CorrelationIdentifierMiddleware"/> class.
+        /// </summary>
+        /// <param name="next">The delegate of the request pipeline to invoke.</param>
+        /// <param name="setup">The <see cref="CorrelationIdentifierOptions" /> which need to be configured.</param>
+        public CorrelationIdentifierMiddleware(RequestDelegate next, IOptions<CorrelationIdentifierOptions> setup) : base(next, setup)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CorrelationIdentifierMiddleware"/> class.
+        /// </summary>
+        /// <param name="next">The delegate of the request pipeline to invoke.</param>
+        /// <param name="setup">The <see cref="CorrelationIdentifierOptions"/> which need to be configured.</param>
+        public CorrelationIdentifierMiddleware(RequestDelegate next, Action<CorrelationIdentifierOptions> setup) : base(next, setup)
+        {
+        }
+
+        /// <summary>
+        /// Executes the <see cref="CorrelationIdentifierMiddleware" />.
+        /// </summary>
+        /// <param name="context">The context of the current request.</param>
+        /// <returns>A task that represents the execution of this middleware.</returns>
+        public override Task InvokeAsync(HttpContext context)
+        {
+            if (!context.Request.Headers.TryGetValue(Options.HeaderName, out var correlationId)) { correlationId = Options.CorrelationProvider().CorrelationId; }
+            context.Items.AddIfNotContainsKey(HttpContextItemsKey, correlationId);
+            context.Response.OnStarting(() =>
+            {
+                if (!context.Response.Headers.ContainsKey(Options.HeaderName))
+                {
+                    context.Response.Headers.AddOrUpdate(Options.HeaderName, correlationId);
+                }
+                return Task.CompletedTask;
+            });
+            return Next(context);
+        }
+    }
+
+    /// <summary>
+    /// This is a factory implementation of the <see cref="CorrelationIdentifierMiddleware"/> class.
+    /// </summary>
+    public static class CorrelationBuilderExtension
+    {
+        /// <summary>
+        /// Adds a correlation identifier HTTP header to the <see cref="IApplicationBuilder"/> request execution pipeline.
+        /// </summary>
+        /// <param name="builder">The type that provides the mechanisms to configure an application’s request pipeline.</param>
+        /// <param name="setup">The <see cref="CorrelationIdentifierOptions"/> middleware which need to be configured.</param>
+        /// <returns>A reference to this instance after the operation has completed.</returns>
+        public static IApplicationBuilder UseCorrelationIdentifierHeader(this IApplicationBuilder builder, Action<CorrelationIdentifierOptions> setup = null)
+        {
+            return ApplicationBuilderFactory.UseMiddlewareConfigurable<CorrelationIdentifierMiddleware, CorrelationIdentifierOptions>(builder, setup);
+        }
+    }
+}
---
+++ src/Cuemon.AspNetCore/CorrelationIdentifierOptions.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Cuemon.Messaging;
+
+namespace Cuemon.AspNetCore
+{
+    /// <summary>
+    /// Configuration options for <see cref="CorrelationIdentifierMiddleware"/>.
+    /// </summary>
+    public class CorrelationIdentifierOptions
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CorrelationIdentifierOptions"/> class.
+        /// </summary>
+        /// <remarks>
+        /// The following table shows the initial property values for an instance of <see cref="CorrelationIdentifierOptions"/>.
+        /// <list type="table">
+        ///     <listheader>
+        ///         <term>Property</term>
+        ///         <description>Initial Value</description>
+        ///     </listheader>
+        ///     <item>
+        ///         <term><see cref="HeaderName"/></term>
+        ///         <description><c>X-Correlation-ID</c></description>
+        ///     </item>
+        ///     <item>
+        ///         <term><see cref="CorrelationProvider"/></term>
+        ///         <description><c>DynamicCorrelation.Create(Guid.NewGuid().ToString("N")</c></description>
+        ///     </item>
+        /// </list>
+        /// </remarks>
+        public CorrelationIdentifierOptions()
+        {
+            HeaderName = "X-Correlation-ID";
+            CorrelationProvider = () => DynamicCorrelation.Create(Guid.NewGuid().ToString("N"));
+        }
+
+        /// <summary>
+        /// Gets or sets the name of the correlation identifier HTTP header.
+        /// </summary>
+        /// <value>The name of the correlation identifier HTTP header.</value>
+        public string HeaderName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the function delegate that provides the correlation implementation.
+        /// </summary>
+        /// <value>The function delegate that provides the correlation implementation.</value>
+        public Func<ICorrelation> CorrelationProvider { get; set; }
+    }
+}
