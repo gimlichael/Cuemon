86f7a9bb6ee35727bf3d8027d20a67896bbebae7
gimlichael <mmdevelopment@hotmail.com>
2018-09-09 17:06:30 +0200

    Added the classes that binds it all together in terms of making any given object cacheable for HTTP transfer. The primare binder for this lies in the extension methods on CacheableObjectResultExtensions.

---
+++ src/Cuemon.AspNetCore.Mvc/Filters/Cacheable/ContentTimeBasedObjectResult.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Cuemon.Integrity;
+
+namespace Cuemon.AspNetCore.Mvc.Filters.Cacheable
+{
+    /// <summary>
+    /// Provides a content and time based object result that is processed by both an HTTP ETag filter- and a Last-Modified filter implementation.
+    /// </summary>
+    /// <typeparam name="T">The type of the object to make cacheable.</typeparam>
+    /// <seealso cref="CacheableObjectResult{T, ContentBasedOptions}" />
+    /// <seealso cref="ICacheableIntegrity" />
+    /// <seealso cref="HttpLastModifiedHeader"/>
+    /// <seealso cref="HttpEntityTagHeader"/>
+    /// <seealso cref="HttpCacheableFilter"/>
+    public class ContentTimeBasedObjectResult<T> :  CacheableObjectResult<T, ContentTimeBasedOptions>, ICacheableEntity
+    {
+        internal ContentTimeBasedObjectResult(T instance, DateTime created, byte[] checksum = null, Action<ContentTimeBasedOptions> setup = null) : base(instance, setup)
+        {
+            var options = setup.ConfigureOptions();
+            Created = created;
+            Checksum = new ChecksumResult(checksum);
+            Modified = options.Modified;
+            Validation = options.GetValidation(checksum);
+        }
+
+        /// <summary>
+        /// Gets a <see cref="DateTime" /> value from when data this instance represents was first created, expressed as the Coordinated Universal Time (UTC).
+        /// </summary>
+        /// <value>The timestamp from when data this instance represents was first created.</value>
+        public DateTime Created { get; set; }
+
+        /// <summary>
+        /// Gets a <see cref="DateTime" /> value from when data this instance represents was last modified, expressed as the Coordinated Universal Time (UTC).
+        /// </summary>
+        /// <value>The timestamp from when data this instance represents was last modified.</value>
+        public DateTime? Modified { get; set; }
+
+        /// <summary>
+        /// Gets the validation strength of the integrity of this instance.
+        /// </summary>
+        /// <value>The validation strength of the integrity of this instance.</value>
+        public ChecksumStrength Validation { get; set; }
+
+        /// <summary>
+        /// Gets a <see cref="ChecksumResult" /> that represents the integrity of this instance.
+        /// </summary>
+        /// <value>The checksum that represents the integrity of this instance.</value>
+        public ChecksumResult Checksum { get; set; }
+
+        /// <summary>
+        /// Gets a value indicating whether this instance has a <see cref="Checksum" /> representation.
+        /// </summary>
+        /// <value><c>true</c> if this instance has a <see cref="Checksum" /> representation; otherwise, <c>false</c>.</value>
+        public bool HasChecksum => Validation != ChecksumStrength.None;
+    }
+}
