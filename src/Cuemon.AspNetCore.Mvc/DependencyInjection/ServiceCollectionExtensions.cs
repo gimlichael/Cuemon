521e09139fa4891e40b77c3ac03e413f3f04483a
gimlichael <mmdevelopment@hotmail.com>
2018-09-09 15:14:07 +0200

    Refactored extension methods to be more DI-friendly.

--- src/Cuemon.AspNetCore.Mvc/DependencyInjection/ServiceCollectionExtensions.cs
+++ src/Cuemon.AspNetCore.Mvc/DependencyInjection/ServiceCollectionExtensions.cs
@@ -1,7 +1,7 @@
 ﻿using Cuemon.AspNetCore.Mvc.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace Cuemon.AspNetCore.Mvc
+namespace Cuemon.AspNetCore.Mvc.DependencyInjection
 {
     /// <summary>
     /// Extension methods for the <see cref="IServiceCollection"/> interface.
@@ -15,22 +15,18 @@ namespace Cuemon.AspNetCore.Mvc
         /// <returns>An <see cref="IServiceCollection"/> that can be used to further configure other services.</returns>
         public static IServiceCollection AddCacheBusting(this IServiceCollection services)
         {
-            return AddCacheBusting(services, new AssemblyCacheBusting());
+            return services.AddCacheBusting<AssemblyCacheBusting>();
         }
 
         /// <summary>
         /// Adds a cache-busting service to the specified <see cref="IServiceCollection"/>.
         /// </summary>
         /// <param name="services">The <see cref="IServiceCollection"/> to add services to.</param>
-        /// <param name="cacheBusting">The <see cref="CacheBusting"/> service implementation of your choice.</param>
         /// <returns>An <see cref="IServiceCollection"/> that can be used to further configure other services.</returns>
-        public static IServiceCollection AddCacheBusting<T>(this IServiceCollection services, T cacheBusting = null)
-            where T : CacheBusting
+        public static IServiceCollection AddCacheBusting<T>(this IServiceCollection services) where T : CacheBusting
         {
             Validator.ThrowIfNull(services, nameof(services));
-            Validator.ThrowIfNull(cacheBusting, nameof(cacheBusting));
-
-            services.AddSingleton(cacheBusting as CacheBusting);
+            services.AddSingleton<ICacheBusting, T>();
             return services;
         }
     }
