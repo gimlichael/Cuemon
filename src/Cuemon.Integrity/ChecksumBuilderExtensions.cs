1dbcfb206e536ef5e6125aad115ac5f0f487da21
gimlichael <mmdevelopment@hotmail.com>
2018-09-09 14:59:43 +0200

    Moved all CombineWith methods away from ChecksumBuilder and added them as generic extension methods. This adds to the flexibility.

---
+++ src/Cuemon.Integrity/ChecksumBuilderExtensions.cs
@@ -0,0 +1,146 @@
+﻿using System.Collections.Generic;
+
+namespace Cuemon.Integrity
+{
+    /// <summary>
+    /// Extension methods for the <see cref="ChecksumBuilder"/>.
+    /// </summary>
+    public static class ChecksumBuilderExtensions
+    {
+        /// <summary>
+        /// Combines the <paramref name="additionalChecksum"/> to the representation of this instance.
+        /// </summary>
+        /// <typeparam name="T">The type of the <see cref="ChecksumBuilder"/>.</typeparam>
+        /// <param name="cb">An instance of a <see cref="ChecksumBuilder"/>.</param>
+        /// <param name="additionalChecksum">A <see cref="double"/> array that contains zero or more checksum of the additional data this instance must represent.</param>
+        /// <returns>An updated instance the specified <paramref name="cb"/> of <typeparamref name="T"/>.</returns>
+        public static T CombineWith<T>(this T cb, params double[] additionalChecksum) where T : ChecksumBuilder
+        {
+            return CombineWith(cb, ByteConverter.FromConvertibles<double>(additionalChecksum));
+        }
+
+        /// <summary>
+        /// Combines the <paramref name="additionalChecksum"/> to the representation of this instance.
+        /// </summary>
+        /// <typeparam name="T">The type of the <see cref="ChecksumBuilder"/>.</typeparam>
+        /// <param name="cb">An instance of a <see cref="ChecksumBuilder"/>.</param>
+        /// <param name="additionalChecksum">An <see cref="short"/> array that contains zero or more checksum of the additional data this instance must represent.</param>
+        /// <returns>An updated instance the specified <paramref name="cb"/> of <typeparamref name="T"/>.</returns>
+        public static T CombineWith<T>(this T cb, params short[] additionalChecksum) where T : ChecksumBuilder
+        {
+            return CombineWith(cb, ByteConverter.FromConvertibles<short>(additionalChecksum));
+        }
+
+        /// <summary>
+        /// Combines the <paramref name="additionalChecksum"/> to the representation of this instance.
+        /// </summary>
+        /// <typeparam name="T">The type of the <see cref="ChecksumBuilder"/>.</typeparam>
+        /// <param name="cb">An instance of a <see cref="ChecksumBuilder"/>.</param>
+        /// <param name="additionalChecksum">A <see cref="string"/> array that contains zero or more checksum of the additional data this instance must represent.</param>
+        /// <returns>An updated instance the specified <paramref name="cb"/> of <typeparamref name="T"/>.</returns>
+        public static T CombineWith<T>(this T cb, params string[] additionalChecksum) where T : ChecksumBuilder
+        {
+            List<long> result = new List<long>();
+            if (additionalChecksum == null)
+            {
+                result.Add(StructUtility.HashCodeForNullValue);
+            }
+            else
+            {
+                for (int i = 0; i < additionalChecksum.Length; i++)
+                {
+                    result.Add(StructUtility.GetHashCode64(additionalChecksum[i]));
+                }
+            }
+            return CombineWith(cb, result.ToArray());
+        }
+
+        /// <summary>
+        /// Combines the <paramref name="additionalChecksum"/> to the representation of this instance.
+        /// </summary>
+        /// <typeparam name="T">The type of the <see cref="ChecksumBuilder"/>.</typeparam>
+        /// <param name="cb">An instance of a <see cref="ChecksumBuilder"/>.</param>
+        /// <param name="additionalChecksum">An <see cref="int"/> array that contains zero or more checksum of the additional data this instance must represent.</param>
+        /// <returns>An updated instance the specified <paramref name="cb"/> of <typeparamref name="T"/>.</returns>
+        public static T CombineWith<T>(this T cb, params int[] additionalChecksum) where T : ChecksumBuilder
+        {
+            return CombineWith(cb, ByteConverter.FromConvertibles<int>(additionalChecksum));
+        }
+
+        /// <summary>
+        /// Combines the <paramref name="additionalChecksum"/> to the representation of this instance.
+        /// </summary>
+        /// <typeparam name="T">The type of the <see cref="ChecksumBuilder"/>.</typeparam>
+        /// <param name="cb">An instance of a <see cref="ChecksumBuilder"/>.</param>
+        /// <param name="additionalChecksum">An <see cref="long"/> array that contains zero or more checksum of the additional data this instance must represent.</param>
+        /// <returns>An updated instance the specified <paramref name="cb"/> of <typeparamref name="T"/>.</returns>
+        public static T CombineWith<T>(this T cb, params long[] additionalChecksum) where T : ChecksumBuilder
+        {
+            return CombineWith(cb, ByteConverter.FromConvertibles<long>(additionalChecksum));
+        }
+
+        /// <summary>
+        /// Combines the <paramref name="additionalChecksum"/> to the representation of this instance.
+        /// </summary>
+        /// <typeparam name="T">The type of the <see cref="ChecksumBuilder"/>.</typeparam>
+        /// <param name="cb">An instance of a <see cref="ChecksumBuilder"/>.</param>
+        /// <param name="additionalChecksum">A <see cref="float"/> array that contains zero or more checksum of the additional data this instance must represent.</param>
+        /// <returns>An updated instance the specified <paramref name="cb"/> of <typeparamref name="T"/>.</returns>
+        public static T CombineWith<T>(this T cb, params float[] additionalChecksum) where T : ChecksumBuilder
+        {
+            return CombineWith(cb, ByteConverter.FromConvertibles<float>(additionalChecksum));
+        }
+
+        /// <summary>
+        /// Combines the <paramref name="additionalChecksum"/> to the representation of this instance.
+        /// </summary>
+        /// <typeparam name="T">The type of the <see cref="ChecksumBuilder"/>.</typeparam>
+        /// <param name="cb">An instance of a <see cref="ChecksumBuilder"/>.</param>
+        /// <param name="additionalChecksum">An <see cref="ushort"/> array that contains zero or more checksum of the additional data this instance must represent.</param>
+        /// <returns>An updated instance the specified <paramref name="cb"/> of <typeparamref name="T"/>.</returns>
+        public static T CombineWith<T>(this T cb, params ushort[] additionalChecksum) where T : ChecksumBuilder
+        {
+            return CombineWith(cb, ByteConverter.FromConvertibles<ushort>(additionalChecksum));
+        }
+
+        /// <summary>
+        /// Combines the <paramref name="additionalChecksum"/> to the representation of this instance.
+        /// </summary>
+        /// <typeparam name="T">The type of the <see cref="ChecksumBuilder"/>.</typeparam>
+        /// <param name="cb">An instance of a <see cref="ChecksumBuilder"/>.</param>
+        /// <param name="additionalChecksum">An <see cref="uint"/> array that contains zero or more checksum of the additional data this instance must represent.</param>
+        /// <returns>An updated instance the specified <paramref name="cb"/> of <typeparamref name="T"/>.</returns>
+        public static T CombineWith<T>(this T cb, params uint[] additionalChecksum) where T : ChecksumBuilder
+        {
+            return CombineWith(cb, ByteConverter.FromConvertibles<uint>(additionalChecksum));
+        }
+
+        /// <summary>
+        /// Combines the <paramref name="additionalChecksum"/> to the representation of this instance.
+        /// </summary>
+        /// <typeparam name="T">The type of the <see cref="ChecksumBuilder"/>.</typeparam>
+        /// <param name="cb">An instance of a <see cref="ChecksumBuilder"/>.</param>
+        /// <param name="additionalChecksum">An <see cref="ulong"/> array that contains zero or more checksum of the additional data this instance must represent.</param>
+        /// <returns>An updated instance the specified <paramref name="cb"/> of <typeparamref name="T"/>.</returns>
+        public static T CombineWith<T>(this T cb, params ulong[] additionalChecksum) where T : ChecksumBuilder
+        {
+            return CombineWith(cb, ByteConverter.FromConvertibles<ulong>(additionalChecksum));
+        }
+
+        /// <summary>
+        /// Combines the <paramref name="additionalChecksum" /> to the representation of this instance.
+        /// </summary>
+        /// <typeparam name="T">The type of the <see cref="ChecksumBuilder"/>.</typeparam>
+        /// <param name="cb">An instance of a <see cref="ChecksumBuilder"/>.</param>
+        /// <param name="additionalChecksum">An array of bytes containing a checksum of the additional data this instance must represent.</param>
+        /// <returns>An updated instance the specified <paramref name="cb"/> of <typeparamref name="T"/>.</returns>
+        public static T CombineWith<T>(this T cb, params byte[] additionalChecksum) where T : ChecksumBuilder
+        {
+            if (additionalChecksum == null) { return cb; }
+            if (additionalChecksum.Length == 0) { return cb; }
+            cb.ComputedChecksum = null;
+            cb.Bytes.AddRange(additionalChecksum);
+            return cb;
+        }
+    }
+}
