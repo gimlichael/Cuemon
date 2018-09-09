aed45c9ac979dcb0d3128cf3eba656713d26cf55
gimlichael <mmdevelopment@hotmail.com>
2018-09-09 16:55:55 +0200

    Added support for a way to make filters exclusive to this HttpCacheableFilter .. for an easy way to extend if necessary. I choose ICacheableAsyncResultFilter that is identical to IAsyncResultFilter.

---
+++ src/Cuemon.AspNetCore.Mvc/Filters/Cacheable/HttpCacheableFilter.cs
@@ -0,0 +1,48 @@
+﻿using System.Threading.Tasks;
+using Cuemon.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Options;
+
+namespace Cuemon.AspNetCore.Mvc.Filters.Cacheable
+{
+    /// <summary>
+    /// A filter that will invoke filters implementing the <see cref="ICacheableObjectResult"/> interface.
+    /// </summary>
+    /// <seealso cref="ConfigurableAsyncResultFilter{HttpCacheableOptions}" />
+    public class HttpCacheableFilter : ConfigurableAsyncResultFilter<HttpCacheableOptions>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="HttpCacheableFilter"/> class.
+        /// </summary>
+        /// <param name="setup">The <see cref="HttpCacheableOptions" /> which need to be configured.</param>
+        public HttpCacheableFilter(IOptions<HttpCacheableOptions> setup) : base(setup)
+        {
+        }
+
+        /// <summary>
+        /// Called asynchronously before the action result.
+        /// </summary>
+        /// <param name="context">The <see cref="ResultExecutingContext" />.</param>
+        /// <param name="next">The <see cref="ResultExecutionDelegate" />. Invoked to execute the next result filter or the result itself.</param>
+        /// <returns>A <see cref="Task" /> that on completion indicates the filter has executed.</returns>
+        public override async Task OnResultExecutionAsync(ResultExecutingContext context, ResultExecutionDelegate next)
+        {
+            if (context.Result is ObjectResult result)
+            {
+                foreach (var filter in Options.Filters)
+                {
+                    await filter.OnResultExecutionAsync(context, next);
+                }
+
+                if (result.Value is ICacheableObjectResult cacheableObjectResult)
+                {
+                    result.Value = cacheableObjectResult.Value;
+                    cacheableObjectResult.Value = null;
+                }
+            }
+            if (!context.HttpContext.Response.HasStarted && context.HttpContext.Response.StatusCode != StatusCodes.Status304NotModified) { await next().ContinueWithSuppressedContext(); }
+        }
+    }
+}
---
+++ src/Cuemon.AspNetCore.Mvc/Filters/Cacheable/HttpCacheableOptions.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace Cuemon.AspNetCore.Mvc.Filters.Cacheable
+{
+    /// <summary>
+    /// Specifies options that is related to the <see cref="HttpCacheableFilter" />.
+    /// </summary>
+    public class HttpCacheableOptions
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="HttpCacheableOptions"/> class.
+        /// </summary>
+        public HttpCacheableOptions()
+        {
+            Filters = new List<ICacheableAsyncResultFilter>();
+        }
+
+        /// <summary>
+        /// Gets the filters that will be invoked one by one in <see cref="HttpCacheableFilter.OnResultExecutionAsync"/>.
+        /// </summary>
+        /// <value>The filters that will be invoked by <see cref="HttpCacheableFilter"/>.</value>
+        public IList<ICacheableAsyncResultFilter> Filters { get; }
+    }
+}
