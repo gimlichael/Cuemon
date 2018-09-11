202c005b3a15c2caa9ca21cd8c6e5cb0502ab0a5
gimlichael <mmdevelopment@hotmail.com>
2018-09-11 20:29:09 +0200

    Removed unused namespace reference.

--- src/Cuemon.Serialization.Json/Converters/JsonConverterCollectionExtensions.cs
+++ src/Cuemon.Serialization.Json/Converters/JsonConverterCollectionExtensions.cs
@@ -2,7 +2,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using Cuemon.Collections;
 using Cuemon.Diagnostics;
 using Cuemon.Serialization.Formatters;
 using Newtonsoft.Json;
