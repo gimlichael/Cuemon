f48dacadc8cc64811b8ebfcd48e5421d6fda1369
gimlichael <mmdevelopment@hotmail.com>
2018-09-18 21:50:13 +0200

    Added support for a generic way of converting from one ecoding to another encoding.

---
+++ src/Cuemon.Core/Text/EncodingConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+
+namespace Cuemon.Text
+{
+    /// <summary>
+    /// This utility class is designed to make <see cref="Encoding"/> conversions easier to work with.
+    /// </summary>
+    public static class EncodingConverter
+    {
+        /// <summary>
+        /// Converts the specified <paramref name="value"/> to its equivalent ASCII encoded representation.
+        /// </summary>
+        /// <param name="value">The value to apply with an ASCII encoding conversion.</param>
+        /// <param name="setup">The <see cref="EncodingOptions"/> which need to be configured.</param>
+        /// <returns>A string that is ASCII encoded.</returns>
+        public static string ToAsciiEncodedString(string value, Action<EncodingOptions> setup = null)
+        {
+            var options = setup.ConfigureOptions();
+            return Encoding.ASCII.ToEncodedString(value, o =>
+            {
+                o.Encoding = options.Encoding;
+                o.Preamble = options.Preamble;
+                o.EncoderFallback = new EncoderReplacementFallback("");
+            });
+        }
+    }
+}
