afc1e1b07305af2e7c60aaa41fbadf5740091147
gimlichael <mmdevelopment@hotmail.com>
2018-12-16 20:42:28 +0100

    Added support for CDN enabled websites with Razor tag helpers.

---
+++ src/Cuemon.AspNetCore.Razor.TagHelpers/CdnUriScheme.cs
@@ -0,0 +1,25 @@
+﻿namespace Cuemon.AspNetCore.Razor.TagHelpers
+{
+    /// <summary>
+    /// Enum CdnUriScheme
+    /// </summary>
+    public enum CdnUriScheme
+    {
+        /// <summary>
+        /// Specifies that the URI scheme is not defined.
+        /// </summary>
+        None,
+        /// <summary>
+        /// Specifies that the URI scheme is protocol-relative (//).
+        /// </summary>
+        Relative,
+        /// <summary>
+        /// Specifies that the URI scheme is Hypertext Transfer Protocol (HTTP).
+        /// </summary>
+        Http,
+        /// <summary>
+        /// Specifies that the URI scheme is Secure Hypertext Transfer Protocol (HTTPS).
+        /// </summary>
+        Https
+    }
+}
