53cd639b909e85f1f7b9b05e3c67b5ca8f94d29c
gimlichael <mmdevelopment@hotmail.com>
2019-01-20 21:33:47 +0100

    Consequence change from CorrelationIdentifier* refactoring.

--- src/Cuemon.AspNetCore.Mvc/Filters/Diagnostics/FaultDescriptorFilter.cs
+++ src/Cuemon.AspNetCore.Mvc/Filters/Diagnostics/FaultDescriptorFilter.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Net;
 using System.Reflection;
+using Cuemon.AspNetCore.Http.Headers;
 using Cuemon.Configuration;
 using Cuemon.Diagnostics;
 using Microsoft.AspNetCore.Mvc.Controllers;
