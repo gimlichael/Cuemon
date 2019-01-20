dd8b28428418ba554d8236ce7bb195ed82beba53
gimlichael <mmdevelopment@hotmail.com>
2019-01-20 21:03:10 +0100

    New extension methods for TimeSpan: Ceiling, Floor, Round.

--- src/Cuemon.Core/TimeSpanExtensions.cs
+++ src/Cuemon.Core/TimeSpanExtensions.cs
@@ -56,5 +56,94 @@ namespace Cuemon
         {
             return (int)(GetTotalMicroseconds(ts) % 1000);
         }
+
+        /// <summary>
+        /// Returns a <see cref="TimeSpan"/> value that is rounded towards negative infinity.
+        /// </summary>
+        /// <param name="value">A <see cref="TimeSpan"/> value to be rounded.</param>
+        /// <param name="interval">The <see cref="TimeSpan"/> value that specifies the rounding of <paramref name="value"/>.</param>
+        /// <returns>A <see cref="TimeSpan"/> value that is rounded towards negative infinity.</returns>
+        public static TimeSpan Floor(this TimeSpan value, TimeSpan interval)
+        {
+            return Round(value, interval, VerticalDirection.Down);
+        }
+
+        /// <summary>
+        /// Returns a <see cref="TimeSpan"/> value that is rounded towards positive infinity.
+        /// </summary>
+        /// <param name="value">A <see cref="TimeSpan"/> value to be rounded.</param>
+        /// <param name="interval">The <see cref="TimeSpan"/> value that specifies the rounding of <paramref name="value"/>.</param>
+        /// <returns>A <see cref="TimeSpan"/> value that is rounded towards positive infinity.</returns>
+        public static TimeSpan Ceiling(this TimeSpan value, TimeSpan interval)
+        {
+            return Round(value, interval, VerticalDirection.Up);
+        }
+
+        /// <summary>
+        /// Returns a <see cref="TimeSpan"/> value that is rounded towards negative infinity.
+        /// </summary>
+        /// <param name="value">A <see cref="TimeSpan"/> value to be rounded.</param>
+        /// <param name="interval">The <see cref="double"/> value that in combination with <paramref name="timeUnit"/> specifies the rounding of <paramref name="value"/>.</param>
+        /// <param name="timeUnit">One of the enumeration values that specifies the time unit of <paramref name="interval"/>.</param>
+        /// <returns>A <see cref="TimeSpan"/> value that is rounded towards negative infinity.</returns>
+        public static TimeSpan Floor(this TimeSpan value, double interval, TimeUnit timeUnit)
+        {
+            return Round(value, interval, timeUnit, VerticalDirection.Down);
+        }
+
+        /// <summary>
+        /// Returns a <see cref="TimeSpan"/> value that is rounded towards positive infinity.
+        /// </summary>
+        /// <param name="value">A <see cref="TimeSpan"/> value to be rounded.</param>
+        /// <param name="interval">The <see cref="double"/> value that in combination with <paramref name="timeUnit"/> specifies the rounding of <paramref name="value"/>.</param>
+        /// <param name="timeUnit">One of the enumeration values that specifies the time unit of <paramref name="interval"/>.</param>
+        /// <returns>A <see cref="TimeSpan"/> value that is rounded towards positive infinity.</returns>
+        public static TimeSpan Ceiling(this TimeSpan value, double interval, TimeUnit timeUnit)
+        {
+            return Round(value, interval, timeUnit, VerticalDirection.Up);
+        }
+
+        /// <summary>
+        /// Returns a <see cref="TimeSpan"/> value that is rounded either towards negative infinity or positive infinity.
+        /// </summary>
+        /// <param name="value">A <see cref="TimeSpan"/> value to be rounded.</param>
+        /// <param name="interval">The <see cref="double"/> value that in combination with <paramref name="timeUnit"/> specifies the rounding of <paramref name="value"/>.</param>
+        /// <param name="timeUnit">One of the enumeration values that specifies the time unit of <paramref name="interval"/>.</param>
+        /// <param name="direction">One of the enumeration values that specifies the direction of the rounding.</param>
+        /// <returns>A <see cref="TimeSpan"/> value that is rounded either towards negative infinity or positive infinity.</returns>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// <paramref name="direction"/> is an invalid enumeration value.
+        /// </exception>
+        public static TimeSpan Round(this TimeSpan value, double interval, TimeUnit timeUnit, VerticalDirection direction)
+        {
+            return Round(value, TimeSpanConverter.FromDouble(interval, timeUnit), direction);
+        }
+
+        /// <summary>
+        /// Returns a <see cref="TimeSpan"/> value that is rounded either towards negative infinity or positive infinity.
+        /// </summary>
+        /// <param name="value">A <see cref="TimeSpan"/> value to be rounded.</param>
+        /// <param name="interval">The <see cref="TimeSpan"/> value that specifies the rounding of <paramref name="value"/>.</param>
+        /// <param name="direction">One of the enumeration values that specifies the direction of the rounding.</param>
+        /// <returns>A <see cref="TimeSpan"/> value that is rounded either towards negative infinity or positive infinity.</returns>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// <paramref name="direction"/> is an invalid enumeration value.
+        /// </exception>
+        public static TimeSpan Round(this TimeSpan value, TimeSpan interval, VerticalDirection direction)
+        {
+            long valueTicks = interval < TimeSpan.Zero ? value.Add(interval).Ticks : value.Ticks;
+            long absoluteIntervalTicks = Math.Abs(interval.Ticks);
+            long remainder = valueTicks % absoluteIntervalTicks;
+            switch (direction)
+            {
+                case VerticalDirection.Up:
+                    long adjustment = (absoluteIntervalTicks - (remainder)) % absoluteIntervalTicks;
+                    return new TimeSpan(valueTicks + adjustment);
+                case VerticalDirection.Down:
+                    return new TimeSpan(valueTicks - remainder);
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(direction));
+            }
+        }
     }
 }
