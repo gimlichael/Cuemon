4758f868e6b7465f3d6c6a7574ec3df39a3d3de4
gimlichael <mmdevelopment@hotmail.com>
2019-02-09 16:11:29 +0100

    New extensions; AddHttpExceptionDescriptorConverter

--- src/Cuemon.AspNetCore.Mvc.Formatters.Json/Converters/JsonConverterCollectionExtensions.cs
+++ src/Cuemon.AspNetCore.Mvc.Formatters.Json/Converters/JsonConverterCollectionExtensions.cs
@@ -1,4 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Cuemon.AspNetCore.Http;
+using Cuemon.Serialization;
 using Cuemon.Serialization.Json;
 using Microsoft.Extensions.Primitives;
 using Newtonsoft.Json;
@@ -10,6 +14,61 @@ namespace Cuemon.AspNetCore.Mvc.Formatters.Json.Converters
     /// </summary>
     public static class JsonConverterCollectionExtensions
     {
+        /// <summary>
+        /// Adds an <see cref="HttpExceptionDescriptor"/> JSON converter to the list.
+        /// </summary>
+        /// <param name="converters">The list of JSON converters.</param>
+        /// <param name="setup">The <see cref="ExceptionDescriptorSerializationOptions"/> which need to be configured.</param>
+        public static void AddHttpExceptionDescriptorConverter(this ICollection<JsonConverter> converters, Action<ExceptionDescriptorSerializationOptions> setup = null)
+        {
+            var options = setup.ConfigureOptions();
+            converters.Add(DynamicJsonConverter.Create<HttpExceptionDescriptor>((writer, descriptor) =>
+            {
+                writer.WriteStartObject();
+                writer.WritePropertyName("error", () => DynamicJsonConverter.UseCamelCase);
+                writer.WriteStartObject();
+                writer.WritePropertyName("status", () => DynamicJsonConverter.UseCamelCase);
+                writer.WriteValue(descriptor.StatusCode);
+                writer.WritePropertyName("code", () => DynamicJsonConverter.UseCamelCase);
+                writer.WriteValue(descriptor.Code);
+                writer.WritePropertyName("message", () => DynamicJsonConverter.UseCamelCase);
+                writer.WriteValue(descriptor.Message);
+                if (descriptor.HelpLink != null)
+                {
+                    writer.WritePropertyName("helpLink", () => DynamicJsonConverter.UseCamelCase);
+                    writer.WriteValue(descriptor.HelpLink.OriginalString);
+                }
+                if (options.IncludeFailure)
+                {
+                    writer.WritePropertyName("failure", () => DynamicJsonConverter.UseCamelCase);
+                    writer.WriteObject(descriptor.Failure);
+                }
+                writer.WriteEndObject();
+                if (options.IncludeEvidence && descriptor.Evidence.Any())
+                {
+                    writer.WritePropertyName("evidence", () => DynamicJsonConverter.UseCamelCase);
+                    writer.WriteStartObject();
+                    foreach (var evidence in descriptor.Evidence)
+                    {
+                        writer.WritePropertyName(evidence.Key, () => DynamicJsonConverter.UseCamelCase);
+                        writer.WriteObject(evidence.Value);
+                    }
+                    writer.WriteEndObject();
+                }
+                if (!descriptor.CorrelationId.IsNullOrWhiteSpace())
+                {
+                    writer.WritePropertyName("correlationId", () => DynamicJsonConverter.UseCamelCase);
+                    writer.WriteValue(descriptor.CorrelationId);
+                }
+                if (!descriptor.RequestId.IsNullOrWhiteSpace())
+                {
+                    writer.WritePropertyName("requestId", () => DynamicJsonConverter.UseCamelCase);
+                    writer.WriteValue(descriptor.RequestId);
+                }
+                writer.WriteEndObject();
+            }));
+        }
+
         /// <summary>
         /// Adds an <see cref="StringValues"/> JSON converter to the list.
         /// </summary>
