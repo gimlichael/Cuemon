47917e2cbbccd650ca7421b8b13d24293f437448
gimlichael <mmdevelopment@hotmail.com>
2019-02-09 15:31:40 +0100

    Added support for Request ID.

---
+++ src/Cuemon.Core/Messaging/IRequest.cs
@@ -0,0 +1,14 @@
+﻿namespace Cuemon.Messaging
+{
+    /// <summary>
+    /// Provides a Request ID that is a unique identifier which is attached to requests and messages that allow reference to a particular transaction.
+    /// </summary>
+    public interface IRequest
+    {
+        /// <summary>
+        /// Gets the unique request identifier.
+        /// </summary>
+        /// <value>The unique request identifier.</value>
+        string RequestId { get; }
+    }
+}
