c29f2d833a4b9875a7546e6bf7b472bb8e0a93a5
Michael Mortensen <CUEMON\Michael Mortensen>
2019-03-10 20:43:44 +0000

    New extensions for IServiceCollection
    git-tfs-id: [http://quagmire.cuemon.dk:8080/tfs/MuchMoreCollection]$/MUCHMORE/Main/Source/DnxCore/Cuemon Framework Core/src;C5458

---
+++ Cuemon.DependencyInjection/ServiceCollectionExtensions.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace Cuemon.DependencyInjection
+{
+    /// <summary>
+    /// Extension methods for the <see cref="IServiceCollection"/> interface.
+    /// </summary>
+    public static class ServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds the specified <typeparamref name="TService"/> with the <typeparamref name="TImplementation"/> and <paramref name="lifetime"/> to the <paramref name="services" />.
+        /// </summary>
+        /// <typeparam name="TService">The type of the service to add.</typeparam>
+        /// <typeparam name="TImplementation">The type of the implementation to use.</typeparam>
+        /// <param name="services">The <see cref="IServiceCollection" /> to add the service to.</param>
+        /// <param name="lifetime">The lifetime of the service.</param>
+        /// <returns>A reference to <paramref name="services"/> after the operation has completed.</returns>
+        public static IServiceCollection Add<TService, TImplementation>(this IServiceCollection services, ServiceLifetime lifetime)
+            where TService : class
+            where TImplementation : class, TService
+        {
+            return services.Add(typeof(TService), typeof(TImplementation), lifetime);
+        }
+
+        /// <summary>
+        /// Adds the specified <typeparamref name="TService"/> with the <typeparamref name="TImplementation"/> and <paramref name="lifetime"/> to the <paramref name="services" />.
+        /// </summary>
+        /// <typeparam name="TService">The type of the service to add.</typeparam>
+        /// <typeparam name="TImplementation">The type of the implementation to use.</typeparam>
+        /// <typeparam name="TOptions">The type of the configured options.</typeparam>
+        /// <param name="services">The <see cref="IServiceCollection" /> to add the service to.</param>
+        /// <param name="lifetime">The lifetime of the service.</param>
+        /// <param name="setup">The <typeparamref name="TOptions"/> which need to be configured.</param>
+        /// <returns>A reference to <paramref name="services"/> after the operation has completed.</returns>
+        public static IServiceCollection Add<TService, TImplementation, TOptions>(this IServiceCollection services, ServiceLifetime lifetime, Action<TOptions> setup)
+            where TService : class
+            where TImplementation : class, TService
+            where TOptions : class, new()
+        {
+            return services.Add(typeof(TService), typeof(TImplementation), lifetime, setup);
+        }
+
+        /// <summary>
+        /// Adds the specified <paramref name="service" /> with the <paramref name="implementation" /> and <paramref name="lifetime"/> to the <paramref name="services" />.
+        /// </summary>
+        /// <param name="services">The <see cref="IServiceCollection" /> to add the service to.</param>
+        /// <param name="service">The type of the service to register.</param>
+        /// <param name="implementation">The implementation type of the service.</param>
+        /// <param name="lifetime">The lifetime of the service.</param>
+        /// <returns>A reference to <paramref name="services"/> after the operation has completed.</returns>
+        public static IServiceCollection Add(this IServiceCollection services, Type service, Type implementation, ServiceLifetime lifetime)
+        {
+            Validator.ThrowIfNull(services, nameof(services));
+            services.AddServices(service, implementation, lifetime, false);
+            return services;
+        }
+
+        /// <summary>
+        /// Adds the specified <paramref name="service" /> with the <paramref name="implementation" /> and <paramref name="lifetime"/> to the <paramref name="services" />.
+        /// </summary>
+        /// <typeparam name="TOptions">The type of the configured options.</typeparam>
+        /// <param name="services">The <see cref="IServiceCollection" /> to add the service to.</param>
+        /// <param name="service">The type of the service to register.</param>
+        /// <param name="implementation">The implementation type of the service.</param>
+        /// <param name="lifetime">The lifetime of the service.</param>
+        /// <param name="setup">The <typeparamref name="TOptions"/> which need to be configured.</param>
+        /// <returns>A reference to <paramref name="services"/> after the operation has completed.</returns>
+        public static IServiceCollection Add<TOptions>(this IServiceCollection services, Type service, Type implementation, ServiceLifetime lifetime, Action<TOptions> setup)
+            where TOptions : class, new()
+        {
+            Validator.ThrowIfNull(services, nameof(services));
+            Validator.ThrowIfNull(setup, nameof(setup));
+            services.AddServices(service, implementation, lifetime, false);
+            services.Configure(setup);
+            return services;
+        }
+
+        /// <summary>
+        /// Adds the specified <typeparamref name="TService"/> with the <typeparamref name="TImplementation"/> and <paramref name="lifetime"/> to the <paramref name="services" /> if the service type has not already been registered.
+        /// </summary>
+        /// <typeparam name="TService">The type of the service to add.</typeparam>
+        /// <typeparam name="TImplementation">The type of the implementation to use.</typeparam>
+        /// <param name="services">The <see cref="IServiceCollection" /> to add the service to.</param>
+        /// <param name="lifetime">The lifetime of the service.</param>
+        /// <returns>A reference to <paramref name="services"/> after the operation has completed.</returns>
+        public static IServiceCollection TryAdd<TService, TImplementation>(this IServiceCollection services, ServiceLifetime lifetime)
+            where TService : class
+            where TImplementation : class, TService
+        {
+            Validator.ThrowIfNull(services, nameof(services));
+            return services.TryAdd(typeof(TService), typeof(TImplementation), lifetime);
+        }
+
+        /// <summary>
+        /// Adds the specified <typeparamref name="TService"/> with the <typeparamref name="TImplementation"/> and <paramref name="lifetime"/> to the <paramref name="services" /> if the service type has not already been registered.
+        /// </summary>
+        /// <typeparam name="TService">The type of the service to add.</typeparam>
+        /// <typeparam name="TImplementation">The type of the implementation to use.</typeparam>
+        /// <typeparam name="TOptions">The type of the configured options.</typeparam>
+        /// <param name="services">The <see cref="IServiceCollection" /> to add the service to.</param>
+        /// <param name="lifetime">The lifetime of the service.</param>
+        /// <param name="setup">The <typeparamref name="TOptions"/> which need to be configured.</param>
+        /// <returns>A reference to <paramref name="services"/> after the operation has completed.</returns>
+        public static IServiceCollection TryAdd<TService, TImplementation, TOptions>(this IServiceCollection services, ServiceLifetime lifetime, Action<TOptions> setup)
+            where TService : class
+            where TImplementation : class, TService
+            where TOptions : class, new()
+        {
+            Validator.ThrowIfNull(services, nameof(services));
+            Validator.ThrowIfNull(setup, nameof(setup));
+            return services.TryAdd(typeof(TService), typeof(TImplementation), lifetime, setup);
+        }
+
+        /// <summary>
+        /// Adds the specified <paramref name="service" /> with the <paramref name="implementation" /> and <paramref name="lifetime"/> to the <paramref name="services" /> if the service type has not already been registered.
+        /// </summary>
+        /// <param name="services">The <see cref="IServiceCollection" /> to add the service to.</param>
+        /// <param name="service">The type of the service to register.</param>
+        /// <param name="implementation">The implementation type of the service.</param>
+        /// <param name="lifetime">The lifetime of the service.</param>
+        /// <returns>A reference to <paramref name="services"/> after the operation has completed.</returns>
+        public static IServiceCollection TryAdd(this IServiceCollection services, Type service, Type implementation, ServiceLifetime lifetime)
+        {
+            Validator.ThrowIfNull(services, nameof(services));
+            services.AddServices(service, implementation, lifetime, true);
+            return services;
+        }
+
+        /// <summary>
+        /// Adds the specified <paramref name="service" /> with the <paramref name="implementation" /> and <paramref name="lifetime"/> to the <paramref name="services" /> if the service type has not already been registered.
+        /// </summary>
+        /// <typeparam name="TOptions">The type of the configured options.</typeparam>
+        /// <param name="services">The <see cref="IServiceCollection" /> to add the service to.</param>
+        /// <param name="service">The type of the service to register.</param>
+        /// <param name="implementation">The implementation type of the service.</param>
+        /// <param name="lifetime">The lifetime of the service.</param>
+        /// <param name="setup">The <typeparamref name="TOptions"/> which need to be configured.</param>
+        /// <returns>A reference to <paramref name="services"/> after the operation has completed.</returns>
+        public static IServiceCollection TryAdd<TOptions>(this IServiceCollection services, Type service, Type implementation, ServiceLifetime lifetime, Action<TOptions> setup)
+            where TOptions : class, new()
+        {
+            Validator.ThrowIfNull(services, nameof(services));
+            Validator.ThrowIfNull(setup, nameof(setup));
+            services.AddServices(service, implementation, lifetime, true);
+            services.Configure(setup);
+            return services;
+        }
+
+        private static void AddServices(this IServiceCollection services, Type service, Type implementation, ServiceLifetime lifetime, bool useTesterDoerPattern)
+        {
+            switch (lifetime)
+            {
+                case ServiceLifetime.Scoped:
+                    Condition.FlipFlop(useTesterDoerPattern, () => services.TryAddScoped(service, implementation), () => services.AddScoped(service, implementation));
+                    break;
+                case ServiceLifetime.Singleton:
+                    Condition.FlipFlop(useTesterDoerPattern, () => services.TryAddSingleton(service, implementation), () => services.AddSingleton(service, implementation));
+                    break;
+                case ServiceLifetime.Transient:
+                    Condition.FlipFlop(useTesterDoerPattern, () => services.TryAddTransient(service, implementation), () => services.AddTransient(service, implementation));
+                    break;
+            }
+        }
+    }
+}
