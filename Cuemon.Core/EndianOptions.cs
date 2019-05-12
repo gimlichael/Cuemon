f4f60e7bb3ff6b282464f78323c26bdab5219908
Michael Mortensen <CUEMON\Michael Mortensen>
2019-05-12 22:00:07 +0000

    git-tfs-id: [http://quagmire.cuemon.dk:8080/tfs/MuchMoreCollection]$/MUCHMORE/Main/Source/DnxCore/Cuemon Framework Core/src;C5631

---
+++ Cuemon.Core/EndianOptions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Cuemon
+{
+    /// <summary>
+    /// Configuration options for <see cref="BitConverter"/>.
+    /// </summary>
+    public class EndianOptions
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EndianOptions"/> class.
+        /// </summary>
+        public EndianOptions()
+        {
+            UseBigEndian = false;
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether to use Big-endian instead of Little-endian.
+        /// </summary>
+        /// <value><c>true</c> if Big-endian should be used; otherwise, <c>false</c>.</value>
+        public bool UseBigEndian { get; set; }
+    }
+}
