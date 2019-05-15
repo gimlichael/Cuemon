605f14b04a966c89a9ac381032c23db9a7c97a2e
Michael Mortensen <CUEMON\Michael Mortensen>
2019-05-15 20:55:12 +0000

    UrlEncodedBase64
    git-tfs-id: [http://quagmire.cuemon.dk:8080/tfs/MuchMoreCollection]$/MUCHMORE/Main/Source/DnxCore/Cuemon Framework Core/src;C5632

---
+++ Cuemon.Core/ComponentModel/TypeConverters/UrlEncodedBase64StringConverter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Cuemon.ComponentModel.TypeConverters
+{
+    /// <summary>
+    /// Provides a converter that converts a <see cref="string"/>, represented in URL-safe base64 characters, to its equivalent <see cref="T:byte[]"/>.
+    /// </summary>
+    public class UrlEncodedBase64StringConverter : IConverter<string, byte[]>
+    {
+        /// <summary>
+        /// Converts the specified <paramref name="input"/> of URL-safe base64 characters to its equivalent <see cref="T:byte[]"/> representation.
+        /// </summary>
+        /// <param name="input">The <see cref="string"/> to be converted into a <see cref="T:byte[]"/>.</param>
+        /// <returns>A <see cref="T:byte[]"/> that is equivalent to <paramref name="input"/>.</returns>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="input"/> cannot be null.
+        /// </exception>
+        /// <exception cref="ArgumentException">
+        /// <paramref name="input"/> cannot be empty or consist only of white-space characters.
+        /// </exception>
+        /// <exception cref="FormatException">
+        /// <paramref name="input"/> consist of illegal base64 characters.
+        /// </exception>
+        /// <remarks>Source: http://tools.ietf.org/html/draft-ietf-jose-json-web-signature-08#appendix-C</remarks>
+        public byte[] ChangeType(string input)
+        {
+            Validator.ThrowIfNullOrWhitespace(input, nameof(input));
+            input = input.Replace('-', '+');
+            input = input.Replace('_', '/');
+            switch (input.Length % 4)
+            {
+                case 0:
+                    break;
+                case 2:
+                    input += "==";
+                    break;
+                case 3:
+                    input += "=";
+                    break;
+                default:
+                    throw new FormatException(FormattableString.Invariant($"The format of {nameof(input)} consist of illegal base64 characters."));
+            }
+            return Convert.FromBase64String(input);
+        }
+    }
+}
---
+++ Cuemon.Core/ComponentModel/TypeConverters/UrlEncodedBase64StringParser.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Cuemon.ComponentModel.TypeConverters
+{
+    /// <summary>
+    /// Provides a parser that converts a <see cref="string"/>, represented in URL-safe base64 characters, to its equivalent <see cref="T:byte[]"/>.
+    /// </summary>
+    public class UrlEncodedBase64StringParser : IParser<byte[]>
+    {
+        /// <summary>
+        /// Converts the string representation of an URL-safe base64 characters to its <see cref="T:byte[]"/> equivalent.
+        /// </summary>
+        /// <param name="input">A string consisting of URL-safe base64 characters.</param>
+        /// <returns>A <see cref="T:byte[]"/> equivalent to <paramref name="input"/>.</returns>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="input"/> cannot be null.
+        /// </exception>
+        /// <exception cref="ArgumentException">
+        /// <paramref name="input"/> cannot be empty or consist only of white-space characters.
+        /// </exception>
+        /// <exception cref="FormatException">
+        /// <paramref name="input"/> consist of illegal base64 characters.
+        /// </exception>
+        /// <seealso cref="UrlEncodedBase64StringConverter"/>
+        public byte[] Parse(string input)
+        {
+            return Converters.FromString.ToByteArray<UrlEncodedBase64StringConverter>(input);
+        }
+
+        /// <summary>
+        /// Converts the string representation of an URL-safe base64 characters number to its <see cref="T:byte[]"/> equivalent. A return value indicates whether the conversion succeeded.
+        /// </summary>
+        /// <param name="input">A string consisting of URL-safe base64 characters.</param>
+        /// <param name="result">When this method returns, contains the <see cref="T:byte[]"/> equivalent of the URL-safe base64 characters contained within <paramref name="input"/>, if the conversion succeeded, or <c>default</c> if the conversion failed.</param>
+        /// <returns><c>true</c> if <paramref name="input"/> was converted successfully; otherwise, <c>false</c>.</returns>
+        public bool TryParse(string input, out byte[] result)
+        {
+            return Patterns.TryParse(() => Parse(input), out result);
+        }
+    }
+}
