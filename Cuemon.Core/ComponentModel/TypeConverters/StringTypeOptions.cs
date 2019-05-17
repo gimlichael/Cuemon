53bdca62952ae46bf81342b307b2b7126b797c4e
Michael Mortensen <CUEMON\Michael Mortensen>
2019-05-17 11:30:57 +0000

    StringTypeParser
    git-tfs-id: [http://quagmire.cuemon.dk:8080/tfs/MuchMoreCollection]$/MUCHMORE/Main/Source/DnxCore/Cuemon Framework Core/src;C5638

---
+++ Cuemon.Core/ComponentModel/TypeConverters/StringTypeOptions.cs
@@ -0,0 +1,48 @@
+﻿using System.ComponentModel;
+using System.Globalization;
+
+namespace Cuemon.ComponentModel.TypeConverters
+{
+    /// <summary>
+    /// Configuration options for <see cref="StringTypeParser"/>.
+    /// </summary>
+    public class StringTypeOptions
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="StringTypeOptions"/> class.
+        /// </summary>
+        /// <remarks>
+        /// The following table shows the initial property values for an instance of <see cref="StringTypeOptions"/>.
+        /// <list type="table">
+        ///     <listheader>
+        ///         <term>Property</term>
+        ///         <description>Initial Value</description>
+        ///     </listheader>
+        ///     <item>
+        ///         <term><see cref="Culture"/></term>
+        ///         <description><see cref="CultureInfo.InvariantCulture"/></description>
+        ///     </item>
+        ///     <item>
+        ///         <term><see cref="DescriptorContext"/></term>
+        ///         <description><c>null</c></description>
+        ///     </item>
+        /// </list>
+        /// </remarks>
+        public StringTypeOptions()
+        {
+            Culture = CultureInfo.InvariantCulture;
+        }
+
+        /// <summary>
+        /// Gets or sets the culture specific formatting information.
+        /// </summary>
+        /// <value>The culture specific formatting information.</value>
+        public CultureInfo Culture { get; set; }
+
+        /// <summary>
+        /// Gets or sets the type specific format context.
+        /// </summary>
+        /// <value>The type specific format context.</value>
+        public ITypeDescriptorContext DescriptorContext { get; set; }
+    }
+}
---
+++ Cuemon.Core/ComponentModel/TypeConverters/StringTypeParser.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Cuemon.ComponentModel.TypeConverters
+{
+    /// <summary>
+    /// Provides a parser that converts a <see cref="string"/> to an arbitrary <see cref="object"/>.
+    /// </summary>
+    public class StringTypeParser : ITypeParser<StringTypeOptions>
+    {
+        /// <summary>
+        /// Converts the specified string to an arbitrary object of <typeparamref name="T"/>.
+        /// </summary>
+        /// <typeparam name="T">The type of the arbitrary object.</typeparam>
+        /// <param name="input">The string to convert.</param>
+        /// <param name="setup">The <see cref="StringTypeOptions"/> which may be configured.</param>
+        /// <returns>An object of <typeparamref name="T"/> equivalent to <paramref name="input"/>.</returns>
+        /// <exception cref="NotSupportedException">
+        /// <paramref name="input"/> cannot be converted to the specified <typeparamref name="T"/>.
+        /// </exception>
+        public T Parse<T>(string input, Action<StringTypeOptions> setup = null)
+        {
+            if (input == null) { return default; }
+            var options = Patterns.Configure(setup);
+            var converter = TypeDescriptor.GetConverter(typeof(T));
+            return (T)converter.ConvertFromString(options.DescriptorContext, options.Culture, input);
+        }
+
+        /// <summary>
+        /// Converts the specified string to an arbitrary object of <typeparamref name="T"/>. A return value indicates whether the conversion succeeded.
+        /// </summary>
+        /// <typeparam name="T">The type of the arbitrary object.</typeparam>
+        /// <param name="input">The string to convert.</param>
+        /// <param name="result">When this method returns, contains the object of <typeparamref name="T"/> equivalent to <paramref name="input"/>, if the conversion succeeded, or <c>default</c> if the conversion failed.</param>
+        /// <param name="setup">The <see cref="StringTypeOptions"/> which may be configured.</param>
+        /// <returns><c>true</c> if <paramref name="input"/> was converted successfully; otherwise, <c>false</c>.</returns>
+        public bool TryParse<T>(string input, out T result, Action<StringTypeOptions> setup = null)
+        {
+            return Patterns.TryParse(() => Parse<T>(input, setup), out result);
+        }
+    }
+}
