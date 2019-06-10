40daf4041c4cac6a3ae4c6715ee34b0f7bd2f1d6
Michael Mortensen <CUEMON\Michael Mortensen>
2019-06-10 13:36:30 +0000

    git-tfs-id: [http://quagmire.cuemon.dk:8080/tfs/MuchMoreCollection]$/MUCHMORE/Main/Source/DnxCore/Cuemon Framework Core/src;C5649

--- Cuemon.Core/Collections/Generic/ComparisonUtility.cs
+++ Cuemon.Core/Collections/Generic/ComparisonUtility.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using Cuemon.ComponentModel.Converters;
+using Cuemon.ComponentModel.TypeConverters;
 
 namespace Cuemon.Collections.Generic
 {
@@ -23,41 +25,42 @@ namespace Cuemon.Collections.Generic
             Validator.ThrowIfNull(valueSelector, nameof(valueSelector));
             var xValue = valueSelector(x);
             var yValue = valueSelector(y);
-            var code = TypeCodeConverter.FromType(typeSelector(x));
+            var code = ConvertFactory.UseConverter<TypeCodeConverter>().ChangeType(typeSelector(x));
+            var converter = ConvertFactory.UseConverter<ObjectTypeConverter>();
             switch (code)
             {
                 case TypeCode.Boolean:
-                    return Comparer<bool>.Default.Compare(Converter.FromObject<bool>(xValue), Converter.FromObject<bool>(yValue));
+                    return Comparer<bool>.Default.Compare(converter.ChangeTypeOrDefault<bool>(xValue), converter.ChangeTypeOrDefault<bool>(yValue));
                 case TypeCode.Byte:
-                    return Comparer<byte>.Default.Compare(Converter.FromObject<byte>(xValue), Converter.FromObject<byte>(yValue));
+                    return Comparer<byte>.Default.Compare(converter.ChangeTypeOrDefault<byte>(xValue), converter.ChangeTypeOrDefault<byte>(yValue));
                 case TypeCode.Char:
-                    return Comparer<char>.Default.Compare(Converter.FromObject<char>(xValue), Converter.FromObject<char>(yValue));
+                    return Comparer<char>.Default.Compare(converter.ChangeTypeOrDefault<char>(xValue), converter.ChangeTypeOrDefault<char>(yValue));
                 case TypeCode.DateTime:
-                    return Comparer<DateTime>.Default.Compare(Converter.FromObject<DateTime>(xValue), Converter.FromObject<DateTime>(yValue));
+                    return Comparer<DateTime>.Default.Compare(converter.ChangeTypeOrDefault<DateTime>(xValue), converter.ChangeTypeOrDefault<DateTime>(yValue));
                 case TypeCode.Decimal:
-                    return Comparer<decimal>.Default.Compare(Converter.FromObject<decimal>(xValue), Converter.FromObject<decimal>(yValue));
+                    return Comparer<decimal>.Default.Compare(converter.ChangeTypeOrDefault<decimal>(xValue), converter.ChangeTypeOrDefault<decimal>(yValue));
                 case TypeCode.Double:
-                    return Comparer<double>.Default.Compare(Converter.FromObject<double>(xValue), Converter.FromObject<double>(yValue));
+                    return Comparer<double>.Default.Compare(converter.ChangeTypeOrDefault<double>(xValue), converter.ChangeTypeOrDefault<double>(yValue));
                 case TypeCode.Int16:
-                    return Comparer<short>.Default.Compare(Converter.FromObject<short>(xValue), Converter.FromObject<short>(yValue));
+                    return Comparer<short>.Default.Compare(converter.ChangeTypeOrDefault<short>(xValue), converter.ChangeTypeOrDefault<short>(yValue));
                 case TypeCode.Int32:
-                    return Comparer<int>.Default.Compare(Converter.FromObject<int>(xValue), Converter.FromObject<int>(yValue));
+                    return Comparer<int>.Default.Compare(converter.ChangeTypeOrDefault<int>(xValue), converter.ChangeTypeOrDefault<int>(yValue));
                 case TypeCode.Int64:
-                    return Comparer<long>.Default.Compare(Converter.FromObject<long>(xValue), Converter.FromObject<long>(yValue));
+                    return Comparer<long>.Default.Compare(converter.ChangeTypeOrDefault<long>(xValue), converter.ChangeTypeOrDefault<long>(yValue));
                 case TypeCode.SByte:
-                    return Comparer<sbyte>.Default.Compare(Converter.FromObject<sbyte>(xValue), Converter.FromObject<sbyte>(yValue));
+                    return Comparer<sbyte>.Default.Compare(converter.ChangeTypeOrDefault<sbyte>(xValue), converter.ChangeTypeOrDefault<sbyte>(yValue));
                 case TypeCode.Single:
-                    return Comparer<float>.Default.Compare(Converter.FromObject<float>(xValue), Converter.FromObject<float>(yValue));
+                    return Comparer<float>.Default.Compare(converter.ChangeTypeOrDefault<float>(xValue), converter.ChangeTypeOrDefault<float>(yValue));
                 case TypeCode.String:
-                    return Comparer<string>.Default.Compare(Converter.FromObject<string>(xValue), Converter.FromObject<string>(yValue));
+                    return Comparer<string>.Default.Compare(converter.ChangeTypeOrDefault<string>(xValue), converter.ChangeTypeOrDefault<string>(yValue));
                 case TypeCode.UInt16:
-                    return Comparer<ushort>.Default.Compare(Converter.FromObject<ushort>(xValue), Converter.FromObject<ushort>(yValue));
+                    return Comparer<ushort>.Default.Compare(converter.ChangeTypeOrDefault<ushort>(xValue), converter.ChangeTypeOrDefault<ushort>(yValue));
                 case TypeCode.UInt32:
-                    return Comparer<uint>.Default.Compare(Converter.FromObject<uint>(xValue), Converter.FromObject<uint>(yValue));
+                    return Comparer<uint>.Default.Compare(converter.ChangeTypeOrDefault<uint>(xValue), converter.ChangeTypeOrDefault<uint>(yValue));
                 case TypeCode.UInt64:
-                    return Comparer<ulong>.Default.Compare(Converter.FromObject<ulong>(xValue), Converter.FromObject<ulong>(yValue));
+                    return Comparer<ulong>.Default.Compare(converter.ChangeTypeOrDefault<ulong>(xValue), converter.ChangeTypeOrDefault<ulong>(yValue));
                 default:
-                    return Comparer<object>.Default.Compare(Converter.FromObject<object>(xValue), Converter.FromObject<object>(yValue));
+                    return Comparer<object>.Default.Compare(converter.ChangeTypeOrDefault<object>(xValue), converter.ChangeTypeOrDefault<object>(yValue));
             }
         }
     }
