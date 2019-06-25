52925650be13a599a15367ce63e304df763d3c14
Michael Mortensen <CUEMON\Michael Mortensen>
2019-06-25 20:19:24 +0000

    UnkeyedCryptoHash
    git-tfs-id: [http://quagmire.cuemon.dk:8080/tfs/MuchMoreCollection]$/MUCHMORE/Main/Source/DnxCore/Cuemon Framework Core/src;C5656

---
+++ Cuemon.Core/Security/Cryptography/SecureHashAlgorithm1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Security.Cryptography;
+using Cuemon.Integrity;
+
+namespace Cuemon.Security.Cryptography
+{
+    /// <summary>
+    /// Provides a SHA-1 implementation of the SHA (Secure Hash Algorithm) cryptographic hashing algorithm for 160-bit hash values. This class cannot be inherited.
+    /// Implements the <see cref="UnkeyedCryptoHash{TAlgorithm}" />
+    /// </summary>
+    /// <seealso cref="UnkeyedCryptoHash{TAlgorithm}" />
+    /// <seealso cref="Hash{TOptions}" />
+    public sealed class SecureHashAlgorithm1 : UnkeyedCryptoHash<SHA1>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SecureHashAlgorithm1"/> class.
+        /// </summary>
+        /// <param name="setup">The <see cref="ConvertibleOptions" /> which may be configured.</param>
+        public SecureHashAlgorithm1(Action<ConvertibleOptions> setup = null) : base(SHA1.Create, setup)
+        {
+        }
+    }
+}
---
+++ Cuemon.Core/Security/Cryptography/SecureHashAlgorithm256.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Security.Cryptography;
+using Cuemon.Integrity;
+
+namespace Cuemon.Security.Cryptography
+{
+    /// <summary>
+    /// Provides a SHA-256 implementation of the SHA (Secure Hash Algorithm) cryptographic hashing algorithm for 256-bit hash values. This class cannot be inherited.
+    /// Implements the <see cref="UnkeyedCryptoHash{TAlgorithm}" />
+    /// </summary>
+    /// <seealso cref="UnkeyedCryptoHash{TAlgorithm}" />
+    /// <seealso cref="Hash{TOptions}" />
+    public sealed class SecureHashAlgorithm256 : UnkeyedCryptoHash<SHA256>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SecureHashAlgorithm256"/> class.
+        /// </summary>
+        /// <param name="setup">The <see cref="ConvertibleOptions" /> which may be configured.</param>
+        public SecureHashAlgorithm256(Action<ConvertibleOptions> setup = null) : base(SHA256.Create, setup)
+        {
+        }
+    }
+}
