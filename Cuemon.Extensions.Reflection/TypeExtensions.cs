d856cf635e66b624acfabc090bfd4575f4ac816f
Michael Mortensen <CUEMON\Michael Mortensen>
2019-07-04 20:31:22 +0000

    Removed ParseMethodParameters since MethodDescriptor covers it.
    git-tfs-id: [http://quagmire.cuemon.dk:8080/tfs/MuchMoreCollection]$/MUCHMORE/Main/Source/DnxCore/Cuemon Framework Core/src;C5664

--- Cuemon.Extensions.Reflection/TypeExtensions.cs
+++ Cuemon.Extensions.Reflection/TypeExtensions.cs
@@ -11,47 +11,7 @@ namespace Cuemon.Extensions.Reflection
     /// </summary>
     public static class TypeExtensions
     {
-        /// <summary>
-        /// Parses and returns a collection of key/value pairs representing the specified <paramref name="methodName"/>.
-        /// </summary>
-        /// <param name="source">The source to locate the specified <paramref name="methodName"/> in.</param>
-        /// <param name="methodName">The name of the method to parse on <paramref name="source"/>.</param>
-        /// <param name="methodParameters">A variable number of values passed to the <paramref name="methodName"/> on this instance.</param>
-        /// <returns>A collection of key/value pairs representing the specified <paramref name="methodName"/>.</returns>
-        /// <remarks>This method will parse the specified <paramref name="methodName"/> for parameter names and tie them with <paramref name="methodParameters"/>.</remarks>
-        /// <exception cref="ArgumentNullException">This exception is thrown if <paramref name="methodName"/> is null, if <paramref name="source"/> is null or if <paramref name="methodParameters"/> is null and method has resolved parameters.</exception>
-        /// <exception cref="ArgumentException">
-        /// This exception is thrown if either of the following is true:<br/>
-        /// the size of <paramref name="methodParameters"/> does not match the resolved parameters size of <paramref name="methodName"/>,<br/>
-        /// the type of <paramref name="methodParameters"/> does not match the resolved parameters type of <paramref name="methodName"/>.
-        /// </exception>
-        /// <remarks>This method auto resolves the associated <see cref="Type"/> for each object in <paramref name="methodParameters"/>. In case of a null referenced parameter, an <see cref="ArgumentNullException"/> is thrown, and you are encouraged to use the overloaded method instead.</remarks>
-        public static IDictionary<string, object> ParseMethodParameters(this Type source, string methodName, params object[] methodParameters)
-        {
-            return ReflectionUtility.ParseMethodParameters(source, methodName, methodParameters);
-        }
-
-        /// <summary>
-        /// Parses and returns a collection of key/value pairs representing the specified <paramref name="methodName"/>.
-        /// </summary>
-        /// <param name="source">The source to locate the specified <paramref name="methodName"/> in.</param>
-        /// <param name="methodName">The name of the method to parse on <paramref name="source"/>.</param>
-        /// <param name="methodParameters">A variable number of values passed to the <paramref name="methodName"/> on this instance.</param>
-        /// <param name="methodSignature">An array of <see cref="Type"/> objects representing the number, order, and type of the parameters for the method to get.</param>
-        /// <returns>A collection of key/value pairs representing the specified <paramref name="methodName"/>.</returns>
-        /// <remarks>This method will parse the specified <paramref name="methodName"/> for parameter names and tie them with <paramref name="methodParameters"/>.</remarks>
-        /// <exception cref="ArgumentNullException">This exception is thrown if <paramref name="methodName"/> is null, if <paramref name="source"/> is null or if <paramref name="methodParameters"/> is null and method has resolved parameters.</exception>
-        /// <exception cref="ArgumentException">
-        /// This exception is thrown if either of the following is true:<br/>
-        /// the size of <paramref name="methodParameters"/> does not match the resolved parameters size of <paramref name="methodName"/>,<br/>
-        /// the type of <paramref name="methodParameters"/> does not match the resolved parameters type of <paramref name="methodName"/>.
-        /// </exception>
-        public static IDictionary<string, object> ParseMethodParameters(this Type source, string methodName, Type[] methodSignature, params object[] methodParameters)
-        {
-            return ReflectionUtility.ParseMethodParameters(source, methodName, methodSignature, methodParameters);
-        }
-
-        /// <summary>
+       /// <summary>
         /// Loads the embedded resources from the associated <see cref="Assembly"/> of the specified <see cref="Type"/> following the <see cref="ManifestResourceMatch"/> ruleset of <paramref name="match"/>.
         /// </summary>
         /// <param name="source">The source type to load the resource from.</param>
