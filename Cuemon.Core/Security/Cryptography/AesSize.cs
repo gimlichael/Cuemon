7b7ef49b869529e4d15267bc751090fa6fdf7033
Michael Mortensen <CUEMON\Michael Mortensen>
2019-08-04 20:15:30 +0000

    AesCryptor -->Makes AdvancedEncryptionStandardUtility obsolete.
    git-tfs-id: [http://quagmire.cuemon.dk:8080/tfs/MuchMoreCollection]$/MUCHMORE/Main/Source/DnxCore/Cuemon Framework Core/src;C5668

---
+++ Cuemon.Core/Security/Cryptography/AesSize.cs
@@ -0,0 +1,21 @@
+﻿namespace Cuemon.Security.Cryptography
+{
+    /// <summary>
+    /// Specifies the size of the Advanced Encryption Standard (AES) symmetric algorithm.
+    /// </summary>
+    public enum AesSize
+    {
+        /// <summary>
+        /// The Advanced Encryption Standard (AES) symmetric algorithm with a 128 bit key.
+        /// </summary>
+        Aes128 = 0,
+        /// <summary>
+        /// The Advanced Encryption Standard (AES) symmetric algorithm with a 192 bit key.
+        /// </summary>
+        Aes192 = 1,
+        /// <summary>
+        /// The Advanced Encryption Standard (AES) symmetric algorithm with a 256 bit key.
+        /// </summary>
+        Aes256 = 2,
+    }
+}
