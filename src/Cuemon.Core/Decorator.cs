8702b336a6dccedd589c05eb4ed120a303119e62
gimlichael <michael@wbpa.dk>
2020-05-02 17:14:08 +0200

    Support for Decorator pattern in a generic way.

---
+++ src/Cuemon.Core/Decorator.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Cuemon
+{
+    /// <summary>
+    /// Provides a way to dynamically enclose/wrap an object to support the decorator pattern.
+    /// </summary>
+    public static class Decorator
+    {
+        /// <summary>
+        /// Encloses the specified <paramref name="inner"/> so that it can be extended without violating SRP.
+        /// </summary>
+        /// <typeparam name="T">The type of the <paramref name="inner"/> to decorate.</typeparam>
+        /// <param name="inner">The type to decorate.</param>
+        /// <returns>An instance of <see cref="Decorator{T}"/>.</returns>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="inner"/> cannot be null.
+        /// </exception>
+        public static Decorator<T> Enclose<T>(T inner)
+        {
+            return new Decorator<T>(inner);
+        }
+
+        /// <summary>
+        /// Syntactic sugar for the rare cases where retrieving properties exposed as methods is a necessity.
+        /// </summary>
+        /// <typeparam name="T">The type to decorate.</typeparam>
+        /// <returns>An instance of <see cref="Decorator{T}"/> where the <see cref="Decorator{T}.Inner"/> defaults to <typeparamref name="T"/>.</returns>
+        public static Decorator<T> Syntactic<T>()
+        {
+            return new Decorator<T>();
+        }
+    }
+
+    /// <summary>
+    /// Provides a generic way to implement the decorator pattern.
+    /// Implements the <see cref="IDecorator{T}" />
+    /// </summary>
+    /// <typeparam name="T">The type of the inner decorated object.</typeparam>
+    /// <seealso cref="IDecorator{T}" />
+    public class Decorator<T> : IDecorator<T>
+    {
+        internal Decorator()
+        {
+            Inner = default;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Decorator{T}"/> class.
+        /// </summary>
+        /// <param name="inner">The type to decorate.</param>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="inner"/> cannot be null.
+        /// </exception>
+        internal Decorator(T inner)
+        {
+            Validator.ThrowIfNull(inner, nameof(inner));
+            Inner = inner;
+        }
+
+        /// <summary>
+        /// Gets the inner object of this decorator.
+        /// </summary>
+        /// <value>The inner object of this decorator.</value>
+        public T Inner { get; }
+    }
+}
---
+++ src/Cuemon.Core/IDecorator.cs
@@ -0,0 +1,17 @@
+﻿namespace Cuemon
+{
+    /// <summary>
+    /// Defines a decorator that exposes the inner decorated type.
+    /// </summary>
+    /// <typeparam name="T">The type of the inner decorated object. This type parameter is covariant. That is, you can use either the type you specified or any type that is more derived.</typeparam>
+    /// <seealso cref="Decorator"/>
+    /// <seealso cref="Decorator{T}"/>
+    public interface IDecorator<out T>
+    {
+        /// <summary>
+        /// Gets the inner object of this decorator.
+        /// </summary>
+        /// <value>The inner object of this decorator.</value>
+        T Inner { get; }
+    }
+}
