0ab11c26201fbb75fc525db6ee2413a7923b51fe
gimlichael <michael@wbpa.dk>
2020-08-15 18:53:40 +0200

    Test that we are compliant with https://rules.sonarsource.com/csharp/RSPEC-3925

---
+++ test/Cuemon.Core.Tests/ExceptionTest.cs
@@ -0,0 +1,54 @@
+﻿using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using Cuemon.Extensions.Xunit;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Cuemon
+{
+    public class ExceptionTest : Test
+    {
+        public ExceptionTest(ITestOutputHelper output = null) : base(output)
+        {
+        }
+
+        [Fact]
+        public void TypeArgumentException_ShouldBeSerializable()
+        {
+            var ex = new TypeArgumentException(Generate.RandomString(10));
+
+            TestOutput.WriteLine(ex.ToString());
+
+            var bf = new BinaryFormatter();
+            using (var ms = new MemoryStream())
+            {
+                bf.Serialize(ms, ex);
+                ms.Position = 0;
+                var desEx = bf.Deserialize(ms) as TypeArgumentException;
+                Assert.Equal(ex.ParamName, desEx.ParamName);
+                Assert.Equal(ex.Message, desEx.Message);
+                Assert.Equal(ex.ToString(), desEx.ToString());
+            }
+        }
+
+        [Fact]
+        public void TypeArgumentOutOfRangeException_ShouldBeSerializable()
+        {
+            var ex = new TypeArgumentOutOfRangeException(Generate.RandomString(10), 42, Generate.RandomString(50));
+
+            TestOutput.WriteLine(ex.ToString());
+
+            var bf = new BinaryFormatter();
+            using (var ms = new MemoryStream())
+            {
+                bf.Serialize(ms, ex);
+                ms.Position = 0;
+                var desEx = bf.Deserialize(ms) as TypeArgumentOutOfRangeException;
+                Assert.Equal(ex.ParamName, desEx.ParamName);
+                Assert.Equal(ex.Message, desEx.Message);
+                Assert.Equal(ex.ActualValue, desEx.ActualValue);
+                Assert.Equal(ex.ToString(), desEx.ToString());
+            }
+        }
+    }
+}
