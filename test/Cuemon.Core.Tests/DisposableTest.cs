546cfa1bdff8a9a5cc63381b2f9870790e250aa7
gimlichael <michael@wbpa.dk>
2020-08-20 00:35:49 +0200

    Added test for Disposable and FinalizeDisposable.

---
+++ test/Cuemon.Core.Tests/DisposableTest.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Cuemon.Assets;
+using Cuemon.Extensions.Xunit;
+using Cuemon.IO;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Cuemon
+{
+    public class DisposableTest : Test
+    {
+        public DisposableTest(ITestOutputHelper output = null) : base(output)
+        {
+            
+        }
+
+        [Fact]
+        public void SafeInvoke_ShouldAbideRuleCA2000()
+        {
+            var guid = Guid.NewGuid();
+            var called = 0;
+            var stream = Disposable.SafeInvoke(() => new MemoryStream(), ms =>
+            {
+                called++;
+                ms.WriteByte(1);
+                ms.Position = 0;
+                return ms;
+            });
+            Assert.NotNull(stream);
+            Assert.Equal(1, called);
+            Assert.Equal(1, stream.Length);
+
+            MemoryStream msRef = null;
+            called = 0;
+            stream = Disposable.SafeInvoke(() => new MemoryStream(), (ms, g) =>
+            {
+                msRef = ms;
+                Assert.Equal(guid, g);
+                throw new InvalidOperationException();
+            }, guid, (exception, g) =>
+            {
+                Assert.Equal(guid, g);
+                Assert.True(exception is InvalidOperationException);
+            });
+            Assert.Equal(0, called);
+            Assert.Null(stream);
+            Assert.Throws<ObjectDisposedException>(() => msRef.Length);
+
+            stream = Disposable.SafeInvoke(() => new MemoryStream(), (ms, n1, n2, n3, n4, n5) =>
+            {
+                called++;
+                ms.Write(Decorator.Enclose($"{n1}{n2}{n3}{n4}{n5}").ToByteArray());
+                ms.Position = 0;
+                return ms;
+            }, 1, 2, 3, 4, 5);
+            Assert.NotNull(stream);
+            Assert.Equal(1, called);
+            Assert.Equal(5, stream.Length);
+            Assert.Equal("12345", Decorator.Enclose(stream).ToEncodedString());
+        }
+
+        [Fact]
+        public async Task SafeInvokeAsync_ShouldAbideRuleCA2000()
+        {
+            var guid = Guid.NewGuid();
+            var called = 0;
+            var stream = await Disposable.SafeInvokeAsync(() => new MemoryStream(), async (ms, ct) =>
+            {
+                called++;
+                await ms.WriteAsync(new byte[] { 1 }, ct);
+                ms.Position = 0;
+                return ms;
+            });
+            Assert.NotNull(stream);
+            Assert.Equal(1, called);
+            Assert.Equal(1, stream.Length);
+
+            MemoryStream msRef = null;
+            called = 0;
+            stream = await Disposable.SafeInvokeAsync(() => new MemoryStream(), (ms, g, ct) =>
+            {
+                msRef = ms;
+                Assert.Equal(guid, g);
+                throw new InvalidOperationException();
+            }, guid, default, (exception, g, ct) =>
+            {
+                Assert.Equal(guid, g);
+                Assert.True(exception is InvalidOperationException);
+                return Task.CompletedTask;
+            });
+            Assert.Equal(0, called);
+            Assert.Null(stream);
+            Assert.Throws<ObjectDisposedException>(() => msRef.Length);
+
+            var ctsShouldFail = new CancellationTokenSource(TimeSpan.FromMilliseconds(5));
+            msRef = null;
+            called = 0;
+            stream = await Disposable.SafeInvokeAsync(() => new MemoryStream(), async (ms, g, ct) =>
+            {
+                msRef = ms;
+                Assert.Equal(guid, g);
+                await Task.Delay(TimeSpan.FromSeconds(1));
+                await ms.WriteAsync(new byte[] { 1 }, ct);
+                ms.Position = 0;
+                return ms;
+            }, guid, ctsShouldFail.Token, (exception, g, ct) =>
+            {
+                Assert.Equal(guid, g);
+                Assert.True(exception is TaskCanceledException);
+                return Task.CompletedTask;
+            });
+            Assert.Equal(0, called);
+            Assert.Null(stream);
+            Assert.Throws<ObjectDisposedException>(() => msRef.Length);
+
+            stream = await Disposable.SafeInvokeAsync(() => new MemoryStream(), async (ms, n1, n2, n3, n4, n5, ct) =>
+            {
+                called++;
+                var bytes = Decorator.Enclose($"{n1}{n2}{n3}{n4}{n5}").ToByteArray();
+                await ms.WriteAsync(bytes, ct);
+                ms.Position = 0;
+                return ms;
+            }, 1, 2, 3, 4, 5, default);
+            Assert.NotNull(stream);
+            Assert.Equal(1, called);
+            Assert.Equal(5, stream.Length);
+            Assert.Equal("12345", Decorator.Enclose(stream).ToEncodedString());
+        }
+
+        [Fact]
+        public void ManagedDisposable_VerifyThatAssetIsBeingDisposed()
+        {
+            ManagedDisposable mdRef = null;
+            using (var md = new ManagedDisposable())
+            {
+                mdRef = md;
+                Assert.NotNull(md.Stream);
+                Assert.Equal(0, md.Stream.Length);
+                Assert.False(mdRef.Disposed);
+            }
+            Assert.NotNull(mdRef);
+            Assert.Null(mdRef.Stream);
+            Assert.True(mdRef.Disposed);
+        }
+
+        private WeakReference<UnmanagedDisposable> unmanaged = null;
+
+        [Fact]
+        public void UnmanagedDisposable_VerifyThatAssetIsBeingDisposedOnFinalize()
+        {
+            Action body = () =>
+            {
+                var o = new UnmanagedDisposable();
+                Assert.NotEqual(IntPtr.Zero, o._handle);
+                unmanaged = new WeakReference<UnmanagedDisposable>(o, true);
+            };
+
+            try
+            {
+                body();
+            }
+            finally
+            {
+                GC.Collect(0, GCCollectionMode.Forced);
+                GC.WaitForPendingFinalizers();
+            }
+
+            Thread.Sleep(500);
+
+            if (unmanaged.TryGetTarget(out var ud2))
+            {
+                Assert.True(ud2.Disposed);
+            }
+        }
+    }
+}
