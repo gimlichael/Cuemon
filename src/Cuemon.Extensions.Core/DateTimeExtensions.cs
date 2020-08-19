5b00f325bb44012f155a99961ddc927884bdea2c
gimlichael <michael@wbpa.dk>
2020-08-19 21:54:56 +0200

    Removed redundant usings.

--- src/Cuemon.Extensions.Core/DateTimeExtensions.cs
+++ src/Cuemon.Extensions.Core/DateTimeExtensions.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using Cuemon.Diagnostics;
 
 namespace Cuemon.Extensions
 {
--- src/Cuemon.Extensions.Core/StringExtensions.cs
+++ src/Cuemon.Extensions.Core/StringExtensions.cs
@@ -5,7 +5,6 @@ using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
-using Cuemon.Diagnostics;
 using Cuemon.Text;
 
 namespace Cuemon.Extensions
