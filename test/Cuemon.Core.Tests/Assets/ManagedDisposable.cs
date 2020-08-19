5157b75dba5c25b390e695cec03cb3dfcec511ec
gimlichael <michael@wbpa.dk>
2020-08-20 01:36:02 +0200

    Disposable assets.

---
+++ test/Cuemon.Core.Tests/Assets/ManagedDisposable.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+
+namespace Cuemon.Assets
+{
+    public class ManagedDisposable : Disposable
+    {
+        public ManagedDisposable()
+        {
+            Stream = new MemoryStream();
+        }
+
+        public MemoryStream Stream { get; private set; }
+
+        protected override void OnDisposeManagedResources()
+        {
+            try
+            {
+                Stream?.Dispose();
+            }
+            finally
+            {
+                Stream = null;
+            }
+        }
+    }
+}
---
+++ test/Cuemon.Core.Tests/Assets/UnmanagedDisposable.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Cuemon.Assets
+{
+    public class UnmanagedDisposable : FinalizeDisposable
+    {
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto,
+            CallingConvention = CallingConvention.StdCall,
+            SetLastError = true)]
+        public static extern IntPtr CreateFile(
+            string lpFileName,
+            uint dwDesiredAccess,
+            uint dwShareMode,
+            IntPtr SecurityAttributes,
+            uint dwCreationDisposition,
+            uint dwFlagsAndAttributes,
+            IntPtr hTemplateFile
+        );
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool CloseHandle(IntPtr hObject);
+
+        public IntPtr _handle = IntPtr.Zero;
+
+        public UnmanagedDisposable()
+        {
+            _handle = CreateFile(@"C:\TestFile.txt",
+                0x80000000, //access read-only
+                1, //share-read
+                IntPtr.Zero,
+                3, //open existing
+                0,
+                IntPtr.Zero);
+        }
+
+        protected override void OnDisposeManagedResources()
+        {
+
+        }
+
+        protected override void OnDisposeUnmanagedResources()
+        {
+            if (_handle != IntPtr.Zero)
+            {
+                CloseHandle(_handle);
+            }
+        }
+    }
+}
