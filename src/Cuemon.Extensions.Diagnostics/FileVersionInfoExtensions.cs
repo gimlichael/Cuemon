da1404503fda6129046379f3b6439fb7a666660f
gimlichael <michael@wbpa.dk>
2020-08-27 01:59:35 +0200

    Refactored to use VersionResult over Version.

--- src/Cuemon.Extensions.Diagnostics/FileVersionInfoExtensions.cs
+++ src/Cuemon.Extensions.Diagnostics/FileVersionInfoExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
+using Cuemon.Reflection;
 
 namespace Cuemon.Extensions.Diagnostics
 {
@@ -9,32 +10,30 @@ namespace Cuemon.Extensions.Diagnostics
     public static class FileVersionInfoExtensions
     {
         /// <summary>
-        /// Returns a <see cref="Version"/> from the specified <paramref name="fvi"/>.
+        /// Returns a <see cref="VersionResult"/> from the specified <paramref name="fvi"/>.
         /// </summary>
         /// <param name="fvi">An instance of <see cref="FileVersionInfo"/>.</param>
-        /// <returns>A <see cref="Version"/> that represents the product version that the <see cref="FileVersionInfo"/> is distributed with.</returns>
-        /// <remarks>Should the specified <paramref name="fvi"/> not contain any product version, a <see cref="Version"/> initialized to 0.0.0.0 is returned.</remarks>
-        public static Version ToProductVersion(this FileVersionInfo fvi)
+        /// <returns>A <see cref="VersionResult"/> that represents the product version that the <see cref="FileVersionInfo"/> is distributed with.</returns>
+        public static VersionResult ToProductVersion(this FileVersionInfo fvi)
         {
             return ToVersion(fvi, info => info.ProductVersion);
         }
 
         /// <summary>
-        /// Returns a <see cref="Version"/> from the specified <paramref name="fvi"/>.
+        /// Returns a <see cref="VersionResult"/> from the specified <paramref name="fvi"/>.
         /// </summary>
         /// <param name="fvi">An instance of <see cref="FileVersionInfo"/>.</param>
-        /// <returns>A <see cref="Version"/> that represents the file version that the <see cref="FileVersionInfo"/> is distributed with.</returns>
-        /// <remarks>Should the specified <paramref name="fvi"/> not contain any file version, a <see cref="Version"/> initialized to 0.0.0.0 is returned.</remarks>
-        public static Version ToFileVersion(this FileVersionInfo fvi)
+        /// <returns>A <see cref="VersionResult"/> that represents the file version that the <see cref="FileVersionInfo"/> is distributed with.</returns>
+        public static VersionResult ToFileVersion(this FileVersionInfo fvi)
         {
             return ToVersion(fvi, info => info.FileVersion);
         }
 
-        private static Version ToVersion(this FileVersionInfo fvi, Func<FileVersionInfo, string> propertySelector)
+        private static VersionResult ToVersion(this FileVersionInfo fvi, Func<FileVersionInfo, string> propertySelector)
         {
             Validator.ThrowIfNull(fvi, nameof(fvi));
             var version = propertySelector(fvi);
-            return new Version(string.IsNullOrWhiteSpace(version) ? "0.0.0.0" : version);
+            return new VersionResult(version);
         }
     }
 }
