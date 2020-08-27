df7b98dcf52d12b07232970555c424e516f0a7b5
gimlichael <michael@wbpa.dk>
2020-08-27 19:33:10 +0200

    Added a few MVP tests.

---
+++ test/Cuemon.Core.Tests/Data/DsvDataReaderTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Cuemon.Extensions.Xunit;
+using Cuemon.Reflection;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Cuemon.Data
+{
+    public class DsvDataReaderTest : Test
+    {
+        public DsvDataReaderTest(ITestOutputHelper output = null) : base(output)
+        {
+        }
+
+        [Fact]
+        public void DsvDataReader_ShouldReadEmbeddedResourceLineByLine()
+        {
+            var file = Decorator.Enclose(typeof(DsvDataReaderTest).Assembly).GetManifestResources("DsvDataReaderTest_Wiki.csv", ManifestResourceMatch.ContainsName).Values.Single();
+            DsvDataReader reader = null;
+            using (reader = new DsvDataReader(new StreamReader(file)))
+            {
+                while (reader.Read())
+                {
+                    TestOutput.WriteLine(reader.ToString());
+                }
+            }
+            Assert.Equal(5, reader.FieldCount);
+            Assert.Equal(4, reader.RowCount);
+            Assert.True(reader.Disposed);
+            Assert.Throws<ObjectDisposedException>(() => reader.Read());
+        }
+    }
+}
---
+++ test/Cuemon.Core.Tests/Security/Cryptography/AesCryptorTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using Cuemon.Extensions.Xunit;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Cuemon.Security.Cryptography
+{
+    public class AesCryptorTest : Test
+    {
+        private readonly byte[] _secretKey;
+        private readonly byte[] _iv;
+
+        public AesCryptorTest(ITestOutputHelper output = null) : base(output)
+        {
+            _secretKey = AesCryptor.GenerateKey();
+            _iv = AesCryptor.GenerateInitializationVector();
+        }
+
+        [Fact]
+        public void AesCryptor_ShouldEncryptAndDecrypt()
+        {
+            var cryptor = new AesCryptor(_secretKey, _iv);
+            var secretMessage = Decorator.Enclose("This is my secret message that needs encryption!").ToByteArray();
+            
+            Assert.True(_secretKey.SequenceEqual(cryptor.Key));
+            Assert.True(_iv.SequenceEqual(cryptor.InitializationVector));
+
+            var enc = cryptor.Encrypt(secretMessage);
+            TestOutput.WriteLine(Convert.ToBase64String(enc));
+
+            var dec = cryptor.Decrypt(enc);
+            TestOutput.WriteLine(Convert.ToBase64String(dec));
+            
+            Assert.True(dec.SequenceEqual(secretMessage));
+        }
+    }
+}
