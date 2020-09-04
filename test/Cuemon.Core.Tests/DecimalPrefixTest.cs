538833095307ff4216c41656e9d5fff8d771b52b
gimlichael <michael@wbpa.dk>
2020-09-04 22:27:01 +0200

    Added some unit tests.

---
+++ test/Cuemon.Core.Tests/DecimalPrefixTest.cs
@@ -0,0 +1,28 @@
+﻿using Cuemon.Extensions.Xunit;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Cuemon
+{
+    public class DecimalPrefixTest : Test
+    {
+        public DecimalPrefixTest(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        [Fact]
+        public void DecimalPrefix_ShouldVerifyMultiplePrefixConstants()
+        {
+            Assert.Equal(DecimalPrefix.Deca.Multiplier, 10d);
+            Assert.Equal(DecimalPrefix.Hecto.Multiplier, 100d);
+            Assert.Equal(DecimalPrefix.Kilo.Multiplier, 1000d);
+            Assert.Equal(DecimalPrefix.Mega.Multiplier, 1000000d);
+            Assert.Equal(DecimalPrefix.Giga.Multiplier, 1000000000d);
+            Assert.Equal(DecimalPrefix.Tera.Multiplier, 1000000000000d);
+            Assert.Equal(DecimalPrefix.Peta.Multiplier, 1000000000000000d);
+            Assert.Equal(DecimalPrefix.Exa.Multiplier, 1000000000000000000d);
+            Assert.Equal(DecimalPrefix.Zetta.Multiplier, 1000000000000000000000d);
+            Assert.Equal(DecimalPrefix.Yotta.Multiplier, 1000000000000000000000000d);
+        }
+    }
+}
