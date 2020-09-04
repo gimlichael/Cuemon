0b0bcb3c52ab4ad181c7adc4c5b2d09cba4a4489
gimlichael <michael@wbpa.dk>
2020-09-04 18:29:43 +0200

    Added properties with assembly directives (consistency).

---
+++ src/Cuemon.AspNetCore.Authentication/Properties/AssemblyInfo.cs
@@ -0,0 +1,4 @@
+﻿using System.Runtime.InteropServices;
+
+[assembly: ComVisible(false)]
+[assembly: Guid("cbba5566-2383-459e-86d5-8a1fc232e8ed")]
---
+++ src/Cuemon.AspNetCore.Razor/Properties/AssemblyInfo.cs
@@ -0,0 +1,4 @@
+﻿using System.Runtime.InteropServices;
+
+[assembly: ComVisible(false)]
+[assembly: Guid("74d499ee-862e-412b-9a2d-fb2fe30d518e")]
