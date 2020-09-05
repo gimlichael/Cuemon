968c7032e9578c756f751352a86d3678eea343c9
gimlichael <michael@wbpa.dk>
2020-09-05 17:42:20 +0200

    New addition to the Cuemon assmebly family.

---
+++ src/Cuemon.Extensions.Hosting/HostEnvironmentExtensions.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Hosting;
+
+namespace Cuemon.Extensions.Hosting
+{
+    #if NETCOREAPP
+    /// <summary>
+    /// Extension methods for the <see cref="IHostEnvironment"/> interface.
+    /// </summary>
+    public static class HostEnvironmentExtensions
+    {
+        /// <summary>
+        /// Determines whether the specified <paramref name="environment"/> is equal to <c>LocalDevelopment</c>.
+        /// </summary>
+        /// <param name="environment">The <see cref="IHostingEnvironment"/> to extend.</param>
+        /// <returns><c>true</c> if <paramref name="environment"/> is <c>LocalDevelopment</c>; otherwise <c>false</c></returns>
+        public static bool IsLocalDevelopment(this IHostEnvironment environment)
+        {
+            return environment.IsEnvironment("LocalDevelopment");
+        }
+
+        /// <summary>
+        /// Determines whether the specified <paramref name="environment"/> is different from <c>Production</c>.
+        /// </summary>
+        /// <param name="environment">The <see cref="IHostingEnvironment"/> to extend.</param>
+        /// <returns><c>true</c> if <paramref name="environment"/> is not <c>Production</c>; otherwise <c>false</c></returns>
+        public static bool IsNonProduction(this IHostEnvironment environment)
+        {
+            return !environment.IsProduction();
+        }
+    }
+    #endif
+}
---
+++ src/Cuemon.Extensions.Hosting/Properties/AssemblyInfo.cs
@@ -0,0 +1,4 @@
+﻿using System.Runtime.InteropServices;
+
+[assembly: ComVisible(false)]
+[assembly: Guid("e9bc4285-5627-4b81-b00b-712b38757ede")]
