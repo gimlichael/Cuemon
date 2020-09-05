d0ffc8eae47b8f27258355a5432daba798e87176
gimlichael <michael@wbpa.dk>
2020-09-05 21:33:06 +0200

    Added unit.

---
+++ test/Cuemon.Extensions.Xunit.Tests/TestTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Xunit;
+using Xunit.Abstractions;
+using Xunit.Sdk;
+
+namespace Cuemon.Extensions.Xunit
+{
+    public class TestTest : Test
+    {
+        private const string ExpectedStringValue = "AllIsGood";
+        private bool _onDisposeManagedResourcesCalled;
+
+        public TestTest(ITestOutputHelper output = null) : base(output)
+        {
+        }
+
+        [Fact]
+        public void Test_ShouldHaveTestOutput()
+        {
+            Assert.True(HasTestOutput);
+            Assert.IsAssignableFrom<ITestOutputHelper>(TestOutput);
+            Assert.IsType<TestOutputHelper>(TestOutput);
+        }
+
+        [Fact]
+        public void Test_ShouldInvokeDispose()
+        {
+            Assert.Equal(ExpectedStringValue, DisposeSensitiveMethod());
+            Assert.False(_onDisposeManagedResourcesCalled);
+            Dispose();
+            Assert.True(_onDisposeManagedResourcesCalled);
+            Assert.True(Disposed);
+            Assert.Throws<ObjectDisposedException>(DisposeSensitiveMethod);
+        }
+
+        public string DisposeSensitiveMethod()
+        {
+            if (Disposed) { throw new ObjectDisposedException(GetType().FullName); }
+            return ExpectedStringValue;
+        }
+
+        protected override void OnDisposeManagedResources()
+        {
+            _onDisposeManagedResourcesCalled = true;
+            base.OnDisposeManagedResources();
+        }
+    }
+}
