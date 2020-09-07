2b1372c95e5e20bca5925506e1a112b5601fc3d2
gimlichael <michael@wbpa.dk>
2020-09-08 00:33:53 +0200

    Cleanup.

--- src/Cuemon.Xml/GlobalSuppressions.cs
+++ src/Cuemon.Xml/GlobalSuppressions.cs
@@ -6,6 +6,5 @@
 using System.Diagnostics.CodeAnalysis;
 
 [assembly: SuppressMessage("Major Code Smell", "S907:\"goto\" statement should not be used", Justification = "Legacy implementation.", Scope = "member", Target = "~M:Cuemon.Xml.Serialization.Converters.DefaultXmlConverter.ParseReadXmlDefault(System.Xml.XmlReader,System.Type)~System.Object")]
-[assembly: SuppressMessage("Major Code Smell", "S907:\"goto\" statement should not be used", Justification = "Legacy implementation.", Scope = "member", Target = "~M:Cuemon.Xml.XmlDataReader.ReadNext(System.Boolean)~System.Boolean")]
 [assembly: SuppressMessage("Major Code Smell", "S907:\"goto\" statement should not be used", Justification = "Legacy implementation.", Scope = "member", Target = "~M:Cuemon.Xml.XmlReaderDecoratorExtensions.ToHierarchy(Cuemon.IDecorator{System.Xml.XmlReader})~Cuemon.IHierarchy{Cuemon.DataPair}")]
 [assembly: SuppressMessage("Minor Code Smell", "S3626:Jump statements should not be redundant", Justification = "False-positive.", Scope = "member", Target = "~M:Cuemon.Xml.XmlStreamFactory.CreateStream(System.Action{System.Xml.XmlWriter},System.Action{System.Xml.XmlWriterSettings})~System.IO.Stream")]
