291ddabd26fccf66c63736bf36286ded5ea2d7da
gimlichael <michael@wbpa.dk>
2020-09-14 00:11:34 +0200

    Added initial DocFx build using Docker.

---
+++ BuildDocfxImage.ps1
@@ -0,0 +1,4 @@
+﻿docfx metadata docfx/docfx.json
+docker build -t cuemon-docfx:6.0.0  -f Dockerfile.docfx .
+remove-item docfx/obj -recurse
+get-childItem -recurse -path docfx/api -include *.yml, .manifest | remove-item
