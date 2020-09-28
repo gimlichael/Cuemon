9f1e44fdff169258765fb20e29b60afe3bd2e7f8
gimlichael <michael@wbpa.dk>
2020-09-28 22:16:37 +0200

    Renamed "parser" to "extractor".

--- src/Cuemon.Extensions.Newtonsoft.Json/JDataResultExtensions.cs
+++ src/Cuemon.Extensions.Newtonsoft.Json/JDataResultExtensions.cs
@@ -10,16 +10,16 @@ namespace Cuemon.Extensions.Newtonsoft.Json
     public static class JDataResultExtensions
     {
         /// <summary>
-        /// Extracts one or more values from JSON objects using the specified <paramref name="propertyNames"/> and <paramref name="parser"/> delegate.
+        /// Extracts one or more values from JSON objects using the specified <paramref name="propertyNames"/> and <paramref name="extractor"/> delegate.
         /// </summary>
         /// <param name="source">The sequence of <see cref="JDataResult"/> to parse.</param>
         /// <param name="propertyNames">The comma-delimited property names (JSON path) to math in a JSON document.</param>
-        /// <param name="parser">The delegate that will extract values from <paramref name="propertyNames"/>.</param>
-        public static void ExtractObjectValues(this IEnumerable<JDataResult> source, string propertyNames, Action<IDictionary<string, JDataResult>> parser)
+        /// <param name="extractor">The delegate that will extract values from <paramref name="propertyNames"/>.</param>
+        public static void ExtractObjectValues(this IEnumerable<JDataResult> source, string propertyNames, Action<IDictionary<string, JDataResult>> extractor)
         {
             Validator.ThrowIfNull(source, nameof(source));
             Validator.ThrowIfNullOrWhitespace(propertyNames, nameof(propertyNames));
-            Validator.ThrowIfNull(parser, nameof(parser));
+            Validator.ThrowIfNull(extractor, nameof(extractor));
 
             var names = propertyNames.Split(',').Select(s => s.Trim()).ToList();
             var partial = new List<JDataResult>();
@@ -33,23 +33,23 @@ namespace Cuemon.Extensions.Newtonsoft.Json
 
                 if (partial.Count == names.Count)
                 {
-                    parser(partial.ToDictionary(jk => jk.PropertyName, jv => jv));
+                    extractor(partial.ToDictionary(jk => jk.PropertyName, jv => jv));
                     partial.Clear();
                 }
             }
         }
 
         /// <summary>
-        /// Extracts one or more values from JSON arrays using the specified <paramref name="propertyNames"/> and <paramref name="parser"/> delegate.
+        /// Extracts one or more values from JSON arrays using the specified <paramref name="propertyNames"/> and <paramref name="extractor"/> delegate.
         /// </summary>
         /// <param name="source">The sequence of <see cref="JDataResult"/> to parse.</param>
         /// <param name="propertyNames">The comma-delimited property names (JSON path) to math in a JSON document.</param>
-        /// <param name="parser">The delegate that will extract values from <paramref name="propertyNames"/>.</param>
-        public static void ExtractArrayValues(this IEnumerable<JDataResult> source, string propertyNames, Action<IDictionary<string, IEnumerable<JDataResult>>> parser) 
+        /// <param name="extractor">The delegate that will extract values from <paramref name="propertyNames"/>.</param>
+        public static void ExtractArrayValues(this IEnumerable<JDataResult> source, string propertyNames, Action<IDictionary<string, IEnumerable<JDataResult>>> extractor) 
         {
             Validator.ThrowIfNull(source, nameof(source));
             Validator.ThrowIfNullOrWhitespace(propertyNames, nameof(propertyNames));
-            Validator.ThrowIfNull(parser, nameof(parser));
+            Validator.ThrowIfNull(extractor, nameof(extractor));
 
             var names = propertyNames.Split(',').Select(s => s.Trim()).ToList();
             var partial = new List<JDataResult>();
@@ -63,7 +63,7 @@ namespace Cuemon.Extensions.Newtonsoft.Json
 
                 if (partial.Count == names.Count)
                 {
-                    parser(partial.ToDictionary(jk => jk.PropertyName, jv => jv.Children as IEnumerable<JDataResult>));
+                    extractor(partial.ToDictionary(jk => jk.PropertyName, jv => jv.Children as IEnumerable<JDataResult>));
                     partial.Clear();
                 }
             }
