021d97a0c8db7409c26979b5ae4f140ea0854285
gimlichael <michael@wbpa.dk>
2020-09-29 02:24:38 +0200

    Unit test for both JSON and XML formatters.

---
+++ test/Cuemon.Extensions.Newtonsoft.Json.Tests/Formatters/JsonFormatterOptionsTest.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using Cuemon.Extensions.Xunit;
+using Newtonsoft.Json;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Cuemon.Extensions.Newtonsoft.Json.Formatters
+{
+    public class JsonFormatterOptionsTest : Test
+    {
+        public JsonFormatterOptionsTest(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        [Fact]
+        public void DefaultConverters_ShouldHaveSameAmountOfDefaultConverters()
+        {
+            var defaultConverters = new List<JsonConverter>();
+            JsonFormatterOptions.DefaultConverters(defaultConverters);
+
+            var x = new JsonFormatterOptions();
+            var y = new JsonFormatterOptions();
+            var bootstrapInvocationList = JsonFormatterOptions.DefaultConverters.GetInvocationList().Length;
+            
+            Assert.Equal(4, defaultConverters.Count);
+            Assert.Equal(1, bootstrapInvocationList);
+            Assert.Equal(2, x.Settings.Converters.Count - defaultConverters.Count);
+            Assert.Equal(2, y.Settings.Converters.Count - defaultConverters.Count);
+
+            Assert.Equal(x.Settings.Converters.Count, y.Settings.Converters.Count);
+        }
+    }
+}
---
+++ test/Cuemon.Xml.Tests/Serialization/Formatters/XmlFormatterOptionsTest.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using Cuemon.Extensions.Xunit;
+using Cuemon.Xml.Serialization.Converters;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Cuemon.Xml.Serialization.Formatters
+{
+    public class XmlFormatterOptionsTest : Test
+    {
+        public XmlFormatterOptionsTest(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        [Fact]
+        public void DefaultConverters_ShouldHaveSameAmountOfDefaultConverters()
+        {
+            var defaultConverters = new List<XmlConverter>();
+            XmlFormatterOptions.DefaultConverters(defaultConverters);
+
+            var x = new XmlFormatterOptions();
+            var y = new XmlFormatterOptions();
+            var bootstrapInvocationList = XmlFormatterOptions.DefaultConverters.GetInvocationList().Length;
+            
+            Assert.Equal(6, defaultConverters.Count);
+            Assert.Equal(1, bootstrapInvocationList);
+            Assert.Equal(1, x.Settings.Converters.Count - defaultConverters.Count);
+            Assert.Equal(1, y.Settings.Converters.Count - defaultConverters.Count);
+
+            Assert.Equal(x.Settings.Converters.Count, y.Settings.Converters.Count);
+        }
+    }
+}
