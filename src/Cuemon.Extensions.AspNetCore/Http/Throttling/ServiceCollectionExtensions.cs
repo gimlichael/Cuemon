9d1dcf56778be61187a32734f5b14e9c3e816aa6
gimlichael <michael@wbpa.dk>
2020-10-04 02:48:59 +0200

    Added one overload for consistency.

--- src/Cuemon.Extensions.AspNetCore/Http/Throttling/ServiceCollectionExtensions.cs
+++ src/Cuemon.Extensions.AspNetCore/Http/Throttling/ServiceCollectionExtensions.cs
@@ -9,13 +9,24 @@ namespace Cuemon.Extensions.AspNetCore.Http.Throttling
     public static class ServiceCollectionExtensions
     {
         /// <summary>
-        /// Adds a memory-based throttling cache service to the specified <see cref="IServiceCollection"/>.
+        /// Adds a <see cref="MemoryThrottlingCache"/> service to the specified <see cref="IServiceCollection"/>.
         /// </summary>
         /// <param name="services">The <see cref="IServiceCollection"/> to add services to.</param>
         /// <returns>An <see cref="IServiceCollection"/> that can be used to further configure other services.</returns>
-        public static IServiceCollection AddMemoryThrottling(this IServiceCollection services)
+        public static IServiceCollection AddMemoryThrottlingCache(this IServiceCollection services)
         {
-            services.AddSingleton<IThrottlingCache, MemoryThrottlingCache>();
+            return services.AddThrottlingCache<MemoryThrottlingCache>();
+        }
+
+        /// <summary>
+        /// Adds a throttling cache service to the specified <see cref="IServiceCollection"/>.
+        /// </summary>
+        /// <param name="services">The <see cref="IServiceCollection"/> to add services to.</param>
+        /// <returns>An <see cref="IServiceCollection"/> that can be used to further configure other services.</returns>
+        public static IServiceCollection AddThrottlingCache<T>(this IServiceCollection services) where T : class, IThrottlingCache
+        {
+            Validator.ThrowIfNull(services, nameof(services));
+            services.AddSingleton<IThrottlingCache, T>();
             return services;
         }
     }
