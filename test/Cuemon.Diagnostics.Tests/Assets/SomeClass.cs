fdc7c514bdd140809452d08ace497e93aaa53bed
gimlichael <michael@wbpa.dk>
2020-10-04 21:27:53 +0200

    Minor improvements to ExceptionDescriptor and added long overdue unit testing.

---
+++ test/Cuemon.Diagnostics.Tests/Assets/SomeClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Cuemon.Extensions.Collections.Generic;
+
+namespace Cuemon.Diagnostics.Assets
+{
+    public class SomeClass
+    {
+        [ArgumentNullExceptionDescriptor]
+        public string[] StringToArray(string value)
+        {
+            Validator.ThrowIfNull(value, nameof(value), "Null is a no-go!");
+            return value.Split(',');
+        }
+
+        [ExceptionDescriptor(typeof(ArgumentNullException), Code = "ArgumentNullException", Message = "The value cannot be null (none-resource).", MessageResourceName = "FaultDescriptor_ArgumentNullException", ResourceType = typeof(TestContext))]
+        public string Shuffle(string value)
+        {
+            Validator.ThrowIfNull(value, nameof(value), "Null is a no-go!");
+            return string.Concat(value.Shuffle());
+        }
+
+        [ExceptionDescriptor(typeof(ArgumentNullException), Code = "ArgumentNullException", Message = "The value cannot be null (none-resource).")]
+        public string ShuffleNoLoc(string value)
+        {
+            Validator.ThrowIfNull(value, nameof(value), "Null is a no-go!");
+            return string.Concat(value.Shuffle());
+        }
+    }
+}
