580cb48bd108b5363d856f1fd49bd8d42bd8b349
Michael Mortensen <michael@geekle.io>
2020-10-12 21:13:53 +0200

    Typo fix.

--- src/Cuemon.Data.Integrity/IEntityDataIntegrity.cs
+++ src/Cuemon.Data.Integrity/IEntityDataIntegrity.cs
@@ -1,7 +1,7 @@
 ﻿namespace Cuemon.Data.Integrity
 {
     /// <summary>
-    /// An interface that represents the integrity od data that is normally associated with an entity/resource.
+    /// An interface that represents the integrity of data that is normally associated with an entity/resource.
     /// </summary>
     /// <seealso cref="IDataIntegrity" />
     public interface IEntityDataIntegrity : IDataIntegrity
