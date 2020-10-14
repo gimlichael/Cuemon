2a811b90735b176d4deb635e0d9da2ccd86a3800
Michael Mortensen <michael@geekle.io>
2020-10-15 00:55:32 +0200

    Unit testing of Mvc assembly.

---
+++ test/Cuemon.AspNetCore.Mvc.Tests/Assets/FakeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Cuemon.AspNetCore.Mvc.Assets
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FakeController : ControllerBase
+    {
+        [HttpGet]
+        [BearerThrottlingSentinel(10, 5, TimeUnit.Seconds)]
+        public IActionResult Get()
+        {
+            return Ok("Unit Test");
+        }
+    }
+}
---
+++ test/Cuemon.AspNetCore.Mvc.Tests/Filters/Throttling/ThrottlingSentinelAttributeTest.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Cuemon.AspNetCore.Http.Throttling;
+using Cuemon.AspNetCore.Mvc.Assets;
+using Cuemon.Extensions.AspNetCore.Http.Throttling;
+using Cuemon.Extensions.Xunit.Hosting.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Cuemon.AspNetCore.Mvc.Filters.Throttling
+{
+    public class ThrottlingSentinelAttributeTest : AspNetCoreHostTest<AspNetCoreHostFixture>
+    {
+        private readonly IServiceProvider _provider;
+
+        public ThrottlingSentinelAttributeTest(AspNetCoreHostFixture hostFixture, ITestOutputHelper output) : base(hostFixture, output)
+        {
+            _provider = hostFixture.ServiceProvider;
+        }
+
+        [Fact]
+        public async Task Bearer_ShouldThrottleWhenQuotaIsExceeded()
+        {
+            var cache = _provider.GetRequiredService<IThrottlingCache>();
+            var client = Host.GetTestClient();
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", nameof(Bearer_ShouldThrottleWhenQuotaIsExceeded));
+
+            var te = await Assert.ThrowsAsync<ThrottlingException>(async () =>
+            {
+                for (var i = 0; i < 15; i++)
+                {
+                    var result = await client.GetAsync("/fake");
+                    Assert.Equal(StatusCodes.Status200OK, (int)result.StatusCode);
+                    Assert.Equal("Unit Test", await result.Content.ReadAsStringAsync());
+                }
+            });
+
+
+            var ce = cache[nameof(Bearer_ShouldThrottleWhenQuotaIsExceeded)];
+            
+            Assert.InRange(ce.Total, te.RateLimit, 15);
+            Assert.Equal(ce.Quota.RateLimit, te.RateLimit);
+            Assert.Equal(ce.Quota.Window, TimeSpan.FromSeconds(5));
+            Assert.Equal(StatusCodes.Status429TooManyRequests, te.StatusCode);
+        }
+
+        [Fact]
+        public async Task Bearer_ShouldThrottleAndThenRehydrateAfterWindowHasPassed()
+        {
+            var cache = _provider.GetRequiredService<IThrottlingCache>();
+            var client = Host.GetTestClient();
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", nameof(Bearer_ShouldThrottleAndThenRehydrateAfterWindowHasPassed));
+
+            var te = await Assert.ThrowsAsync<ThrottlingException>(async () =>
+            {
+                for (var i = 0; i < 15; i++)
+                {
+                    var result = await client.GetAsync("/fake");
+                    Assert.Equal(StatusCodes.Status200OK, (int)result.StatusCode);
+                    Assert.Equal("Unit Test", await result.Content.ReadAsStringAsync());
+                }
+            });
+
+
+            var ce = cache[nameof(Bearer_ShouldThrottleAndThenRehydrateAfterWindowHasPassed)];
+            
+            Assert.InRange(ce.Total, te.RateLimit, 15);
+            Assert.Equal(ce.Quota.RateLimit, te.RateLimit);
+            Assert.Equal(ce.Quota.Window, TimeSpan.FromSeconds(5));
+            Assert.Equal(StatusCodes.Status429TooManyRequests, te.StatusCode);
+
+            await Task.Delay(TimeSpan.FromSeconds(5));
+
+            for (var i = 0; i < 5; i++)
+            {
+                var result = await client.GetAsync("/fake");
+                Assert.Equal(StatusCodes.Status200OK, (int)result.StatusCode);
+                Assert.Equal("Unit Test", await result.Content.ReadAsStringAsync());
+            }
+
+            Assert.Equal(5, ce.Total);
+        }
+
+        public override void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers().AddApplicationPart(typeof(FakeController).Assembly);
+            services.AddFakeHttpContextAccessor(ServiceLifetime.Singleton);
+            services.AddMemoryThrottlingCache();
+        }
+
+        public override void ConfigureApplication(IApplicationBuilder app)
+        {
+            app.UseRouting();
+            app.UseEndpoints(routes =>
+            {
+                routes.MapControllers();
+            });
+        }
+    }
+}
