920e7ed0712e7bbf1d9ab078d6773081ff1b3462
Michael Mortensen <michael@geekle.io>
2020-10-15 18:45:48 +0200

    Extended controller.

--- test/Cuemon.AspNetCore.Mvc.Tests/Assets/FakeController.cs
+++ test/Cuemon.AspNetCore.Mvc.Tests/Assets/FakeController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Threading.Tasks;
+using Cuemon.AspNetCore.Mvc.Filters.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Cuemon.AspNetCore.Mvc.Assets
 {
@@ -16,6 +19,21 @@ namespace Cuemon.AspNetCore.Mvc.Assets
         [HttpGet("it")]
         public IActionResult GetIt()
         {
+            return Ok("Unit Test");
+        }
+
+        [HttpGet("oneSecond")]
+        public async Task<IActionResult> GetAfter1Second()
+        {
+            await Task.Delay(TimeSpan.FromSeconds(1));
+            return Ok("Unit Test");
+        }
+
+        [ServerTiming(Name = "action-result")]
+        [HttpGet("oneSecondAttribute")]
+        public async Task<IActionResult> GetAfter1SecondDecorated()
+        {
+            await Task.Delay(TimeSpan.FromSeconds(1));
             return Ok("Unit Test");
         }
     }
