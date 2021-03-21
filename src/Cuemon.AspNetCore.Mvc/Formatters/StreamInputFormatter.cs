aaab7e01e40baad23543df24bcd27d4f1029eb5b
Michael Mortensen <michael@geekle.io>
2021-03-22 00:03:14 +0100

    Removed duplicated blocks of lines of code with a refactoring. Added unit test to verify the change did not cause any issues.

---
+++ src/Cuemon.AspNetCore.Mvc/Formatters/StreamInputFormatter.cs
@@ -0,0 +1,54 @@
+﻿using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Cuemon.AspNetCore.Mvc.Filters.Diagnostics;
+using Cuemon.Reflection;
+using Cuemon.Runtime.Serialization.Formatters;
+using Microsoft.AspNetCore.Mvc.Formatters;
+
+namespace Cuemon.AspNetCore.Mvc.Formatters
+{
+    /// <summary>
+    /// Provides a way to read an object from a request body with a text format with the constraint that <typeparamref name="TFormatter"/> must be assignable from <see cref="Formatter{Stream}"/>.
+    /// Implements the <see cref="ConfigurableInputFormatter{TOptions}" />
+    /// </summary>
+    /// <typeparam name="TFormatter">The type of the <seealso cref="Formatter{TFormat}"/>.</typeparam>
+    /// <typeparam name="TOptions">The type of the configured options.</typeparam>
+    public abstract class StreamInputFormatter<TFormatter, TOptions> : ConfigurableInputFormatter<TOptions>
+        where TFormatter : Formatter<Stream>
+        where TOptions : class, new()
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="StreamInputFormatter{TFormatter, TOptions}"/> class.
+        /// </summary>
+        /// <param name="options">The <typeparamref name="TOptions"/> which need to be configured.</param>
+        protected StreamInputFormatter(TOptions options) : base(options)
+        {
+            SupportedEncodings.Add(Encoding.UTF8);
+            SupportedEncodings.Add(Encoding.Unicode);
+        }
+
+        /// <summary>
+        /// Reads an object from the request body.
+        /// </summary>
+        /// <param name="context">The <see cref="InputFormatterContext" />.</param>
+        /// <param name="encoding">The <see cref="Encoding" /> used to read the request body.</param>
+        /// <returns>A <see cref="Task" /> that on completion deserializes the request body.</returns>
+        /// <remarks>In this implementation <paramref name="encoding"/> is disregarded.</remarks>
+        public override async Task<InputFormatterResult> ReadRequestBodyAsync(InputFormatterContext context, Encoding encoding)
+        {
+            Validator.ThrowIfNull(context, nameof(context));
+            var requestBody = new MemoryStream();
+            #if NETCOREAPP
+            await context.HttpContext.Request.BodyReader.CopyToAsync(requestBody).ConfigureAwait(false);
+            #else
+            await context.HttpContext.Request.Body.CopyToAsync(requestBody).ConfigureAwait(false);
+            #endif
+            requestBody.Position = 0;
+            var formatter = ActivatorFactory.CreateInstance<TOptions, TFormatter>(Options);
+            var deserializedObject = formatter.Deserialize(requestBody, context.ModelType);
+            context.HttpContext.Items.Add(FaultDescriptorFilter.HttpContextItemsKeyForCapturedRequestBody, requestBody);
+            return await InputFormatterResult.SuccessAsync(deserializedObject).ConfigureAwait(false);
+        }
+    }
+}
---
+++ test/Cuemon.Extensions.AspNetCore.Mvc.Formatters.Xml.Tests/Assets/WeatherForecast.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace Cuemon.Extensions.AspNetCore.Mvc.Formatters.Xml.Assets
+{
+    public class WeatherForecast
+    {
+        public WeatherForecast()
+        {
+            Date = DateTime.UtcNow;
+            TemperatureC = Generate.RandomNumber(-20, 55);
+            Summary = "Scorching";
+        }
+
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+}
