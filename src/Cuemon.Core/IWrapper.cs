gimlichael/Cuemon

commit a2315153dc8aa583f9c1382423bce9f115fddfe6

Changed because of https://rules.sonarsource.com/csharp/RSPEC-2292; but did not follow recomendation due to virtual members.

src/Cuemon.Core/Wrapper.cs
@@ -82,7 +82,7 @@ namespace Cuemon
     {
         private T _instance;
         private Type _instanceType;
-        private MemberInfo _memberReference;
+        private readonly MemberInfo _memberReference;
         private readonly IDictionary<string, object> _data = new Dictionary<string, object>();
 
         #region Constructors
@@ -113,8 +113,8 @@ namespace Cuemon
         /// <value>The object that this wrapper represents.</value>
         public virtual T Instance
         {
-            get { return _instance; }
-            protected set { _instance = value; }
+            get => _instance;
+            protected set => _instance = value;
         }
 
         /// <summary>
@@ -123,37 +123,28 @@ namespace Cuemon
         /// <value>The type of the that this wrapper represents.</value>
         public virtual Type InstanceType
         {
-            get { return _instanceType; }
-            protected set { _instanceType = value; }
+            get => _instanceType;
+            protected set => _instanceType = value;
         }
 
         /// <summary>
         /// Gets the member from where <see cref="Instance"/> was referenced.
         /// </summary>
         /// <value>The member from where <see cref="Instance"/> was referenced.</value>
-        public virtual MemberInfo MemberReference
-        {
-            get { return _memberReference; }
-            set { _memberReference = value; }
-        }
+        public virtual MemberInfo MemberReference { get; set; }
 
         /// <summary>
         /// Gets a value indicating whether this instance has a member reference.
         /// </summary>
         /// <value><c>true</c> if this instance has a member reference; otherwise, <c>false</c>.</value>
-        public virtual bool HasMemberReference
-        {
-            get { return (_memberReference != null); }
-        }
+        public virtual bool HasMemberReference => (_memberReference != null);
 
         /// <summary>
         /// Gets a collection of key/value pairs that provide additional user-defined information about this wrapper object.
         /// </summary>
         /// <value>An object that implements the <see cref="IDictionary{TKey,TValue}"/> interface and contains a collection of user-defined key/value pairs.</value>
-        public virtual IDictionary<string, object> Data
-        {
-            get { return _data; }
-        }
+        public virtual IDictionary<string, object> Data => _data;
+
         #endregion
 
         #region Methods

commit 8acea5cae8a1c49fec58329f296b8382722efd19

Fixed bug introduced with a2315153dc.

src/Cuemon.Core/IWrapper.cs
@@ -26,7 +26,7 @@ namespace Cuemon
         /// Gets the member from where <see cref="Instance"/> was referenced.
         /// </summary>
         /// <value>The member from where <see cref="Instance"/> was referenced.</value>
-        MemberInfo MemberReference { get; set; }
+        MemberInfo MemberReference { get; }
 
         /// <summary>
         /// Gets a value indicating whether this instance has a member reference.

src/Cuemon.Core/Wrapper.cs
@@ -82,8 +82,7 @@ namespace Cuemon
     {
         private T _instance;
         private Type _instanceType;
-        private readonly MemberInfo _memberReference;
-        private readonly IDictionary<string, object> _data = new Dictionary<string, object>();
+        private MemberInfo _memberReference;
 
         #region Constructors
         /// <summary>
@@ -131,7 +130,11 @@ namespace Cuemon
         /// Gets the member from where <see cref="Instance"/> was referenced.
         /// </summary>
         /// <value>The member from where <see cref="Instance"/> was referenced.</value>
-        public virtual MemberInfo MemberReference { get; set; }
+        public virtual MemberInfo MemberReference
+        {
+            get => _memberReference;
+            protected set => _memberReference = value;
+        }
 
         /// <summary>
         /// Gets a value indicating whether this instance has a member reference.
@@ -143,7 +146,7 @@ namespace Cuemon
         /// Gets a collection of key/value pairs that provide additional user-defined information about this wrapper object.
         /// </summary>
         /// <value>An object that implements the <see cref="IDictionary{TKey,TValue}"/> interface and contains a collection of user-defined key/value pairs.</value>
-        public virtual IDictionary<string, object> Data => _data;
+        public virtual IDictionary<string, object> Data { get; } = new Dictionary<string, object>();
 
         #endregion
 

src/Cuemon.Xml/Serialization/XmlSerializer.cs
@@ -55,7 +55,6 @@ namespace Cuemon.Xml.Serialization
 
         internal void Serialize(XmlWriter writer, object value, Type objectType)
         {
-            
             GetWriterConverter(objectType).WriteXml(writer, value);
         }
 

src/Cuemon.Xml/Serialization/XmlWrapper.cs
@@ -58,7 +58,7 @@ namespace Cuemon.Xml.Serialization
         public override MemberInfo MemberReference
         {
             get => base.MemberReference;
-            set => base.MemberReference = value;
+            protected set => base.MemberReference = value;
         }
     }
 }