09ed643cae106e873e203bfda9b4bf09c94808ac
Michael Mortensen <michael@geekle.io>
2021-03-27 22:07:51 +0100

    Added unit test for Cuemon.Extensions.Diagnostics assembly.

---
+++ test/Cuemon.Extensions.Diagnostics.Tests/ExceptionDescriptorExtensionsTest.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using Cuemon.Collections.Generic;
+using Cuemon.Diagnostics;
+using Cuemon.Extensions.Xunit;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Cuemon.Extensions.Diagnostics
+{
+    public class ExceptionDescriptorExtensionsTest : Test
+    {
+        public ExceptionDescriptorExtensionsTest(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        [Fact]
+        public void ToInsightsString_ShouldReturnDetailedExceptionString_WithAllCaptures()
+        {
+            InsufficientMemoryException ime = null;
+            try
+            {
+                throw new InsufficientMemoryException();
+            }
+            catch (InsufficientMemoryException e)
+            {
+                ime = ExceptionInsights.Embed(e, snapshots: SystemSnapshots.CaptureAll);
+            }
+
+            var sut1 = ExceptionDescriptor.Extract(ime);
+            var sut2 = sut1.ToInsightsString();
+
+            TestOutput.WriteLine(sut2);
+
+            Assert.Contains(@"Error
+	Code: UnhandledException
+	Message: An unhandled exception occurred.", sut2);
+            Assert.Contains(@$"Failure
+	System.InsufficientMemoryException
+		Source: Cuemon.Extensions.Diagnostics.Tests
+		Message: Insufficient memory to continue the execution of the program.
+		Stack:
+			at Cuemon.Extensions.Diagnostics.ExceptionDescriptorExtensionsTest.{nameof(ToInsightsString_ShouldReturnDetailedExceptionString_WithAllCaptures)}()", sut2);
+            Assert.Contains(@$"Evidence
+	Thrower:
+		Cuemon.Extensions.Diagnostics.ExceptionDescriptorExtensionsTest.{nameof(ToInsightsString_ShouldReturnDetailedExceptionString_WithAllCaptures)}()", sut2);
+            Assert.Contains(@"	Thread:", sut2);
+            Assert.Contains(@"	Process:", sut2);
+            Assert.Contains(@"	Environment:", sut2);
+        }
+
+        [Theory]
+        [MemberData(nameof(GetData))]
+        public void ToInsightsString_ShouldReturnDetailedExceptionString_WithCaptures_MakeUseOfIncludeOptions(ExceptionDescriptorOptions options, SystemSnapshots snapshots)
+        {
+            InsufficientMemoryException ime = null;
+            try
+            {
+                throw new InsufficientMemoryException();
+            }
+            catch (InsufficientMemoryException e)
+            {
+                ime = ExceptionInsights.Embed(e, Arguments.ToArray(options, snapshots), snapshots);
+            }
+
+            var sut1 = ExceptionDescriptor.Extract(ime);
+            var sut2 = sut1.ToInsightsString(Patterns.ConfigureRevert(options));
+
+            TestOutput.WriteLine(sut2);
+
+            Assert.Contains(@"Error
+	Code: UnhandledException
+	Message: An unhandled exception occurred.", sut2);
+
+            Condition.FlipFlop(options.IncludeEvidence, () =>
+            {
+                Assert.Contains(@$"Evidence
+	Thrower:
+		Cuemon.Extensions.Diagnostics.ExceptionDescriptorExtensionsTest.{nameof(ToInsightsString_ShouldReturnDetailedExceptionString_WithCaptures_MakeUseOfIncludeOptions)}", sut2);
+            }, () =>
+            {
+                Assert.DoesNotContain(@$"Evidence
+	Thrower:
+		Cuemon.Extensions.Diagnostics.ExceptionDescriptorExtensionsTest.{nameof(ToInsightsString_ShouldReturnDetailedExceptionString_WithCaptures_MakeUseOfIncludeOptions)}", sut2);
+            });
+
+            Condition.FlipFlop(options.IncludeFailure, () =>
+            {
+                Assert.Contains(@$"Failure
+	System.InsufficientMemoryException
+		Source: Cuemon.Extensions.Diagnostics.Tests
+		Message: Insufficient memory to continue the execution of the program.", sut2);
+            }, () =>
+            {
+                Assert.DoesNotContain(@$"Failure
+	System.InsufficientMemoryException
+		Source: Cuemon.Extensions.Diagnostics.Tests
+		Message: Insufficient memory to continue the execution of the program.", sut2);
+            });
+
+            Condition.FlipFlop(options.IncludeFailure && options.IncludeStackTrace, () =>
+            {
+                Assert.Contains(@$"Stack:
+			at Cuemon.Extensions.Diagnostics.ExceptionDescriptorExtensionsTest.{nameof(ToInsightsString_ShouldReturnDetailedExceptionString_WithCaptures_MakeUseOfIncludeOptions)}", sut2);
+            }, () =>
+            {
+                Assert.DoesNotContain(@$"Stack:
+			at Cuemon.Extensions.Diagnostics.ExceptionDescriptorExtensionsTest.{nameof(ToInsightsString_ShouldReturnDetailedExceptionString_WithCaptures_MakeUseOfIncludeOptions)}", sut2);
+            });
+
+            Condition.FlipFlop(snapshots.HasFlag(SystemSnapshots.CaptureThreadInfo), () => Assert.Contains(@"	Thread:", sut2), () => Assert.DoesNotContain(@"	Thread:", sut2));
+            Condition.FlipFlop(snapshots.HasFlag(SystemSnapshots.CaptureProcessInfo), () => Assert.Contains(@"	Process:", sut2), () => Assert.DoesNotContain(@"	Process:", sut2));
+            Condition.FlipFlop(snapshots.HasFlag(SystemSnapshots.CaptureEnvironmentInfo), () => Assert.Contains(@"	Environment:", sut2), () => Assert.DoesNotContain(@"	Environment:", sut2));
+        }
+
+        private static IEnumerable<object[]> GetData()
+        {
+            var parameters = new List<object[]>()
+            {
+                new object[] { new ExceptionDescriptorOptions(), SystemSnapshots.CaptureAll },
+                new object[] 
+                { 
+                    new ExceptionDescriptorOptions()
+                    {
+                        IncludeEvidence = false
+                    },
+                    SystemSnapshots.None
+                },
+                new object[] 
+                { 
+                    new ExceptionDescriptorOptions()
+                    {
+                        IncludeFailure = false
+                    }, 
+                    SystemSnapshots.CaptureThreadInfo
+                },
+                new object[] 
+                { 
+                    new ExceptionDescriptorOptions()
+                    {
+                        IncludeStackTrace = false
+                    }, 
+                    SystemSnapshots.CaptureProcessInfo
+                },
+                new object[] 
+                { 
+                    new ExceptionDescriptorOptions()
+                    {
+                        IncludeEvidence = false,
+                        IncludeFailure = false,
+                        IncludeStackTrace = false
+                    }, 
+                    SystemSnapshots.None
+                }
+            };
+
+            return parameters;
+        }
+    }
+}
---
+++ test/Cuemon.Extensions.Diagnostics.Tests/FileVersionInfoExtensionsTest.cs
@@ -0,0 +1,44 @@
+﻿using System.Diagnostics;
+using Cuemon.Extensions.Xunit;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Cuemon.Extensions.Diagnostics
+{
+    public class FileVersionInfoExtensionsTest : Test
+    {
+        public FileVersionInfoExtensionsTest(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        [Fact]
+        public void ToProductVersion_ShouldConvertFileVersionInfoToVersionResult()
+        {
+            var sut1 = FileVersionInfo.GetVersionInfo(typeof(FileVersionInfoExtensions).Assembly.Location);
+            var sut2 = sut1.ToProductVersion();
+
+            TestOutput.WriteLine(sut1.ToString());
+            TestOutput.WriteLine(sut2.ToString());
+
+            Assert.True(sut2.HasAlphanumericVersion);
+            Assert.True(sut2.IsSemanticVersion());
+            Assert.Equal(sut2.AlphanumericVersion, sut1.ProductVersion);
+            Assert.Equal(sut2.ToString(), sut1.ProductVersion);
+        }
+
+        [Fact]
+        public void ToFileVersion_ShouldConvertFileVersionInfoToVersionResult()
+        {
+            var sut1 = FileVersionInfo.GetVersionInfo(typeof(FileVersionInfoExtensions).Assembly.Location);
+            var sut2 = sut1.ToFileVersion();
+
+            TestOutput.WriteLine(sut1.ToString());
+            TestOutput.WriteLine(sut2.ToString());
+
+            Assert.False(sut2.HasAlphanumericVersion);
+            Assert.False(sut2.IsSemanticVersion());
+            Assert.NotEqual(sut2.AlphanumericVersion, sut1.ProductVersion);
+            Assert.Equal(sut2.ToString(), sut1.FileVersion);
+        }
+    }
+}
