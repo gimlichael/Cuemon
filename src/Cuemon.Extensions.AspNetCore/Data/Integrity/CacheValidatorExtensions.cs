738f2ba871cb44eb3cab80f2a267704a5c340a13
Michael Mortensen <michael@geekle.io>
2021-03-29 01:30:17 +0200

    ToEntityTag --> ToEntityTagHeaderValue

--- src/Cuemon.Extensions.AspNetCore/Data/Integrity/CacheValidatorExtensions.cs
+++ src/Cuemon.Extensions.AspNetCore/Data/Integrity/CacheValidatorExtensions.cs
@@ -13,7 +13,7 @@ namespace Cuemon.Extensions.AspNetCore.Data.Integrity
         /// </summary>
         /// <param name="validator">The validator to extend.</param>
         /// <returns>An <see cref="EntityTagHeaderValue"/> that is initiated with a hexadecimal representation of <see cref="ChecksumBuilder.Checksum"/> and a value that indicates if the tag is weak.</returns>
-        public static EntityTagHeaderValue ToEntityTag(this CacheValidator validator)
+        public static EntityTagHeaderValue ToEntityTagHeaderValue(this CacheValidator validator)
         {
             Validator.ThrowIfNull(validator, nameof(validator));
             return validator.ToEntityTagHeaderValue(validator.Validation != EntityDataIntegrityValidation.Strong);
