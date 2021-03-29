1bff9a3eb0393ae586e7b3c6e7d44ef8226c1e7c
Michael Mortensen <michael@geekle.io>
2021-03-29 13:52:38 +0200

    Simplified.

--- src/Cuemon.Extensions.Xunit.Hosting.AspNetCore/Http/Features/FakeHttpResponseFeature.cs
+++ src/Cuemon.Extensions.Xunit.Hosting.AspNetCore/Http/Features/FakeHttpResponseFeature.cs
@@ -12,8 +12,6 @@ namespace Cuemon.Extensions.Xunit.Hosting.AspNetCore.Http.Features
     public class FakeHttpResponseFeature : HttpResponseFeature
     {
         private bool _hasStarted;
-        private Func<object, Task> _callback;
-        private object _state;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="FakeHttpResponseFeature"/> class.
@@ -24,29 +22,15 @@ namespace Cuemon.Extensions.Xunit.Hosting.AspNetCore.Http.Features
         }
 
         /// <summary>
-        /// Registers a callback to be invoked just before the response starts. This is the
-        /// last chance to modify the <see cref="P:Microsoft.AspNetCore.Http.Features.IHttpResponseFeature.Headers" />, <see cref="P:Microsoft.AspNetCore.Http.Features.IHttpResponseFeature.StatusCode" />, or
-        /// <see cref="P:Microsoft.AspNetCore.Http.Features.IHttpResponseFeature.ReasonPhrase" />.
+        /// Registers a callback to be invoked just before the response starts. This is the last chance to modify the <see cref="P:Microsoft.AspNetCore.Http.Features.IHttpResponseFeature.Headers" />, <see cref="P:Microsoft.AspNetCore.Http.Features.IHttpResponseFeature.StatusCode" />, or <see cref="P:Microsoft.AspNetCore.Http.Features.IHttpResponseFeature.ReasonPhrase" />.
         /// </summary>
         /// <param name="callback">The callback to invoke when starting the response.</param>
         /// <param name="state">The state to pass into the callback.</param>
         public override void OnStarting(Func<object, Task> callback, object state)
         {
-            if (_hasStarted) { return; }
-            if (ShortCircuitOnStarting)
-            {
-                _hasStarted = true;
-                callback?.Invoke(state);
-            }
-            _callback = callback;
-            _state = state;
+            _hasStarted = true;
+            callback?.Invoke(state);
         }
-
-        /// <summary>
-        /// Gets a value indicating whether this instance has <see cref="OnStarting"/> callback.
-        /// </summary>
-        /// <value><c>true</c> if this instance has <see cref="OnStarting"/> callback; otherwise, <c>false</c>.</value>
-        public bool HasOnStartingCallback => _callback != null;
 
         /// <summary>
         /// Indicates if the response has started. If true, the <see cref="P:Microsoft.AspNetCore.Http.Features.IHttpResponseFeature.StatusCode" />,
@@ -55,21 +39,5 @@ namespace Cuemon.Extensions.Xunit.Hosting.AspNetCore.Http.Features
         /// </summary>
         /// <value><c>true</c> if this instance has started; otherwise, <c>false</c>.</value>
         public override bool HasStarted => _hasStarted;
-
-        /// <summary>
-        /// Gets or sets a value indicating whether <see cref="OnStarting"/> is invoked immediately upon initialization.
-        /// </summary>
-        /// <value><c>true</c> if <see cref="OnStarting"/> is invoked immediately upon initialization; otherwise, <c>false</c>.</value>
-        public bool ShortCircuitOnStarting { get; set; }
-
-        /// <summary>
-        /// Executes the function delegate assigned by <see cref="OnStarting"/>.
-        /// </summary>
-        /// <returns>A task that represents the asynchronous operation.</returns>
-        public Task TriggerOnStartingAsync()
-        {
-            _hasStarted = true;
-            return HasOnStartingCallback ? _callback(_state) : Task.CompletedTask;
-        }
     }
 }
