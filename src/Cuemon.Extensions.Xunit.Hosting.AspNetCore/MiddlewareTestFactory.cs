9a0005f0d788b05e4b27a016c72678f05362209b
Michael Mortensen <michael@geekle.io>
2021-03-29 14:14:50 +0200

    Added new method; RunMiddlewareTest. Also added support for configuring options for the Host.

--- src/Cuemon.Extensions.Xunit.Hosting.AspNetCore/MiddlewareTestFactory.cs
+++ src/Cuemon.Extensions.Xunit.Hosting.AspNetCore/MiddlewareTestFactory.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Threading.Tasks;
 using Cuemon.Extensions.Xunit.Hosting.AspNetCore.Http;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace Cuemon.Extensions.Xunit.Hosting.AspNetCore
 {
@@ -12,16 +14,33 @@ namespace Cuemon.Extensions.Xunit.Hosting.AspNetCore
     public static class MiddlewareTestFactory
     {
         /// <summary>
-        /// Creates and returns an <see cref="IMiddlewareTest"/> implementation.
+        /// Creates and returns an <see cref="IMiddlewareTest" /> implementation.
         /// </summary>
-        /// <param name="pipelineSetup">The <see cref="IApplicationBuilder"/> which may be configured.</param>
-        /// <param name="serviceSetup">The <see cref="IServiceCollection"/> which may be configured.</param>
-        /// <returns>An instance of an <see cref="IMiddlewareTest"/> implementation.</returns>
-        public static IMiddlewareTest CreateMiddlewareTest(Action<IApplicationBuilder> pipelineSetup = null, Action<IServiceCollection> serviceSetup = null)
+        /// <param name="pipelineSetup">The <see cref="IApplicationBuilder" /> which may be configured.</param>
+        /// <param name="serviceSetup">The <see cref="IServiceCollection" /> which may be configured.</param>
+        /// <param name="hostSetup">The <see cref="IHostBuilder"/> which may be configured.</param>
+        /// <returns>An instance of an <see cref="IMiddlewareTest" /> implementation.</returns>
+        public static IMiddlewareTest CreateMiddlewareTest(Action<IApplicationBuilder> pipelineSetup = null, Action<IServiceCollection> serviceSetup = null, Action<IHostBuilder> hostSetup = null)
         {
-            pipelineSetup ??= app => app.UseFakeHttpResponseTrigger();
             serviceSetup ??= services => services.AddScoped<IHttpContextAccessor, FakeHttpContextAccessor>();
-            return new MiddlewareAspNetCoreHostTest(pipelineSetup, serviceSetup, new AspNetCoreHostFixture());
+            return new MiddlewareAspNetCoreHostTest(pipelineSetup, serviceSetup, hostSetup, new AspNetCoreHostFixture());
+        }
+
+        /// <summary>
+        /// Runs a middleware test.
+        /// </summary>
+        /// <param name="pipelineSetup">The <see cref="IApplicationBuilder" /> which may be configured.</param>
+        /// <param name="serviceSetup">The <see cref="IServiceCollection" /> which may be configured.</param>
+        /// <param name="hostSetup">The <see cref="IHostBuilder" /> which may be configured.</param>
+        /// <returns>A task that represents the execution of the middleware.</returns>
+        public static async Task RunMiddlewareTest(Action<IApplicationBuilder> pipelineSetup = null, Action<IServiceCollection> serviceSetup = null, Action<IHostBuilder> hostSetup = null)
+        {
+            using (var middleware = CreateMiddlewareTest(pipelineSetup, serviceSetup, hostSetup))
+            {
+                var context = middleware.ServiceProvider.GetRequiredService<IHttpContextAccessor>().HttpContext;
+                var pipeline = middleware.Application.Build();
+                await pipeline(context).ConfigureAwait(false);
+            }
         }
     }
 }
