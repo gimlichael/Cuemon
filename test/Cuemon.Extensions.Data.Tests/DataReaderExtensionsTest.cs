18e942c65fd511f581c89fd77960e71fb78487a5
Michael Mortensen <michael@geekle.io>
2021-04-06 00:31:23 +0200

    New unit tests.

---
+++ test/Cuemon.Extensions.Data.Tests/DataReaderExtensionsTest.cs
@@ -0,0 +1,56 @@
+﻿using System.IO;
+using System.Linq;
+using Cuemon.Data;
+using Cuemon.Extensions.Reflection;
+using Cuemon.Extensions.Xunit;
+using Cuemon.Reflection;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Cuemon.Extensions.Data
+{
+    public class DataReaderExtensionsTest : Test
+    {
+        public DataReaderExtensionsTest(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        [Fact]
+        public void ToRows_ShouldConvertDataReaderToDataTransferRowCollection()
+        {
+            var sut1 = this.GetType().GetEmbeddedResources("DsvDataReaderTest_Wiki.csv", ManifestResourceMatch.ContainsName).Values.Single();
+            var sut2 = new DsvDataReader(new StreamReader(sut1));
+            var sut3 = sut2.ToRows();
+
+            foreach (var sut4 in sut3)
+            {
+                TestOutput.WriteLine(sut4.ToString());
+            }
+
+            sut2.Dispose();
+
+            Assert.Equal(4, sut3.Count);
+            Assert.Equal(5, sut3.ColumnNames.Count());
+        }
+
+        [Fact]
+        public void ToColumns_ShouldConvertDataReaderTDataTransferColumnCollection()
+        {
+            var sut1 = this.GetType().GetEmbeddedResources("DsvDataReaderTest_Wiki.csv", ManifestResourceMatch.ContainsName).Values.Single();
+            var sut2 = new DsvDataReader(new StreamReader(sut1));
+
+            sut2.Read(); // we have to read at least once to have columns (reason is, that IDataReader source can be anything .. and does not necessary know before hand what columns are exposed per row
+
+            var sut3 = sut2.ToColumns();
+
+            foreach (var sut4 in sut3)
+            {
+                TestOutput.WriteLine(sut4.ToString());
+            }
+
+            sut2.Dispose();
+
+            Assert.Equal(5, sut3.Count);
+        }
+    }
+}
---
+++ test/Cuemon.Extensions.Data.Tests/QueryFormatExtensionsTest.cs
@@ -0,0 +1,150 @@
+﻿using System.Linq;
+using Cuemon.Data;
+using Cuemon.Extensions.Xunit;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Cuemon.Extensions.Data
+{
+    public class QueryFormatExtensionsTest : Test
+    {
+        public QueryFormatExtensionsTest(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        [Fact]
+        public void Embed_ShouldWrapSequenceInDelimitedQueryFormat()
+        {
+            var sut1 = Enumerable.Range(1, 10).ToList();
+            var sut2 = QueryFormat.Delimited.Embed(sut1);
+            var sut3 = sut2.Split(',');
+
+            TestOutput.WriteLine(sut2);
+
+            Assert.Collection(sut3,
+                s => Assert.Equal("1", s),
+                s => Assert.Equal("2", s),
+                s => Assert.Equal("3", s),
+                s => Assert.Equal("4", s),
+                s => Assert.Equal("5", s),
+                s => Assert.Equal("6", s),
+                s => Assert.Equal("7", s),
+                s => Assert.Equal("8", s),
+                s => Assert.Equal("9", s),
+                s => Assert.Equal("10", s));
+        }
+
+        [Fact]
+        public void Embed_ShouldWrapSequenceInDelimitedSquareBracketQueryFormat()
+        {
+            var sut1 = Enumerable.Range(1, 10).ToList();
+            var sut2 = QueryFormat.DelimitedSquareBracket.Embed(sut1);
+            var sut3 = sut2.Split(',');
+
+            TestOutput.WriteLine(sut2);
+
+            Assert.Collection(sut3,
+                s => Assert.Equal("[1]", s),
+                s => Assert.Equal("[2]", s),
+                s => Assert.Equal("[3]", s),
+                s => Assert.Equal("[4]", s),
+                s => Assert.Equal("[5]", s),
+                s => Assert.Equal("[6]", s),
+                s => Assert.Equal("[7]", s),
+                s => Assert.Equal("[8]", s),
+                s => Assert.Equal("[9]", s),
+                s => Assert.Equal("[10]", s));
+        }
+
+        [Fact]
+        public void Embed_ShouldWrapSequenceInDelimitedStringQueryFormat()
+        {
+            var sut1 = Enumerable.Range(1, 10).ToList();
+            var sut2 = QueryFormat.DelimitedString.Embed(sut1);
+            var sut3 = sut2.Split(',');
+
+            TestOutput.WriteLine(sut2);
+
+            Assert.Collection(sut3,
+                s => Assert.Equal("'1'", s),
+                s => Assert.Equal("'2'", s),
+                s => Assert.Equal("'3'", s),
+                s => Assert.Equal("'4'", s),
+                s => Assert.Equal("'5'", s),
+                s => Assert.Equal("'6'", s),
+                s => Assert.Equal("'7'", s),
+                s => Assert.Equal("'8'", s),
+                s => Assert.Equal("'9'", s),
+                s => Assert.Equal("'10'", s));
+        }
+
+        [Fact]
+        public void Embed_ShouldWrapSequenceInDelimitedQueryFormat_Long()
+        {
+            long counter = int.MaxValue;
+            var sut1 = Generate.RangeOf(10, i => counter + i).ToList();
+            var sut2 = QueryFormat.Delimited.Embed(sut1);
+            var sut3 = sut2.Split(',');
+
+            TestOutput.WriteLine(sut2);
+
+            Assert.Collection(sut3,
+                s => Assert.Equal("2147483647", s),
+                s => Assert.Equal("2147483648", s),
+                s => Assert.Equal("2147483649", s),
+                s => Assert.Equal("2147483650", s),
+                s => Assert.Equal("2147483651", s),
+                s => Assert.Equal("2147483652", s),
+                s => Assert.Equal("2147483653", s),
+                s => Assert.Equal("2147483654", s),
+                s => Assert.Equal("2147483655", s),
+                s => Assert.Equal("2147483656", s));
+        }
+
+        [Fact]
+        public void Embed_ShouldWrapSequenceInDelimitedSquareBracketQueryFormat_Long()
+        {
+            long counter = int.MaxValue;
+            var sut1 = Generate.RangeOf(10, i => counter + i).ToList();
+            var sut2 = QueryFormat.DelimitedSquareBracket.Embed(sut1);
+            var sut3 = sut2.Split(',');
+
+            TestOutput.WriteLine(sut2);
+
+            Assert.Collection(sut3,
+                s => Assert.Equal("[2147483647]", s),
+                s => Assert.Equal("[2147483648]", s),
+                s => Assert.Equal("[2147483649]", s),
+                s => Assert.Equal("[2147483650]", s),
+                s => Assert.Equal("[2147483651]", s),
+                s => Assert.Equal("[2147483652]", s),
+                s => Assert.Equal("[2147483653]", s),
+                s => Assert.Equal("[2147483654]", s),
+                s => Assert.Equal("[2147483655]", s),
+                s => Assert.Equal("[2147483656]", s));
+        }
+
+        [Fact]
+        public void Embed_ShouldWrapSequenceInDelimitedStringQueryFormat_Long()
+        {
+            long counter = int.MaxValue;
+            var sut1 = Generate.RangeOf(10, i => counter + i).ToList();
+            var sut2 = QueryFormat.DelimitedString.Embed(sut1);
+            var sut3 = sut2.Split(',');
+
+            TestOutput.WriteLine(sut2);
+
+            Assert.Collection(sut3,
+                s => Assert.Equal("'2147483647'", s),
+                s => Assert.Equal("'2147483648'", s),
+                s => Assert.Equal("'2147483649'", s),
+                s => Assert.Equal("'2147483650'", s),
+                s => Assert.Equal("'2147483651'", s),
+                s => Assert.Equal("'2147483652'", s),
+                s => Assert.Equal("'2147483653'", s),
+                s => Assert.Equal("'2147483654'", s),
+                s => Assert.Equal("'2147483655'", s),
+                s => Assert.Equal("'2147483656'", s));
+        }
+    }
+}
