ef0805058cdf6f4696c328033e223dc1de8c3894
Michael Mortensen <michael@geekle.io>
2021-04-12 00:53:11 +0200

    Refactored Dependency and Watcher concepts to be consistent with other areas of Cuemon.

---
+++ src/Cuemon.Core/Runtime/IWatcher.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Cuemon.Runtime
+{
+    /// <summary>
+    /// Specifies that this object supports a way to monitor a resource.
+    /// </summary>
+    public interface IWatcher : IDisposable
+    {
+        /// <summary>
+        /// Occurs when a resource has changed.
+        /// </summary>
+        event EventHandler<WatcherEventArgs> Changed;
+
+        /// <summary>
+        /// Gets the time when the resource being monitored was last changed.
+        /// </summary>
+        /// <value>The time when the resource being monitored was last changed.</value>
+        DateTime UtcLastModified { get; }
+
+        /// <summary>
+        /// Starts the monitoring of this <see cref="IWatcher"/> implementation.
+        /// </summary>
+        void StartMonitoring();
+    }
+}
