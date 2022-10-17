34bd701068c3c31e70eb0d9a01307cd794f215d0
gimlichael <michael@geekle.io>
2022-10-18 00:58:53 +0200

    Minor optmization

--- src/Cuemon.Core/ExceptionInsights.cs
+++ src/Cuemon.Core/ExceptionInsights.cs
@@ -15,6 +15,8 @@ namespace Cuemon
     /// </summary>
     public static class ExceptionInsights
     {
+        private static readonly string EmptyBase64 = Convert.ToBase64String(Convertible.GetBytes(""));
+
         /// <summary>
         /// The <see cref="Key"/> used when applying insights to the <see cref="Exception.Data"/> dictionary.
         /// </summary>
@@ -46,7 +48,7 @@ namespace Cuemon
         {
             Validator.ThrowIfNull(exception, nameof(exception));
             var builder = new StringBuilder();
-            var empty = Convert.ToBase64String(Convertible.GetBytes(""));
+            var empty = EmptyBase64;
             if (thrower != null || exception.TargetSite != null)
             {
                 var descriptor = new MethodDescriptor(thrower ?? exception.TargetSite);
