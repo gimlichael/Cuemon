c7686c3dc80cfffc67c301a6f1f9d0b2dd1a714e
gimlichael <michael@geekle.io>
2022-10-30 22:20:10 +0100

    Rare bug triggered on some *ix systems

---
+++ test/Cuemon.Core.Tests/Globalization/WorldTest.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cuemon.Extensions.Xunit;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Cuemon.Globalization
+{
+    public class WorldTest : Test
+    {
+        public WorldTest(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        [Fact]
+        public void Regions_ShouldReturnAllRegions()
+        {
+            var sut1 = World.Regions.ToList();
+
+            TestOutput.WriteLine(sut1.Count.ToString());
+
+            Assert.NotNull(sut1);
+            Assert.True(sut1.Count > 220, "sut1.Count > 220");
+        }
+
+        [Fact]
+        public void Regions_ShouldReturnAllCultures_FromRegions()
+        {
+            var sut1 = World.Regions.ToList();
+            var sut2 = new List<CultureInfo>();
+
+            foreach (var region in sut1)
+            {
+                foreach (var culture in World.GetCultures(region))
+                {
+                    sut2.Add(culture);
+                    if (culture.IsNeutralCulture)
+                    {
+                        TestOutput.WriteLine(culture.Name);
+                    }
+                }
+            }
+
+            TestOutput.WriteLine(sut2.Count.ToString());
+
+            Assert.NotNull(sut2);
+            Assert.True(sut2.Count > 500, "sut1.Count > 500");
+        }
+    }
+}
