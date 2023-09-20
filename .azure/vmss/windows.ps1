e903a5e7d70c3216e54dd4718c0d7fc36a3874fa
gimlichael <michael@geekle.io>
2023-09-20 22:00:39 +0200

    Messing with env. vars

--- .azure/vmss/windows.ps1
+++ .azure/vmss/windows.ps1
@@ -1,2 +1,4 @@
-Invoke-WebRequest 'https://dot.net/v1/dotnet-install.ps1' -OutFile 'dotnet-install.ps1'; ./dotnet-install.ps1 -Channel LTS
+Invoke-WebRequest 'https://dot.net/v1/dotnet-install.ps1' -OutFile 'dotnet-install.ps1'; ./dotnet-install.ps1 -Channel LTS -InstallDir 'C:\Program Files\dotnet'
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
+If (!(Test-Path $Profile.CurrentUserAllHosts)) {New-Item -Path $Profile.CurrentUserAllHosts -Force}
+Add-Content -Path $Profile.CurrentUserAllHosts -Value '$env:Path += \";C:\Program Files\dotnet;%USERPROFILE%\.dotnet\tools\"'
