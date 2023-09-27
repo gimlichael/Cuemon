1c62cff1f32534edae9d8d0af5bffc78a2f5cdcd
gimlichael <michael@geekle.io>
2023-09-27 23:34:18 +0200

    tweaked vmss scripts

--- .azure/vmss/windows.ps1
+++ .azure/vmss/windows.ps1
@@ -1,4 +1,4 @@
 Invoke-WebRequest 'https://dot.net/v1/dotnet-install.ps1' -OutFile 'dotnet-install.ps1'; ./dotnet-install.ps1 -Version 8.0.100-rc.2.23469.4 -InstallDir 'C:\Program Files\dotnet'
-$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
-If (!(Test-Path $Profile.CurrentUserAllHosts)) {New-Item -Path $Profile.CurrentUserAllHosts -Force}
-Add-Content -Path $Profile.CurrentUserAllHosts -Value '$env:Path += \";C:\Program Files\dotnet;$env:USERPROFILE\.dotnet\tools\"'
+$envVariable = "Path"
+$machinePath = $Env:Path + '";C:\Program Files\dotnet;' + $Env:USERPROFILE + '\.dotnet\tools\"'
+[System.Environment]::SetEnvironmentVariable($envVariable, $machinePath, [System.EnvironmentVariableTarget]::Machine)
