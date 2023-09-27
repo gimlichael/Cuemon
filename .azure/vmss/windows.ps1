2157eacce0b6782c4e5460cad7942451e8aafa45
gimlichael <michael@geekle.io>
2023-09-27 23:50:19 +0200

    added dotnet_root

--- .azure/vmss/windows.ps1
+++ .azure/vmss/windows.ps1
@@ -1,4 +1,7 @@
 Invoke-WebRequest 'https://dot.net/v1/dotnet-install.ps1' -OutFile 'dotnet-install.ps1'; ./dotnet-install.ps1 -Version 8.0.100-rc.2.23469.4 -InstallDir 'C:\Program Files\dotnet'
-$envVariable = "Path"
-$machinePath = $Env:Path + '";C:\Program Files\dotnet;' + $Env:USERPROFILE + '\.dotnet\tools\"'
-[System.Environment]::SetEnvironmentVariable($envVariable, $machinePath, [System.EnvironmentVariableTarget]::Machine)
+$pathVariableName = 'Path'
+$dotnetRootVariableName = 'DOTNET_ROOT'
+$dotnetRootPath = 'C:\Program Files\dotnet'
+$machinePath = $Env:Path + ';' + $dotnetRootPath + ';' + $Env:USERPROFILE + '\.dotnet\tools\'
+[System.Environment]::SetEnvironmentVariable($dotnetRootVariableName, $dotnetRootPath, [System.EnvironmentVariableTarget]::Machine)
+[System.Environment]::SetEnvironmentVariable($pathVariableName, $machinePath, [System.EnvironmentVariableTarget]::Machine)
