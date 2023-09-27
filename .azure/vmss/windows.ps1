d07eb3b3a56a741d1fa24714b0e1298a9f828e18
gimlichael <michael@geekle.io>
2023-09-27 20:49:06 +0200

    Try to run pwsh from powershell

--- .azure/vmss/windows.ps1
+++ .azure/vmss/windows.ps1
@@ -1,6 +1,7 @@
+$ProgressPreference = 'SilentlyContinue'
+Invoke-WebRequest 'https://github.com/PowerShell/PowerShell/releases/download/v7.3.7/PowerShell-7.3.7-win-x64.msi' -OutFile 'PowerShell-7.3.7-win-x64.msi' ; ./PowerShell-7.3.7-win-x64.msi /quiet ADD_EXPLORER_CONTEXT_MENU_OPENPOWERSHELL=0 ADD_FILE_CONTEXT_MENU_RUNPOWERSHELL=0 ENABLE_PSREMOTING=0 REGISTER_MANIFEST=0 USE_MU=1 ENABLE_MU=1 ADD_PATH=1
+pwsh
 Invoke-WebRequest 'https://dot.net/v1/dotnet-install.ps1' -OutFile 'dotnet-install.ps1'; ./dotnet-install.ps1 -Channel STS -InstallDir 'C:\Program Files\dotnet'
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
 If (!(Test-Path $Profile.CurrentUserAllHosts)) {New-Item -Path $Profile.CurrentUserAllHosts -Force}
 Add-Content -Path $Profile.CurrentUserAllHosts -Value '$env:Path += \";C:\Program Files\dotnet;$env:USERPROFILE\.dotnet\tools\"'
-$ProgressPreference = 'SilentlyContinue'
-Invoke-WebRequest 'https://github.com/PowerShell/PowerShell/releases/download/v7.3.7/PowerShell-7.3.7-win-x64.msi' -OutFile 'PowerShell-7.3.7-win-x64.msi' ; ./PowerShell-7.3.7-win-x64.msi /quiet ADD_EXPLORER_CONTEXT_MENU_OPENPOWERSHELL=0 ADD_FILE_CONTEXT_MENU_RUNPOWERSHELL=0 ENABLE_PSREMOTING=0 REGISTER_MANIFEST=0 USE_MU=1 ENABLE_MU=1 ADD_PATH=1
