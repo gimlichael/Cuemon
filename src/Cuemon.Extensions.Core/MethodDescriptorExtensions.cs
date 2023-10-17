b591bbdb9bc3ff35563c745ab567a4c230d1fd82
gimlichael <michael@geekle.io>
2023-10-17 17:00:25 +0200

    Removed HasParameters to Extensions. Made IsProperty to a private propery.

---
+++ src/Cuemon.Extensions.Core/MethodDescriptorExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using Cuemon.Reflection;
+
+namespace Cuemon.Extensions
+{
+    /// <summary>
+    /// Extension methods for the <see cref="MethodDescriptor"/> class.
+    /// </summary>
+    public static class MethodDescriptorExtensions
+    {
+        /// <summary>
+        /// Determines whether the underlying method has parameters.
+        /// </summary>
+        /// <param name="descriptor">The <see cref="MethodDescriptor"/> to extend.</param>
+        /// <returns><c>true</c> if the specified descriptor has parameters; otherwise, <c>false</c>.</returns>
+        public static bool HasParameters(this MethodDescriptor descriptor)
+        {
+            return descriptor.Parameters.Any();
+        }
+    }
+}
