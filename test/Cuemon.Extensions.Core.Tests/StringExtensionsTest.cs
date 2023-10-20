67f70f5caf267af73baeab3973933426ef9956ae
gimlichael <michael@geekle.io>
2023-10-20 19:14:27 +0200

    Complemented with a unit-test for ReplaceLineEndings()

--- test/Cuemon.Extensions.Core.Tests/StringExtensionsTest.cs
+++ test/Cuemon.Extensions.Core.Tests/StringExtensionsTest.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
-using System.Text;
+using System.Runtime.InteropServices;
 using Cuemon.Collections.Generic;
 using Cuemon.Extensions.Xunit;
 using Xunit;
@@ -14,6 +14,29 @@ namespace Cuemon.Extensions
     {
         public StringExtensionsTest(ITestOutputHelper output) : base(output)
         {
+        }
+
+        [Fact]
+        public void ReplaceLineEndings_ShouldReplaceNewLineOccurrences()
+        {
+            var lineEndings = "Windows has \r\n (CRLF) and Linux has \n (LF)";
+
+            TestOutput.WriteLine($$"""
+                                   Before: {{lineEndings}}
+                                   After: {{lineEndings.ReplaceLineEndings()}}
+                                   """);
+
+            TestOutput.WriteLine(RuntimeInformation.OSDescription);
+            TestOutput.WriteLine(RuntimeInformation.FrameworkDescription);
+
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                Assert.Equal("Windows has \n (CRLF) and Linux has \n (LF)", lineEndings.ReplaceLineEndings());
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                Assert.Equal("Windows has \r\n (CRLF) and Linux has \r\n (LF)", lineEndings.ReplaceLineEndings());
+            }
         }
 
         [Fact]
