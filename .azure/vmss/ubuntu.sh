b005bbcbe78f249fc79f0337aec00ea828525bab
gimlichael <michael@geekle.io>
2023-11-07 00:30:47 +0100

    Exclude mono ..

--- .azure/vmss/ubuntu.sh
+++ .azure/vmss/ubuntu.sh
@@ -33,14 +33,6 @@ sudo apt-get update
 # Install PowerShell
 sudo apt-get install -y powershell
 
-echo "Installing MONO  ..."
-
-sudo apt-get install gnupg ca-certificates
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
-echo "deb https://download.mono-project.com/repo/ubuntu stable-focal main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
-sudo apt-get update
-sudo apt-get install -y mono-devel
-
 echo "Installing NuGet  ..."
 
 sudo apt-get install -y nuget
