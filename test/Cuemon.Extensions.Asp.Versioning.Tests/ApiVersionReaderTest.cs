e9367463a91dda76ba3a2ed1485d4495cb5f056b
gimlichael <michael@geekle.io>
2023-11-10 20:17:46 +0100

    Extended unit test with one use-case for Newtonsoft JSON.

--- test/Cuemon.Extensions.Asp.Versioning.Tests/ApiVersionReaderTest.cs
+++ test/Cuemon.Extensions.Asp.Versioning.Tests/ApiVersionReaderTest.cs
@@ -1,19 +1,16 @@
-﻿using System.Collections.Generic;
-using System.Net;
+﻿using System.Net;
 using System.Net.Http;
 using System.Text.Encodings.Web;
-using System.Text.Unicode;
 using System.Threading.Tasks;
-using Asp.Versioning;
-using Cuemon.AspNetCore.Diagnostics;
 using Cuemon.AspNetCore.Http;
-using Cuemon.AspNetCore.Mvc.Filters.Diagnostics;
 using Cuemon.Diagnostics;
 using Cuemon.Extensions.Asp.Versioning.Assets;
 using Cuemon.Extensions.AspNetCore.Diagnostics;
 using Cuemon.Extensions.AspNetCore.Mvc.Filters;
+using Cuemon.Extensions.AspNetCore.Mvc.Formatters.Newtonsoft.Json;
 using Cuemon.Extensions.AspNetCore.Mvc.Formatters.Text.Json;
 using Cuemon.Extensions.AspNetCore.Mvc.Formatters.Xml;
+using Cuemon.Extensions.Newtonsoft.Json.Formatters;
 using Cuemon.Extensions.Text.Json.Formatters;
 using Cuemon.Extensions.Xunit;
 using Cuemon.Extensions.Xunit.Hosting.AspNetCore.Mvc;
@@ -22,7 +19,6 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
-using Microsoft.Extensions.WebEncoders.Testing;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -91,7 +87,7 @@ namespace Cuemon.Extensions.Asp.Versioning
                 Assert.Equal(@"Error: 
   Status: 406
   Code: NotAcceptable
-  Message: The HTTP resource that matches the request URI 'http://localhost/fake/' does not support the API version '2.0'.",stringResult, ignoreLineEndingDifferences: true);
+  Message: The HTTP resource that matches the request URI 'http://localhost/fake/' does not support the API version '2.0'.", stringResult, ignoreLineEndingDifferences: true);
             }
         }
 
@@ -129,7 +125,7 @@ namespace Cuemon.Extensions.Asp.Versioning
         }
 
         [Fact]
-        public async Task GetRequest_ShouldFailWithBadRequestFormattedAsJsonResponse_As_d3_IsAnUnknownVersion_CorreclySetOnApplicationJsonAccept()
+        public async Task GetRequest_ShouldFailWithBadRequestFormattedAsJsonResponse_As_d3_IsAnUnknownVersion_CorrectlySetOnApplicationJsonAccept()
         {
             using (var app = WebApplicationTestFactory.Create(services =>
             {
@@ -139,7 +135,7 @@ namespace Cuemon.Extensions.Asp.Versioning
                 services.AddHttpContextAccessor();
                 services.AddRestfulApiVersioning();
             }, app =>
-                   { 
+                   {
                        app.UseFaultDescriptorExceptionHandler(o =>
                        {
                            o.NonMvcResponseHandlers
@@ -168,6 +164,74 @@ namespace Cuemon.Extensions.Asp.Versioning
     ""message"": ""The HTTP resource that matches the request URI \u0027http://localhost/fake/throw\u0027 does not support the API version \u0027d3\u0027.""
   }
 }", await sut.Content.ReadAsStringAsync(), ignoreLineEndingDifferences: true);
+            }
+        }
+
+        [Fact]
+        public async Task GetRequest_ShouldFailWithBadRequestFormattedAsNewtonsoftJsonResponse_As_d3_IsAnUnknownVersion_CorrectlySetOnApplicationJsonAccept()
+        {
+            using (var app = WebApplicationTestFactory.Create(services =>
+            {
+                services.AddControllers(o => o.Filters.AddFaultDescriptor())
+                    .AddApplicationPart(typeof(FakeController).Assembly)
+                    .AddNewtonsoftJsonFormatters(o => o.SensitivityDetails = FaultSensitivityDetails.Evidence);
+                services.AddHttpContextAccessor();
+                services.AddRestfulApiVersioning();
+            }, app =>
+                   {
+                       app.UseFaultDescriptorExceptionHandler(o =>
+                       {
+                           o.SensitivityDetails = FaultSensitivityDetails.Evidence;
+                           o.NonMvcResponseHandlers
+                               .AddNewtonsoftJsonResponseHandler(app.ApplicationServices.GetRequiredService<IOptions<NewtonsoftJsonFormatterOptions>>())
+                               .AddXmlResponseHandler(app.ApplicationServices.GetRequiredService<IOptions<XmlFormatterOptions>>());
+                       });
+                       app.UseRestfulApiVersioning();
+                       app.UseRouting();
+                       app.UseEndpoints(routes => { routes.MapControllers(); });
+
+                   }))
+            {
+                var client = app.Host.GetTestClient();
+                client.DefaultRequestHeaders.Add("Accept", "text/html,application/xhtml+xml,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9,application/json;q=10.0;v=d3");
+                var sut = await client.GetAsync("/fake/throw");
+
+                TestOutput.WriteLine(await sut.Content.ReadAsStringAsync());
+
+                Assert.Equal(HttpStatusCode.BadRequest, sut.StatusCode);
+                Assert.Equal(HttpMethod.Get, sut.RequestMessage.Method);
+                Assert.EndsWith("application/json", sut.Content.Headers.ContentType.ToString());
+                Assert.Equal("""
+                             {
+                               "error": {
+                                 "status": 400,
+                                 "code": "BadRequest",
+                                 "message": "The HTTP resource that matches the request URI 'http://localhost/fake/throw' does not support the API version 'd3'."
+                               },
+                               "evidence": {
+                                 "request": {
+                                   "location": "http://localhost/fake/throw",
+                                   "method": "GET",
+                                   "headers": {
+                                     "accept": [
+                                       "text/html",
+                                       "application/xhtml+xml",
+                                       "image/avif",
+                                       "image/webp",
+                                       "image/apng",
+                                       "*/*; q=0.8",
+                                       "application/signed-exchange; v=b3; q=0.9",
+                                       "application/json; q=10.0; v=d3"
+                                     ],
+                                     "host": "localhost"
+                                   },
+                                   "query": [],
+                                   "cookies": [],
+                                   "body": ""
+                                 }
+                               }
+                             }
+                             """, await sut.Content.ReadAsStringAsync(), ignoreLineEndingDifferences: true);
             }
         }
 
