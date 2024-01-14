fb91ce31977925e8815728bb7fdc64891b3cf127
gimlichael <michael@geekle.io>
2024-01-14 16:34:08 +0100

    Added yet-another support for Basic Authentication but using AuthenticationHandler, e.g., welcome to BasicAuthenticationHandler. Supports built-in ASP.NET Core Authorize use-cases.

---
+++ src/Cuemon.Extensions.AspNetCore.Authentication/AuthenticationBuilderExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Cuemon.AspNetCore.Authentication.Basic;
+using Microsoft.AspNetCore.Authentication;
+
+namespace Cuemon.Extensions.AspNetCore.Authentication
+{
+	/// <summary>
+	/// Extension methods for the <see cref="AuthenticationBuilder"/> class.
+	/// </summary>
+	public static class AuthenticationBuilderExtensions
+	{
+		/// <summary>
+		/// Adds an <see cref="BasicAuthenticationHandler"/> to the authentication middleware.
+		/// </summary>
+		/// <param name="builder">The <see cref="AuthenticationBuilder"/> to extend.</param>
+		/// <param name="setup">The <see cref="BasicAuthenticationOptions"/> which needs to be configured.</param>
+		/// <returns>A reference to <paramref name="builder" /> so that additional calls can be chained.</returns>
+		/// <exception cref="ArgumentNullException">
+		/// <paramref name="builder"/> cannot be null.
+		/// </exception>
+		/// <exception cref="ArgumentException">
+		/// <paramref name="setup"/> failed to configure an instance of <see cref="BasicAuthenticationOptions"/> in a valid state.
+		/// </exception>
+		public static AuthenticationBuilder AddBasic(this AuthenticationBuilder builder, Action<BasicAuthenticationOptions> setup)
+		{
+			Validator.ThrowIfNull(builder);
+			Validator.ThrowIfInvalidConfigurator(setup, out _);
+			return builder.AddScheme<BasicAuthenticationOptions, BasicAuthenticationHandler>(BasicAuthorizationHeader.Scheme, setup);
+		}
+	}
+}
---
+++ test/Cuemon.AspNetCore.Authentication.Tests/Assets/FakeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cuemon.AspNetCore.Authentication.Assets
+{
+    [Authorize]
+    [ApiController]
+    [Route("[controller]")]
+    public class FakeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Unit Test");
+        }
+
+        [AllowAnonymous]
+        [HttpGet("anonymous")]
+        public IActionResult GetAnonymous()
+        {
+	        return Ok("Unit Test");
+        }
+    }
+}
