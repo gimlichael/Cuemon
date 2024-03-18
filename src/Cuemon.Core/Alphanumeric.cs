c4a507c4cd7f8d91caa5e3e682ef289fc9516860
Michael Mortensen <mmdevelopment@hotmail.com>
2024-03-18 19:05:56 +0100

    Removed U+180E as it is no longer considered a whitespace.

--- src/Cuemon.Core/Alphanumeric.cs
+++ src/Cuemon.Core/Alphanumeric.cs
@@ -38,7 +38,7 @@
         /// <summary>
         /// A representation of the most common whitespace characters.
         /// </summary>
-        public const string WhiteSpace = "\u0009\u000A\u000B\u000C\u000D\u0020\u0085\u00A0\u1680\u180E\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200A\u2028\u2029\u202F\u205F\u3000";
+        public const string WhiteSpace = "\u0009\u000A\u000B\u000C\u000D\u0020\u0085\u00A0\u1680\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200A\u2028\u2029\u202F\u205F\u3000";
 
         /// <summary>
         /// A representation of a hexadecimal character set consisting of the numbers 0 to 9 and the letters A to F.
