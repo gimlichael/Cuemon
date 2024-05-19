579edf434095b0b92e438c2a7c45b50ba0c04dc9
Michael Mortensen <michael@geekle.io>
2024-05-19 21:54:44 +0200

    :construction_worker: migration from AzDO to GitHub Actions

--- .azure/vmss/windows.ps1
+++
@@ -1,2 +0,0 @@
-$ProgressPreference = 'SilentlyContinue'
-Invoke-WebRequest 'https://github.com/PowerShell/PowerShell/releases/download/v7.3.7/PowerShell-7.3.7-win-x64.msi' -OutFile 'PowerShell-7.3.7-win-x64.msi' ; ./PowerShell-7.3.7-win-x64.msi /quiet ADD_EXPLORER_CONTEXT_MENU_OPENPOWERSHELL=0 ADD_FILE_CONTEXT_MENU_RUNPOWERSHELL=0 ENABLE_PSREMOTING=0 REGISTER_MANIFEST=0 USE_MU=1 ENABLE_MU=1 ADD_PATH=1
