712df766bba2337956587752808ed443d8582fbd
Michael Mortensen <michael@geekle.io>
2024-06-01 15:02:35 +0200

    :hammer: updated docfx scripts to use minver properly

--- .docfx/BuildDocfxImage.ps1
+++ .docfx/BuildDocfxImage.ps1
@@ -1,4 +1,4 @@
-﻿$version = minver -i
+﻿$version = minver -i -t v -v w
 docfx metadata docfx.json
 docker buildx build -t cuemon-docfx:$version --platform linux/arm64,linux/amd64 --load -f Dockerfile.docfx .
 get-childItem -recurse -path api -include *.yml, .manifest | remove-item
--- .docfx/PublishDocfxImage.ps1
+++ .docfx/PublishDocfxImage.ps1
@@ -1,3 +1,3 @@
-$version = minver -i
+$version = minver -i -t v -v w
 docker tag cuemon-docfx:$version jcr.codebelt.net/geekle/cuemon-docfx:$version
 docker push jcr.codebelt.net/geekle/cuemon-docfx:$version
