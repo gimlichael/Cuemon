f92f5297207cae22b9e3deb6f013e76f6a2e1a9f
Michael Mortensen <michael@geekle.io>
2024-06-01 13:57:12 +0200

    :memo: updated changelog and release notes

--- src/Cuemon.Extensions.Xunit.Hosting.AspNetCore/WebHostTestFactory.cs
+++ src/Cuemon.Extensions.Xunit.Hosting.AspNetCore/WebHostTestFactory.cs
@@ -8,7 +8,7 @@ using Microsoft.Extensions.Hosting;
 namespace Cuemon.Extensions.Xunit.Hosting.AspNetCore
 {
     /// <summary>
-    /// Provides a set of static methods for ASP.NET Core, ASP.NET Core MVC, Razor and related unit testing.
+    /// Provides a set of static methods for ASP.NET Core (including, but not limited to MVC, Razor and related) unit testing.
     /// </summary>
     /// <seealso cref="IHostTest"/>.
     public static class WebHostTestFactory
