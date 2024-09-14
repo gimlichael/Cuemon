018c6910db78cdfc8953bbb624f7eb3b76cca4c0
Michael Mortensen <michael@geekle.io>
2024-09-15 00:18:16 +0200

    :rotating_light: https://learn.microsoft.com/en-us/dotnet/fundamentals/code-analysis/quality-rules/ca2215

--- src/Cuemon.Extensions.Net/GlobalSuppressions.cs
+++ src/Cuemon.Extensions.Net/GlobalSuppressions.cs
@@ -6,3 +6,4 @@
 using System.Diagnostics.CodeAnalysis;
 
 [assembly: SuppressMessage("Major Code Smell", "S3881:\"IDisposable\" should be implemented correctly", Justification = "The lifetime of this is tracked separately by ActiveHandler.", Scope = "type", Target = "~T:Cuemon.Extensions.Net.Http.TrackingHttpMessageHandler")]
+[assembly: SuppressMessage("Usage", "CA2215:Dispose methods should call base class dispose", Justification = "The lifetime of this is tracked separately by ActiveHandler.", Scope = "member", Target = "~M:Cuemon.Extensions.Net.Http.TrackingHttpMessageHandler.Dispose(System.Boolean)")]
