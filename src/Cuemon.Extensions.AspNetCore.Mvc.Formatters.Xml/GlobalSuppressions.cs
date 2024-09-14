2a17ebb4546471f0fee28e284985ccb50ec804f7
Michael Mortensen <michael@geekle.io>
2024-09-15 01:04:23 +0200

    :rotating_light: https://learn.microsoft.com/en-us/dotnet/fundamentals/code-analysis/style-rules/ide0076

--- src/Cuemon.Extensions.AspNetCore.Mvc.Formatters.Xml/GlobalSuppressions.cs
+++
@@ -1,8 +0,0 @@
-﻿// This file is used by Code Analysis to maintain SuppressMessage
-// attributes that are applied to this project.
-// Project-level suppressions either have no target or are given
-// a specific target and scoped to a namespace, type, member, etc.
-
-using System.Diagnostics.CodeAnalysis;
-
-[assembly: SuppressMessage("Critical Code Smell", "S3776:Cognitive Complexity of methods should not be too high", Justification = "Clear enough; XML Converter.", Scope = "member", Target = "~M:Cuemon.Extensions.AspNetCore.Mvc.Formatters.Xml.Converters.XmlConverterExtensions.AddHttpExceptionDescriptorConverter(System.Collections.Generic.IList{Cuemon.Xml.Serialization.Converters.XmlConverter},System.Action{Cuemon.Diagnostics.ExceptionDescriptorOptions})~System.Collections.Generic.IList{Cuemon.Xml.Serialization.Converters.XmlConverter}")]
