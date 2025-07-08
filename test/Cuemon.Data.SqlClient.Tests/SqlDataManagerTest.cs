69c43b7dfbcb6101e7625be1f72e47a3bc48258a
Michael Mortensen <michael@geekle.io>
2025-07-08 23:21:38 +0200

    :rotating_light: replaced System.Data.SqlClient with Microsoft.Data.SqlClient (for f*** sake, Microsoft: https://github.com/dotnet/SqlClient/discussions/3115)

--- test/Cuemon.Data.SqlClient.Tests/SqlDataManagerTest.cs
+++ test/Cuemon.Data.SqlClient.Tests/SqlDataManagerTest.cs
@@ -1,6 +1,6 @@
 using System;
 using System.Data;
-using System.Data.SqlClient;
+using Microsoft.Data.SqlClient;
 using System.Linq;
 using Cuemon.Collections.Generic;
 using Cuemon.Data.SqlClient.Assets;
